8bfc9bc282f75ebd65020e99a7b79fb7b27723b8
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2019-05-06 14:03:15 +0200

    InvalidClauseException to file

---
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/InvalidClauseException.kt
@@ -0,0 +1,6 @@
+package it.unibo.tuprolog.core
+
+open class InvalidClauseException(val term: Term, cause: Throwable? = null) : TuprologRuntimeException(cause) {
+    override val message: String?
+        get() = "Term `term` is not a valid clause"
+}
--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/TuprologRuntimeException.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/TuprologRuntimeException.kt
@@ -7,7 +7,3 @@ open class TuprologRuntimeException : Exception {
     constructor(cause: Throwable?) : super(cause)
 }
 
-open class InvalidClauseException(val term: Term, cause: Throwable? = null) : TuprologRuntimeException(cause) {
-    override val message: String?
-        get() = "Term `term` is not a valid clause"
-}
