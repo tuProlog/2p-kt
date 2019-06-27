4a0e2c07583b74c5e929dd422ca42a6a49f2d6d5
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-06-27 14:41:41 +0200

    Shortened Term class

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
@@ -1,12 +1,8 @@
 package it.unibo.tuprolog.core
 
-import kotlin.collections.List
-
 interface Term {
 
-    fun <T : Term> castTo(): T {
-        return this as T
-    }
+    fun <T : Term> castTo(): T = this as T
 
     infix fun structurallyEquals(other: Term): Boolean
     infix fun strictlyEquals(other: Term): Boolean
@@ -15,157 +11,89 @@ interface Term {
         get() = isVar
 
     val isVar: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isBound: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isGround: Boolean
-        get() {
-            return true
-        }
+        get() = true
 
     val isStruct: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isAtom: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isNumber: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isInt: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isReal: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isList: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isEmptySet: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isSet: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isClause: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isRule: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isFact: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isDirective: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isCouple: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isEmptyList: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isTrue: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
     val isFail: Boolean
-        get() {
-            return false
-        }
+        get() = false
 
-    fun clone(): Term {
-        return this
-    }
+    fun clone(): Term = this
 
-    operator fun plus(other: Term): Struct {
-        return Struct.of("+", this, other)
-    }
+    operator fun plus(other: Term): Struct = Struct.of("+", this, other)
 
-    operator fun minus(other: Term): Struct {
-        return Struct.of("-", this, other)
-    }
+    operator fun minus(other: Term): Struct = Struct.of("-", this, other)
 
-    operator fun times(other: Term): Struct {
-        return Struct.of("*", this, other)
-    }
+    operator fun times(other: Term): Struct = Struct.of("*", this, other)
 
-    operator fun div(other: Term): Struct {
-        return Struct.of("/", this, other)
-    }
+    operator fun div(other: Term): Struct = Struct.of("/", this, other)
 
-    infix fun greaterThan(other: Term): Struct {
-        return Struct.of(">", this, other)
-    }
+    infix fun greaterThan(other: Term): Struct = Struct.of(">", this, other)
 
-    infix fun greaterThanOrEqualsTo(other: Term): Struct {
-        return Struct.of(">=", this, other)
-    }
+    infix fun greaterThanOrEqualsTo(other: Term): Struct = Struct.of(">=", this, other)
 
-    infix fun nonLowerThan(other: Term): Struct {
-        return this greaterThanOrEqualsTo other
-    }
+    infix fun nonLowerThan(other: Term): Struct = this greaterThanOrEqualsTo other
 
-    infix fun lowerThan(other: Term): Struct {
-        return Struct.of("<", this, other)
-    }
+    infix fun lowerThan(other: Term): Struct = Struct.of("<", this, other)
 
-    infix fun lowerThanOrEqualsTo(other: Term): Struct {
-        return Struct.of("=<", this, other)
-    }
+    infix fun lowerThanOrEqualsTo(other: Term): Struct = Struct.of("=<", this, other)
 
-    infix fun nonGreaterThan(other: Term): Struct {
-        return this lowerThanOrEqualsTo other
-    }
+    infix fun nonGreaterThan(other: Term): Struct = this lowerThanOrEqualsTo other
 
-    infix fun intDiv(other: Term): Struct {
-        return Struct.of("//", this, other)
-    }
+    infix fun intDiv(other: Term): Struct = Struct.of("//", this, other)
 
-    operator fun rem(other: Term): Struct {
-        return Struct.of("rem", this, other)
-    }
+    operator fun rem(other: Term): Struct = Struct.of("rem", this, other)
 
-    infix fun pow(other: Term): Struct {
-        return Struct.of("**", this, other)
-    }
+    infix fun pow(other: Term): Struct = Struct.of("**", this, other)
 
-    infix fun sup(other: Term): Struct {
-        return Struct.of("^", this, other)
-    }
+    infix fun sup(other: Term): Struct = Struct.of("^", this, other)
 }
