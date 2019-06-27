4fb5100916b2ca24ae3936d7fa6b03337b8e312f
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-06-27 15:18:13 +0200

    Removed Term methods that were made for use as DSL

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
@@ -72,32 +72,4 @@ interface Term {
      * Example: "f(X, g(X))".freshCopy() returns something like "f(X_1, g(X_1))"
      */
     fun freshCopy(): Term = this
-
-    operator fun plus(other: Term): Struct = Struct.of("+", this, other)
-
-    operator fun minus(other: Term): Struct = Struct.of("-", this, other)
-
-    operator fun times(other: Term): Struct = Struct.of("*", this, other)
-
-    operator fun div(other: Term): Struct = Struct.of("/", this, other)
-
-    infix fun greaterThan(other: Term): Struct = Struct.of(">", this, other)
-
-    infix fun greaterThanOrEqualsTo(other: Term): Struct = Struct.of(">=", this, other)
-
-    infix fun nonLowerThan(other: Term): Struct = this greaterThanOrEqualsTo other
-
-    infix fun lowerThan(other: Term): Struct = Struct.of("<", this, other)
-
-    infix fun lowerThanOrEqualsTo(other: Term): Struct = Struct.of("=<", this, other)
-
-    infix fun nonGreaterThan(other: Term): Struct = this lowerThanOrEqualsTo other
-
-    infix fun intDiv(other: Term): Struct = Struct.of("//", this, other)
-
-    operator fun rem(other: Term): Struct = Struct.of("rem", this, other)
-
-    infix fun pow(other: Term): Struct = Struct.of("**", this, other)
-
-    infix fun sup(other: Term): Struct = Struct.of("^", this, other)
 }
