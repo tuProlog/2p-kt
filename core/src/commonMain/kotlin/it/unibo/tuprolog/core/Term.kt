0432d31a2bf0288c1f0e9e1dd21270862e7a8659
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-06-27 15:21:52 +0200

    Further Term class shortening

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Term.kt
@@ -7,62 +7,25 @@ interface Term {
     infix fun structurallyEquals(other: Term): Boolean
     infix fun strictlyEquals(other: Term): Boolean
 
-    val isVariable: Boolean
-        get() = false
-
-    val isBound: Boolean
-        get() = false
-
-    val isGround: Boolean
-        get() = true
-
-    val isStruct: Boolean
-        get() = false
-
-    val isAtom: Boolean
-        get() = false
-
-    val isNumber: Boolean
-        get() = false
-
-    val isInt: Boolean
-        get() = false
-
-    val isReal: Boolean
-        get() = false
-
-    val isList: Boolean
-        get() = false
-
-    val isEmptySet: Boolean
-        get() = false
-
-    val isSet: Boolean
-        get() = false
-
-    val isClause: Boolean
-        get() = false
-
-    val isRule: Boolean
-        get() = false
-
-    val isFact: Boolean
-        get() = false
-
-    val isDirective: Boolean
-        get() = false
-
-    val isCouple: Boolean
-        get() = false
-
-    val isEmptyList: Boolean
-        get() = false
-
-    val isTrue: Boolean
-        get() = false
-
-    val isFail: Boolean
-        get() = false
+    val isVariable: Boolean get() = false
+    val isBound: Boolean get() = false
+    val isGround: Boolean get() = true
+    val isStruct: Boolean get() = false
+    val isAtom: Boolean get() = false
+    val isNumber: Boolean get() = false
+    val isInt: Boolean get() = false
+    val isReal: Boolean get() = false
+    val isList: Boolean get() = false
+    val isEmptySet: Boolean get() = false
+    val isSet: Boolean get() = false
+    val isClause: Boolean get() = false
+    val isRule: Boolean get() = false
+    val isFact: Boolean get() = false
+    val isDirective: Boolean get() = false
+    val isCouple: Boolean get() = false
+    val isEmptyList: Boolean get() = false
+    val isTrue: Boolean get() = false
+    val isFail: Boolean get() = false
 
     /**
      * Returns a fresh copy of this Term.
