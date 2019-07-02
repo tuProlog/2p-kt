22aa0d6fc6e677f487bd6f9873922be1d9668e66
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-02 11:16:27 +0200

    List.from BugFixed :) and removed useless method

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/List.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/List.kt
@@ -17,20 +17,15 @@ interface List : Struct {
 
         fun empty(): List = Empty.list()
 
-        fun from(items: KtList<Term>, last: Term? = null): List = when {
-            last === null -> {
-                val tail = Couple.of(items[items.lastIndex - 1], items[items.lastIndex])
-                items.slice(0 until items.size - 2).foldRight(tail) { h, t -> Couple.of(h, t) }
-            }
-            else -> items.foldRight<Term, List>(Empty.list()) { h, t -> Couple.of(h, t) }
-        }
+        fun from(items: KtList<Term>, last: Term? = null): List =
+                items.foldRight(
+                        if (last === null) Empty.list() else Couple.last(last)
+                ) { head, tail -> Couple.of(head, tail) }
 
         fun from(items: Iterable<Term>, last: Term? = null): List = from(items.toList(), last)
 
         fun from(items: Sequence<Term>, last: Term? = null): List = from(items.toList(), last)
 
-        fun of(vararg items: Term): List = from(items.toList(), Empty.list())
-
-        fun of(items: Iterable<Term>): List = from(items.toList(), Empty.list())
+        fun of(vararg items: Term): List = from(items.toList())
     }
 }
