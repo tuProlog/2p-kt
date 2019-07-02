d33eb22ea87ea0c3c6e910e6239dde64ecf0ce98
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-02 10:05:26 +0200

    Tested Set companion object

---
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/SetTest.kt
@@ -0,0 +1,74 @@
+package it.unibo.tuprolog.core
+
+import it.unibo.tuprolog.core.impl.SetImpl
+import it.unibo.tuprolog.core.testutils.AssertionUtils.assertEqualities
+import it.unibo.tuprolog.core.testutils.AssertionUtils.onCorrespondingItems
+import it.unibo.tuprolog.core.testutils.SetUtils
+import kotlin.test.Test
+import kotlin.test.assertSame
+
+/**
+ * Test class for [Set] companion object
+ *
+ * @author Enrico
+ */
+internal class SetTest {
+
+    @Test
+    fun emptyReturnsEmptySet() {
+        assertEqualities(Empty.set(), Set.empty())
+        assertSame(Empty.set(), Set.empty())
+    }
+
+    @Test
+    fun setOfNoVarargTerms() {
+        assertEqualities(Empty.set(), Set.of())
+    }
+
+    @Test
+    fun setOfVarargTerms() {
+        val correctInstances = SetUtils.mixedSets.map(::SetImpl)
+        val toBeTested = SetUtils.mixedSets.map { Set.of(*it) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun setOfEmptyCollectionOfTerms() {
+        assertEqualities(Empty.set(), Set.of(emptyList<Term>() as Collection<Term>))
+    }
+
+    @Test
+    fun setOfCollectionOfTerms() {
+        val correctInstances = SetUtils.mixedSets.map(::SetImpl)
+        val toBeTested = SetUtils.mixedSets.map { Set.of(it.toList() as Collection<Term>) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun setOfEmptyIterableOfTerms() {
+        assertEqualities(Empty.set(), Set.of(emptyList<Term>() as Iterable<Term>))
+    }
+
+    @Test
+    fun setOfIterableOfTerms() {
+        val correctInstances = SetUtils.mixedSets.map(::SetImpl)
+        val toBeTested = SetUtils.mixedSets.map { Set.of(it.toList() as Iterable<Term>) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun setOfEmptySequenceOfTerms() {
+        assertEqualities(Empty.set(), Set.of(emptySequence()))
+    }
+
+    @Test
+    fun setOfSequenceOfTerms() {
+        val correctInstances = SetUtils.mixedSets.map(::SetImpl)
+        val toBeTested = SetUtils.mixedSets.map { Set.of(it.asSequence()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+}
