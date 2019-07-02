d58475be1025273b5201560c423202734377d51a
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-02 10:54:29 +0200

    [ListTest not passing]: found a Bug in "List.from" method! It won't work with one sized list

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/List.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/List.kt
@@ -15,22 +15,22 @@ interface List : Struct {
 
     companion object {
 
+        fun empty(): List = Empty.list()
+
+        fun from(items: KtList<Term>, last: Term? = null): List = when {
+            last === null -> {
+                val tail = Couple.of(items[items.lastIndex - 1], items[items.lastIndex])
+                items.slice(0 until items.size - 2).foldRight(tail) { h, t -> Couple.of(h, t) }
+            }
+            else -> items.foldRight<Term, List>(Empty.list()) { h, t -> Couple.of(h, t) }
+        }
+
         fun from(items: Iterable<Term>, last: Term? = null): List = from(items.toList(), last)
 
         fun from(items: Sequence<Term>, last: Term? = null): List = from(items.toList(), last)
 
-        fun from(items: KtList<Term>, last: Term? = null): List =
-                if (last === null) {
-                    val tail = Couple.of(items[items.lastIndex - 1], items[items.lastIndex])
-                    items.slice(0 until items.size - 2).foldRight(tail) { h, t -> Couple.of(h, t) }
-                } else {
-                    items.foldRight<Term, List>(Empty.list()) { h, t -> Couple.of(h, t) }
-                }
-
         fun of(vararg items: Term): List = from(items.toList(), Empty.list())
 
         fun of(items: Iterable<Term>): List = from(items.toList(), Empty.list())
-
-        fun empty(): List = Empty.list()
     }
 }
---
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/ListTest.kt
@@ -0,0 +1,34 @@
+package it.unibo.tuprolog.core
+
+import it.unibo.tuprolog.core.impl.CoupleImpl
+import it.unibo.tuprolog.core.testutils.AssertionUtils.assertEqualities
+import it.unibo.tuprolog.core.testutils.AssertionUtils.onCorrespondingItems
+import it.unibo.tuprolog.core.testutils.CoupleUtils
+import kotlin.test.Test
+import kotlin.test.assertSame
+import it.unibo.tuprolog.core.List as LogicList
+
+/**
+ * Test class for [List] companion object
+ *
+ * @author Enrico
+ */
+internal class ListTest {
+
+    private val correctInstances: List<LogicList> = CoupleUtils.coupleInstances(::CoupleImpl)
+
+    @Test
+    fun emptyReturnsEmptyList() {
+        assertEqualities(Empty.list(), LogicList.empty())
+        assertSame(Empty.list(), LogicList.empty())
+    }
+
+    @Test
+    fun fromListOfTermsWithoutLastSpecified() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it) }
+        onCorrespondingItems(correctInstances, toBeTested) { expected, actual ->
+            println(expected)
+            println(actual)
+        }
+    }
+}
