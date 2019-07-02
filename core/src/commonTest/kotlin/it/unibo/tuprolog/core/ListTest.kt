f0e9e95b808c5ce0801154e30e59a052939bb7e7
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-02 11:57:54 +0200

    Completed ListTest implementation

--- core/src/commonTest/kotlin/it/unibo/tuprolog/core/ListTest.kt
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/ListTest.kt
@@ -29,4 +29,113 @@ internal class ListTest {
 
         onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
     }
+
+    @Test
+    fun fromListOfTermsSpecifyingLast() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it.dropLast(1), it.last()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun fromEmptyListWithoutLastSpecified() {
+        val toBeTested = LogicList.from(emptyList())
+
+        assertEqualities(Empty.list(), toBeTested)
+    }
+
+    @Test
+    fun fromEmptyListSpecifyingLast() {
+        val correctInstance = CoupleImpl(Truth.`true`(), Empty.list())
+
+        val toBeTested = LogicList.from(emptyList(), Truth.`true`())
+
+        assertEqualities(correctInstance, toBeTested)
+    }
+
+    @Test
+    fun fromIterableOfTermsWithoutLastSpecified() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it as Iterable<Term>) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun fromIterableOfTermsSpecifyingLast() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it.dropLast(1) as Iterable<Term>, it.last()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun fromEmptyIterableWithoutLastSpecified() {
+        val toBeTested = LogicList.from(emptyList<Term>() as Iterable<Term>)
+
+        assertEqualities(Empty.list(), toBeTested)
+    }
+
+    @Test
+    fun fromEmptyIterableSpecifyingLast() {
+        val correctInstance = CoupleImpl(Truth.`true`(), Empty.list())
+
+        val toBeTested = LogicList.from(emptyList<Term>() as Iterable<Term>, Truth.`true`())
+
+        assertEqualities(correctInstance, toBeTested)
+    }
+
+    @Test
+    fun fromSequenceOfTermsWithoutLastSpecified() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it.asSequence()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun fromSequenceOfTermsSpecifyingLast() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.from(it.dropLast(1).asSequence(), it.last()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun fromEmptySequenceWithoutLastSpecified() {
+        val toBeTested = LogicList.from(emptyList<Term>().asSequence())
+
+        assertEqualities(Empty.list(), toBeTested)
+    }
+
+    @Test
+    fun fromEmptySequenceSpecifyingLast() {
+        val correctInstance = CoupleImpl(Truth.`true`(), Empty.list())
+
+        val toBeTested = LogicList.from(emptyList<Term>().asSequence(), Truth.`true`())
+
+        assertEqualities(correctInstance, toBeTested)
+    }
+
+    @Test
+    fun ofNoVarargTerms() {
+        assertEqualities(Empty.list(), LogicList.of())
+    }
+
+    @Test
+    fun ofVarargTerms() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.of(*it.toTypedArray()) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun ofIterableOfTerms() {
+        val toBeTested = CoupleUtils.coupleInstancesUnfoldedLists.map { LogicList.of(it as Iterable<Term>) }
+
+        onCorrespondingItems(correctInstances, toBeTested, ::assertEqualities)
+    }
+
+    @Test
+    fun ofEmptyIterable() {
+        val toBeTested = LogicList.of(emptyList<Term>() as Iterable<Term>)
+
+        assertEqualities(Empty.list(), toBeTested)
+    }
 }
