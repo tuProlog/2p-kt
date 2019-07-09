13b907e08ec1b421ea4e1072b0d686c17748fd23
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-09 19:29:59 +0200

    Removed TuprologException and made other ones extend TuprologRuntimeException

--- unify/src/commonMain/kotlin/it/unibo/tuprolog/unify/NoUnifyException.kt
+++ unify/src/commonMain/kotlin/it/unibo/tuprolog/unify/NoUnifyException.kt
@@ -1,13 +1,13 @@
 package it.unibo.tuprolog.unify
 
 import it.unibo.tuprolog.core.Term
-import it.unibo.tuprolog.core.TuprologException
+import it.unibo.tuprolog.core.TuprologRuntimeException
 
-open class NoUnifyException(val term1: Term, val term2: Term, other: Throwable?) : TuprologException(other) {
+open class NoUnifyException(private val term1: Term, private val term2: Term, other: Throwable?) : TuprologRuntimeException(other) {
 
     constructor(term1: Term, term2: Term) : this(term1, term2, null)
 
     override val message: String?
-        get() = "Cannot match term `${term1}` with term `${term2}`"
+        get() = "Cannot match term `$term1` with term `$term2`"
 }
 
