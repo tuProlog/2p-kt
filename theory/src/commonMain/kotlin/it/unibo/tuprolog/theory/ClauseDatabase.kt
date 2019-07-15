e10a62fa8d15305aa3ccb3a2d4a4f06f260d8acd
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-15 18:22:08 +0200

    Aspect Changes

--- theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/ClauseDatabase.kt
+++ theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/ClauseDatabase.kt
@@ -5,18 +5,16 @@ import kotlin.collections.List as KtList
 
 interface ClauseDatabase : Iterable<Clause> {
 
+    val clauses: KtList<Clause>
+
     val rules: KtList<Rule>
         get() = clauses.filterIsInstance<Rule>()
 
     val directives: KtList<Directive>
         get() = clauses.filterIsInstance<Directive>()
 
-    val clauses: KtList<Clause>
-
     operator fun plus(clauseDatabase: ClauseDatabase): ClauseDatabase
-    operator fun plus(clause: Clause): ClauseDatabase {
-        return assertZ(clause)
-    }
+    operator fun plus(clause: Clause): ClauseDatabase = assertZ(clause)
 
     operator fun contains(clause: Clause): Boolean
     operator fun contains(head: Struct): Boolean
@@ -25,36 +23,20 @@ interface ClauseDatabase : Iterable<Clause> {
     operator fun get(head: Struct): Sequence<Clause>
 
     fun assertA(clause: Clause): ClauseDatabase
-
-    fun assertA(struct: Struct): ClauseDatabase {
-        return assertA(Fact.of(struct))
-    }
+    fun assertA(struct: Struct): ClauseDatabase = assertA(Fact.of(struct))
 
     fun assertZ(clause: Clause): ClauseDatabase
-
-    fun assertZ(struct: Struct): ClauseDatabase {
-        return assertZ(Fact.of(struct))
-    }
+    fun assertZ(struct: Struct): ClauseDatabase = assertZ(Fact.of(struct))
 
     fun retract(clause: Clause): RetractResult
-
-    fun retract(head: Struct): RetractResult {
-        return retract(Rule.of(head, Var.anonymous()))
-    }
+    fun retract(head: Struct): RetractResult = retract(Rule.of(head, Var.anonymous()))
 
     fun retractAll(clause: Clause): RetractResult
-
-    fun retractAll(head: Struct): RetractResult {
-        return retractAll(Rule.of(head, Var.anonymous()))
-    }
+    fun retractAll(head: Struct): RetractResult = retractAll(Rule.of(head, Var.anonymous()))
 
     companion object {
-        fun of(vararg clause: Clause): ClauseDatabase {
-            return ClauseDatabaseImpl(listOf(*clause))
-        }
+        fun of(vararg clause: Clause): ClauseDatabase = ClauseDatabaseImpl(listOf(*clause))
 
-        fun of(clauses: KtList<Clause>): ClauseDatabase {
-            return ClauseDatabaseImpl(clauses)
-        }
+        fun of(clauses: KtList<Clause>): ClauseDatabase = ClauseDatabaseImpl(clauses)
     }
 }
