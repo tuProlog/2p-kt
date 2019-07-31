b9cc27b90d8154bd78e5370903e0b4d028fe1163
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-07-31 13:52:00 +0200

    OperatorSet doc and testing; added plus and minus of single element to have consistent behaviour after extending Set<Operator>

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/operators/OperatorSet.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/operators/OperatorSet.kt
@@ -1,14 +1,25 @@
 package it.unibo.tuprolog.core.operators
 
+/** Class representing a Set of [Operator]s */
 class OperatorSet(operators: Sequence<Operator>) : Set<Operator> by operators.toHashSet() {
 
     constructor(vararg operator: Operator) : this(operator.asSequence())
 
     constructor(operators: Iterable<Operator>) : this(operators.asSequence())
 
-    operator fun plus(other: OperatorSet): OperatorSet =
-            OperatorSet(this.asSequence() + other.asSequence())
+    /** Creates a new OperatorSet adding to this the other Operator, overriding already present operator if one */
+    operator fun plus(operator: Operator): OperatorSet =
+            OperatorSet(sequenceOf(operator, *this.toTypedArray()))
 
+    /** Creates a new OperatorSet adding to this the other operators, overriding already present operators */
+    operator fun plus(other: OperatorSet): OperatorSet =
+            OperatorSet(other.asSequence() + this.asSequence())
+
+    /** Creates a new OperatorSet removing from this the other operators */
+    operator fun minus(operator: Operator): OperatorSet =
+            OperatorSet(this as Set<Operator> - operator)
+
+    /** Creates a new OperatorSet removing from this the other operators */
     operator fun minus(other: OperatorSet): OperatorSet =
             OperatorSet(((this as Set<Operator>) - (other as Set<Operator>)))
 }
---
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/operators/OperatorSetTest.kt
@@ -0,0 +1,54 @@
+package it.unibo.tuprolog.core.operators
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+/**
+ * Test class for [OperatorSet]
+ *
+ * @author Enrico
+ */
+internal class OperatorSetTest {
+
+    private val plusOperator = Operator("+", Associativity.YFX, 500)
+    private val minusOperator = Operator("-", Associativity.YFX, 500)
+    private val timesOperator = Operator("*", Associativity.YFX, 400)
+    private val divisionOperator = Operator("/", Associativity.YFX, 400)
+
+    private val operatorSet = OperatorSet(plusOperator, minusOperator, timesOperator, divisionOperator)
+
+    private val overridingPlusOperator = Operator("+", Associativity.YFX, 1000)
+    private val overridingOperatorSet = OperatorSet(overridingPlusOperator)
+
+    @Test
+    fun plusOperator() {
+        val toBeTested = operatorSet + overridingPlusOperator
+
+        assertTrue { toBeTested.any { it === overridingPlusOperator } }
+        assertTrue { toBeTested.none { it === plusOperator } }
+    }
+
+    @Test
+    fun plusOperatorSet() {
+        val toBeTested = operatorSet + overridingOperatorSet
+
+        assertTrue { toBeTested.any { it === overridingPlusOperator } }
+        assertTrue { toBeTested.none { it === plusOperator } }
+    }
+
+    @Test
+    fun minusOperator() {
+        val toBeTested = operatorSet - plusOperator
+
+        assertTrue { toBeTested.none { it === plusOperator } }
+        assertTrue { toBeTested.none { it === overridingPlusOperator } }
+    }
+
+    @Test
+    fun minusOperatorSet() {
+        val toBeTested = operatorSet - OperatorSet(plusOperator)
+
+        assertTrue { toBeTested.none { it === plusOperator } }
+        assertTrue { toBeTested.none { it === overridingPlusOperator } }
+    }
+}
