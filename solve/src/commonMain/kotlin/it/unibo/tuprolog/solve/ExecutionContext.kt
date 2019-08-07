93f3626a5d68a69d6985672a742daee185b09e3c
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-08-07 17:24:11 +0200

    ExecutionContext documentation and testing

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/ExecutionContext.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/ExecutionContext.kt
@@ -5,9 +5,14 @@ import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.libraries.Libraries
 import it.unibo.tuprolog.theory.ClauseDatabase
 
+/** A class representing the Solver execution context, containing important information that determines its behaviour */
 data class ExecutionContext(
+        /** Loaded libraries */
         val libraries: Libraries,
+        /** Enabled flags */
         val flags: Map<Atom, Term>,
+        /** Static Knowledge-base, that is a KB that *can't* change executing goals */
         val staticKB: ClauseDatabase,
+        /** Dynamic Knowledge-base, that is a KB that *can* change executing goals */
         val dynamicKB: ClauseDatabase
 )
---
+++ solve/src/commonTest/kotlin/it/unibo/tuprolog/solve/ExecutionContextTest.kt
@@ -0,0 +1,32 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.libraries.Libraries
+import it.unibo.tuprolog.theory.ClauseDatabase
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Test class for [ExecutionContext]
+ *
+ * @author Enrico
+ */
+internal class ExecutionContextTest {
+
+    private val someLibraries = Libraries()
+    private val someFlags = emptyMap<Atom, Term>()
+    private val aStaticKB = ClauseDatabase.of()
+    private val aDynamicKB = ClauseDatabase.of()
+
+    @Test
+    fun executionContextHoldsInsertedData() {
+        val toBeTested = ExecutionContext(someLibraries, someFlags, aStaticKB, aDynamicKB)
+
+        assertEquals(someLibraries, toBeTested.libraries)
+        assertEquals(someFlags, toBeTested.flags)
+        assertEquals(aStaticKB, toBeTested.staticKB)
+        assertEquals(aDynamicKB, toBeTested.dynamicKB)
+    }
+
+}
