c459521cdab011abb215dd48387b14f9332c0992
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-08-12 16:15:39 +0200

    Solver interface method

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/mutable/Solver.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/mutable/Solver.kt
@@ -1,8 +1,14 @@
 package it.unibo.tuprolog.solve.mutable
 
+import it.unibo.tuprolog.solve.Solve
+
+/**
+ * Represents a Prolog Goal solver
+ *
+ * @author Enrico
+ */
 interface Solver {
 
-
-
-
+    /** Solves the provided goal, returning lazily initialized sequence of responses */
+    fun solve(goal: Solve.Request): Sequence<Solve.Response>
 }
