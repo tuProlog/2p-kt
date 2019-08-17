9b57acb05145d9af25f6864b09837b43cee50c0c
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-08-17 22:32:22 +0200

    Further implementing StateGoalSelection but still WIP

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/statemachine/state/StateGoalSelection.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/statemachine/state/StateGoalSelection.kt
@@ -1,8 +1,12 @@
 package it.unibo.tuprolog.solve.solver.statemachine.state
 
 import it.unibo.tuprolog.core.Clause
+import it.unibo.tuprolog.core.Directive
 import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.prepareForExecution
+import it.unibo.tuprolog.solve.Solution
 import it.unibo.tuprolog.solve.Solve
+import it.unibo.tuprolog.solve.solver.NotableFunctor
 import it.unibo.tuprolog.solve.solver.SolverStrategies
 import kotlinx.coroutines.CoroutineScope
 
@@ -31,12 +35,16 @@ internal class StateGoalSelection(
                     signature.name in Clause.notableFunctors -> {
                         val subGoalsToBeSolved = goalsToSolveOrdered(arguments.asSequence(), solverStrategies::predicationChoiceStrategy)
 
-                        // TODO: 17/08/2019 check for sub goals correctness, with Clause.bodyWellFormedVisitor
-                        // TODO: 17/08/2019 prepareForExecution should be called on arguments
+                        val combinedSubSolutions = solveSubGoals(
+                                subGoalsToBeSolved.first(),
+                                subGoalsToBeSolved.last(),
+                                NotableFunctor.valueOf(signature.name)
+                        )
 
-                        // maybe should be a good idea to transform temporarily the notable subgoal into a Clause and use its methods directly
-
-                        // TODO: 17/08/2019 depending on the specific functor the solution given from this state should change
+                        when (combinedSubSolutions) {
+                            is Solution.Yes -> TODO("Extract actual substitution from solution and add it to current context of a StateEnd.True()")
+                            is Solution.No -> yield(StateEnd.False(solveRequest, executionStrategy, solverStrategies))
+                        }
                     }
 
                     // current goal is some other Structure
@@ -48,6 +56,17 @@ internal class StateGoalSelection(
         }
     }
 
+    private fun <P : Term> solveSubGoals(leftSubGoal: P, rightSubGoal: P, notableFunctor: NotableFunctor): Solution {
+
+        // TODO: 17/08/2019 check for sub goals correctness, with Clause.bodyWellFormedVisitor
+        // TODO: 17/08/2019 prepareForExecution should be called on arguments
+
+        // Solver.sld(executionStrategy).solve(Solve.Request(Signature.fromIndicator(leftSubGoal.)!!))
+
+        TODO()
+    }
+
+
     /** Computes the ordered selection of goals according to provided selection strategy, run always on remaining goals */
     private fun <P : Term> goalsToSolveOrdered(goals: Sequence<P>, selectionStrategy: (Sequence<P>) -> P): Sequence<P> =
             mutableListOf<P>()
@@ -55,4 +74,16 @@ internal class StateGoalSelection(
                         repeat(goals.count()) { selected += selectionStrategy(goals - selected) }
                     }.asSequence()
 
+
+    /** Check whether the provided term is a well-formed predication */
+    private fun <P : Term> subGoalWellFormed(subGoal: P): Boolean = subGoal.accept(Clause.bodyWellFormedVisitor)
+
+    /**
+     * Prepares the provided Goal for execution
+     *
+     * For example, the goal `A` is transformed, after preparation for execution, as the Term: `call(A)`
+     */
+    private fun <P : Term> subGoalPrepareForExecution(subGoal: P): P =
+            // exploits "Clause" implementation of prepareForExecution() to do that
+            Directive.of(subGoal).prepareForExecution().args.single().castTo()
 }
