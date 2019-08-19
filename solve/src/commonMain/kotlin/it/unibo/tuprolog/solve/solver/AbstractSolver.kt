8ab23ab1944e6080e7344078d5983a000aef0007
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-08-19 15:19:57 +0200

    Added ExecutionContext parameter to SolverStrategies

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/AbstractSolver.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/AbstractSolver.kt
@@ -2,6 +2,7 @@ package it.unibo.tuprolog.solve.solver
 
 import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.solve.ExecutionContext
 
 /**
  * Represent an abstract [Solver]
@@ -18,13 +19,13 @@ abstract class AbstractSolver : Solver {
     protected open val solverStrategies = object : SolverStrategies {
 
         /** The default implementation, following the Prolog Standard, selects the first predication found */
-        override fun <P : Term> predicationChoiceStrategy(predicationSequence: Sequence<P>): P =
+        override fun <P : Term> predicationChoiceStrategy(predicationSequence: Sequence<P>, context: ExecutionContext): P =
                 predicationSequence.first()
 
         /** The default implementation, following the Prolog Standard, selects the first clause found */
-        override fun <C : Clause> clauseChoiceStrategy(unifiableClauses: Sequence<C>): C =
+        override fun <C : Clause> clauseChoiceStrategy(unifiableClauses: Sequence<C>, context: ExecutionContext): C =
                 unifiableClauses.first()
 
-        override fun successCheckStrategy(term: Term): Boolean = term.isTrue
+        override fun successCheckStrategy(term: Term, context: ExecutionContext): Boolean = term.isTrue
     }
 }
--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/SolverStrategies.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/SolverStrategies.kt
@@ -2,16 +2,17 @@ package it.unibo.tuprolog.solve.solver
 
 import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.solve.ExecutionContext
 
 /** Interface representing internal strategies used by the Solver */
 interface SolverStrategies {
 
     /** Selects the predication to be solved first, from provided sequence */
-    fun <P : Term> predicationChoiceStrategy(predicationSequence: Sequence<P>): P
+    fun <P : Term> predicationChoiceStrategy(predicationSequence: Sequence<P>, context: ExecutionContext): P
 
     /** Selects the clause to be expanded in place of unifying predication, from provided sequence */
-    fun <C : Clause> clauseChoiceStrategy(unifiableClauses: Sequence<C>): C
+    fun <C : Clause> clauseChoiceStrategy(unifiableClauses: Sequence<C>, context: ExecutionContext): C
 
     /** Determines "when and what" is considered successfully demonstrated, during solution process */
-    fun successCheckStrategy(term: Term): Boolean
+    fun successCheckStrategy(term: Term, context: ExecutionContext): Boolean
 }
