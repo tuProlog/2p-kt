d09f500cd4cb08b25f76e4a015ceec625c70906f
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-08-20 18:26:48 +0200

    Implementing StateRuleSelection... WIP

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/statemachine/state/StateRuleSelection.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/solver/statemachine/state/StateRuleSelection.kt
@@ -1,5 +1,7 @@
 package it.unibo.tuprolog.solve.solver.statemachine.state
 
+import it.unibo.tuprolog.core.Clause
+import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.Solve
 import it.unibo.tuprolog.solve.solver.SolverStrategies
 import kotlinx.coroutines.CoroutineScope
@@ -15,7 +17,34 @@ internal class StateRuleSelection(
         override val solverStrategies: SolverStrategies
 ) : AbstractTimedState(solveRequest, executionStrategy, solverStrategies) {
 
-    override fun behaveTimed(): Sequence<State> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun behaveTimed(): Sequence<State> = sequence {
+        val currentGoal = with(solveRequest) { signature.withArgs(arguments)!! }
+        val matchingClauses = solveRequest.context.libraries.theory[currentGoal]
+
+        when {
+            matchingClauses.none() -> yield(StateEnd.False(solveRequest, executionStrategy, solverStrategies))
+
+            else -> clausesOrdered(matchingClauses, solveRequest.context, solverStrategies::clauseChoiceStrategy).forEach {
+                yield(StateGoalSelection(
+                        solveRequest.copy(),
+                        executionStrategy,
+                        solverStrategies
+                ))
+                TODO("copy information to new solveRequest")
+            }
+        }
     }
+
+    /** Computes the ordered selection of clauses, according to provided selection strategy */
+    private fun <C : Clause> clausesOrdered(
+            clauses: Sequence<C>,
+            context: ExecutionContext,
+            selectionStrategy: (Sequence<C>, ExecutionContext) -> C
+    ): Sequence<C> = sequence {
+        mutableListOf<C>()
+                .also { selected ->
+                    repeat(clauses.count()) { yield(selectionStrategy(clauses - selected, context)) }
+                }
+    }
+
 }
