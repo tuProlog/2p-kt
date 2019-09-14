b59ae04e3d9210d5b27b5400ca0675684dfb8027
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-09-14 19:39:14 +0200

    Refactoring TuPrologException base class

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/exception/InvalidClauseException.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/exception/InvalidClauseException.kt
@@ -2,7 +2,7 @@ package it.unibo.tuprolog.core.exception
 
 import it.unibo.tuprolog.core.Term
 
-open class InvalidClauseException(val term: Term, cause: Throwable? = null) : TuprologRuntimeException(cause) {
+open class InvalidClauseException(val term: Term, cause: Throwable? = null) : TuPrologException(cause) {
     override val message: String?
         get() = "Term `$term` is not a valid clause"
 }
--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/exception/TuprologRuntimeException.kt
+++
@@ -1,9 +0,0 @@
-package it.unibo.tuprolog.core.exception
-
-open class TuprologRuntimeException : RuntimeException {
-    constructor() : super()
-    constructor(message: String?) : super(message)
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
-    constructor(cause: Throwable?) : super(cause)
-}
-
