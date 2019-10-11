3208552641eabf923f66cb31e4e4a879aab93a9f
Giovanni <giovanni.ciatto@unibo.it>
2019-10-11 18:23:08 +0200

    refactoring common builtins

--- solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/libraries/stdlib/DefaultBuiltins.kt
+++ solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/libraries/stdlib/DefaultBuiltins.kt
@@ -1,26 +1,18 @@
 package it.unibo.tuprolog.libraries.stdlib
 
-import it.unibo.tuprolog.core.Truth
-import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.libraries.Library
 import it.unibo.tuprolog.libraries.LibraryAliased
 import it.unibo.tuprolog.theory.ClauseDatabase
 
 object DefaultBuiltins : LibraryAliased by Library.of(
-        alias = "prolog.lang",
-        operatorSet = OperatorSet.DEFAULT,
-        theory = ClauseDatabase.of(
+        alias = CommonBuiltins.alias,
+        operatorSet = CommonBuiltins.operators,
+        theory = CommonBuiltins.theory + ClauseDatabase.of(
+                { ruleOf(tupleOf(varOf("A"), varOf("B")), varOf("A"), varOf("B")) },
                 { ruleOf(structOf("call", varOf("X")), varOf("X")) },
-                { ruleOf(structOf("catch", varOf("G"), whatever(), whatever()), varOf("G")) },
-                { ruleOf(structOf("not", varOf("X")), varOf("X"), atomOf("!"), Truth.fail()) },
-                { ruleOf(structOf("\\+", varOf("X")), varOf("X"), atomOf("!"), Truth.fail()) },
-                { ruleOf(structOf(";", varOf("A"), whatever()), varOf("A")) },
-                { ruleOf(structOf(";", whatever(), varOf("B")), varOf("B")) },
-                { ruleOf(structOf("->", varOf("A"), varOf("B")), structOf(";", structOf("\\+", varOf("A")), varOf("B"))) },
-                { factOf(structOf("member", varOf("H"), consOf(varOf("H"), whatever()))) },
-                { ruleOf(structOf("member", varOf("H"), consOf(whatever(), varOf("T"))), structOf("member", varOf("H"), varOf("T"))) }
+                { ruleOf(structOf("catch", varOf("G"), whatever(), whatever()), varOf("G")) }
         ),
-        primitives = sequenceOf(
+        primitives = CommonBuiltins.primitives + sequenceOf(
                 GreaterThan,
                 GreaterThanOrEqualsTo,
                 LowerThan,
---
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/libraries/stdlib/CommonBuiltins.kt
@@ -0,0 +1,22 @@
+package it.unibo.tuprolog.libraries.stdlib
+
+import it.unibo.tuprolog.core.operators.OperatorSet
+import it.unibo.tuprolog.libraries.Library
+import it.unibo.tuprolog.libraries.LibraryAliased
+
+object CommonBuiltins : LibraryAliased by Library.of(
+        alias = "prolog.lang",
+        operatorSet = OperatorSet.DEFAULT,
+        theory = CommonRules,
+        primitives = sequenceOf(
+                GreaterThan,
+                GreaterThanOrEqualsTo,
+                LowerThan,
+                LowerThanOrEqualsTo,
+                UnifiesWith,
+                NotUnifiableWith,
+                EqualsTo,
+                DifferentFrom,
+                Natural
+        ).map { it.descriptionPair }.toMap()
+)
