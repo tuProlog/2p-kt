0c017633071cd1ecb1e2e6f59b763396144d03e1
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-10-24 16:15:18 +0200

    Added static and dynamic check template methods to ExpressionEvaluator

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/primitive/function/ExpressionEvaluator.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/primitive/function/ExpressionEvaluator.kt
@@ -16,12 +16,15 @@ import it.unibo.tuprolog.solve.ExecutionContext
  *
  * @author Enrico
  */
-open class ExpressionEvaluator(protected val context: ExecutionContext) : TermVisitor<Term> {
+open class ExpressionEvaluator(private val context: ExecutionContext) : TermVisitor<Term> {
 
     /** Shorthand to access context loaded functions */
     protected val loadedFunctions by lazy { context.libraries.functions }
 
     override fun defaultValue(term: Term): Term = term
+
+    override fun visit(term: Term): Term =
+            super.visit(term.apply { staticCheck(context) })
 
     override fun visitAtom(term: Atom): Term = loadedFunctions[term.extractSignature()].let {
         when (it) {
@@ -32,9 +35,30 @@ open class ExpressionEvaluator(protected val context: ExecutionContext) : TermVi
 
     override fun visitStruct(term: Struct): Term = loadedFunctions[term.extractSignature()].let {
         when (it) {
-            is UnaryFunction<*> -> it(term.args.first().accept(this), context)
-            is BinaryFunction<*> -> it(term.args.first().accept(this), term.args.last().accept(this), context)
+            is UnaryFunction<*> -> it(
+                    term.args.first().accept(this).apply { dynamicCheck(context) },
+                    context
+            )
+            is BinaryFunction<*> -> it(
+                    term.args.first().accept(this).apply { dynamicCheck(context) },
+                    term.args.last().accept(this).apply { dynamicCheck(context) },
+                    context
+            )
             else -> term
         }
     }
+
+    /**
+     * Template method to implement static checks, i.e. those checks that can be made before evaluating sub-expressions
+     *
+     * This is a stub implementation, that does nothing
+     */
+    protected open fun Term.staticCheck(context: ExecutionContext): Unit = Unit
+
+    /**
+     * Template method to implement dynamic checks, i.e. those checks that must be made after sub-expression evaluation, on its result
+     *
+     * * This is a stub implementation, that does nothing
+     */
+    protected open fun Term.dynamicCheck(context: ExecutionContext): Unit = Unit
 }
