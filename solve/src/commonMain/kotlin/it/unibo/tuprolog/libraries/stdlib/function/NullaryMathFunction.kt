bc847992941554e2ee1e44a72dc06c5a7e3ce748
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-10-29 16:52:46 +0100

    Modified some names in NullaryMathFunction (it is not yet said that will return a constant value)

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/libraries/stdlib/function/NullaryMathFunction.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/libraries/stdlib/function/NullaryMathFunction.kt
@@ -7,16 +7,16 @@ import it.unibo.tuprolog.primitive.function.PrologFunction
 import it.unibo.tuprolog.solve.ExecutionContext
 
 /**
- * Base class to implement nullary math functions (so called math constants)
+ * Base class to implement nullary math functions (with no arguments)
  *
- * @param name the math constant name
+ * @param name the no arguments function name
  *
  * @author Enrico
  */
 abstract class NullaryMathFunction(name: String) : MathFunction(name, 0) {
 
-    override val function: NullaryFunction<Term> = PrologFunction.ofNullary { context -> mathConstant(context) }
+    override val function: NullaryFunction<Term> = PrologFunction.ofNullary { context -> mathFunction(context) }
 
-    /** The actual math constant implementation */
-    protected abstract fun mathConstant(context: ExecutionContext): Numeric
+    /** The actual function implementation */
+    protected abstract fun mathFunction(context: ExecutionContext): Numeric
 }
