c222ef9a3059db9ea9c2fe1b0d3fe68cd04c340d
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-11-07 13:15:32 +0100

    Added a DSL theory creation method variant in PrologWithTheories

--- dsl-theory/src/commonMain/kotlin/it/unibo/tuprolog/dsl/theory/PrologWithTheories.kt
+++ dsl-theory/src/commonMain/kotlin/it/unibo/tuprolog/dsl/theory/PrologWithTheories.kt
@@ -1,16 +1,21 @@
 package it.unibo.tuprolog.dsl.theory
 
-import it.unibo.tuprolog.core.*
+import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.dsl.Prolog
 import it.unibo.tuprolog.dsl.unify.PrologWithUnification
 import it.unibo.tuprolog.theory.ClauseDatabase
-import it.unibo.tuprolog.unify.Unification
 
 interface PrologWithTheories : PrologWithUnification {
 
     fun theoryOf(vararg clause: Clause): ClauseDatabase {
         return ClauseDatabase.of(*clause)
     }
+
+    fun theory(vararg clauseFunctions: Prolog.() -> Any): ClauseDatabase = theoryOf(
+            *clauseFunctions.map { function ->
+                Prolog.empty().function().let { clause { it } }
+            }.toTypedArray()
+    )
 
     companion object {
         fun empty(): PrologWithTheories = PrologWithTheoriesImpl()
