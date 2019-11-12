19b8c011db4ddaf4e954da3a1fca1c476065aad4
Giovanni <giovanni.ciatto@unibo.it>
2019-11-12 17:56:44 +0100

    settings.gradle is not kts

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "2p"
+
+include("core")
+include("unify")
+include("theory")
+include("dsl-core")
+include("dsl-unify")
+include("dsl-theory")
+include("solve")
+include("solve-classic")
+include("solve-streams")
+include("solve-test")
