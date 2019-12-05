bbede07e2b890a548562b64673d423ecef23de02
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-12-05 15:42:17 +0100

    Added 8-Queens benchmarks for solve-classic and solve-streams

--- solve-classic/src/jvmTest/kotlin/TuPrologBenchmark.kt
+++ solve-classic/src/jvmTest/kotlin/TuPrologBenchmark.kt
@@ -1,5 +1,6 @@
 import alice.tuprolog.Prolog
 import alice.tuprolog.Theory
+import it.unibo.tuprolog.solve.TimeInstant
 import it.unibo.tuprolog.solve.currentTimeInstant
 import org.junit.Ignore
 import org.junit.Test
@@ -14,52 +15,62 @@ internal class TuPrologBenchmark {
     @Test
     @Ignore
     fun `8-QueensBenchmark`() {
-        val numberOfExecutions = 20
+        val queensTheory = Theory(
+            """
+                top:-queens(8,Qs),fail.
+                top.
+
+                queens(N,Qs) :-
+                    range(1,N,Ns),
+                    queens(Ns,[],Qs).
+
+                queens([],Qs,Qs).
+                queens(UnplacedQs,SafeQs,Qs) :-
+                    select(UnplacedQs,UnplacedQs1,Q),
+                    not_attack(SafeQs,Q),
+                    queens(UnplacedQs1,[Q|SafeQs],Qs).
+
+                not_attack(Xs,X) :-
+                    not_attack(Xs,X,1).
+
+                not_attack([],_,_) :- !.
+                not_attack([Y|Ys],X,N) :-
+                    X =\= Y+N, X =\= Y-N,
+                    N1 is N+1,
+                    not_attack(Ys,X,N1).
+
+                select([X|Xs],Xs,X).
+                select([Y|Ys],[Y|Zs],X) :- select(Ys,Zs,X).
+
+                range(N,N,[N]) :- !.
+                range(M,N,[M|Ns]) :-
+                    M < N,
+                    M1 is M+1,
+                    range(M1,N,Ns).
+                """.trimIndent()
+        )
+
+        val numberOfExecutions = 10 // it will take approximately 5s
+//        val numberOfExecutions = 50 // it will take approximately 21s
+//        val numberOfExecutions = 100 // it will take approximately 42s
+//        val numberOfExecutions = 1000 // it will take approximately 6m 3s
+
         val timeList = mutableListOf<Long>()
 
-        for (i in 1..numberOfExecutions) {
-            val engine = Prolog()
-            engine.theory = Theory(
-                """
-                    top:-queens(8,Qs),fail.
-                    top.
-    
-                    queens(N,Qs) :-
-                        range(1,N,Ns),
-                        queens(Ns,[],Qs).
-    
-                    queens([],Qs,Qs).
-                    queens(UnplacedQs,SafeQs,Qs) :-
-                        select(UnplacedQs,UnplacedQs1,Q),
-                        not_attack(SafeQs,Q),
-                        queens(UnplacedQs1,[Q|SafeQs],Qs).
-    
-                    not_attack(Xs,X) :-
-                        not_attack(Xs,X,1).
-    
-                    not_attack([],_,_) :- !.
-                    not_attack([Y|Ys],X,N) :-
-                        X =\= Y+N, X =\= Y-N,
-                        N1 is N+1,
-                        not_attack(Ys,X,N1).
-    
-                    select([X|Xs],Xs,X).
-                    select([Y|Ys],[Y|Zs],X) :- select(Ys,Zs,X).
-    
-                    range(N,N,[N]) :- !.
-                    range(M,N,[M|Ns]) :-
-                        M < N,
-                        M1 is M+1,
-                        range(M1,N,Ns).
-                """.trimIndent()
-            )
+        var engine: Prolog
+        var initialTime: TimeInstant
 
+        repeat(numberOfExecutions) {
+            println(it)
 
-            val initialTime: Long = currentTimeInstant()
+            engine = Prolog()
+            engine.theory = queensTheory
+
+            initialTime = currentTimeInstant()
             engine.solve("top.")
             timeList.add(currentTimeInstant() - initialTime)
-
-            if (timeList.size == numberOfExecutions) println("8-Queens: ${timeList.sum() / timeList.size} ms to solve in an average of $numberOfExecutions executions")
         }
+
+        println("\n8-Queens: ${timeList.sum() / timeList.size} ms to solve in an average of $numberOfExecutions executions")
     }
 }
---
+++ solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/benchmark/StreamsSolverBenchmark.kt
@@ -0,0 +1,105 @@
+package it.unibo.tuprolog.solve.benchmark
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.libraries.Libraries
+import it.unibo.tuprolog.libraries.stdlib.DefaultBuiltins
+import it.unibo.tuprolog.solve.*
+import it.unibo.tuprolog.theory.ClauseDatabase
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+/**
+ * Benchmark class for [StreamsSolver] implementation
+ *
+ * @author Enrico
+ */
+internal class StreamsSolverBenchmark : SolverFactory {
+
+    override val defaultLibraries: Libraries = Libraries(DefaultBuiltins)
+
+    override fun solverOf(
+        libraries: Libraries,
+        flags: PrologFlags,
+        staticKB: ClauseDatabase,
+        dynamicKB: ClauseDatabase
+    ): Solver = StreamsSolver(libraries, flags, staticKB, dynamicKB)
+
+    @Test
+    @Ignore
+    fun `8-QueensBenchmark`() {
+        val queensDB = prolog {
+            theory(
+                { "top" `if` ("queens"(8, "Qs") and false) },
+                { "top" },
+
+                {
+                    "queens"("N", "Qs") `if` (
+                            "range"(1, "N", "Ns") and
+                                    "queens"("Ns", "[]", "Qs")
+                            )
+                },
+
+                { "queens"("[]", "Qs", "Qs") },
+                {
+                    "queens"("UnplacedQs", "SafeQs", "Qs") `if` (
+                            "select"("UnplacedQs", "UnplacedQs1", "Q") and
+                                    "not_attack"("SafeQs", "Q") and
+                                    "queens"(
+                                        "UnplacedQs1",
+                                        listFrom(kotlin.collections.listOf(varOf("Q")), varOf("SafeQs")),
+                                        "Qs"
+                                    )
+                            )
+                },
+
+                { "not_attack"("Xs", "X") `if` "not_attack"("Xs", "X", 1) },
+
+                { "not_attack"("[]", `_`, `_`) `if` "!" },
+                {
+                    "not_attack"(listFrom(kotlin.collections.listOf(varOf("Y")), varOf("Ys")), "X", "N") `if` (
+                            "=\\="("X", varOf("Y") + "N") and
+                                    "=\\="("X", varOf("Y") - "N") and
+                                    ("N1" `is` (varOf("N") + 1)) and
+                                    "not_attack"("Ys", "X", "N1")
+                            )
+                },
+
+                { "select"(listFrom(kotlin.collections.listOf(varOf("X")), varOf("Xs")), "Xs", "X") },
+                {
+                    "select"(
+                        listFrom(kotlin.collections.listOf(varOf("Y")), varOf("Ys")),
+                        listFrom(kotlin.collections.listOf(varOf("Y")), varOf("Zs")),
+                        "X"
+                    ) `if` "select"("Ys", "Zs", "X")
+                },
+
+                { ("range"("N", "N", listOf("N")) `if` "!") },
+                {
+                    ("range"("M", "N", listFrom(kotlin.collections.listOf(varOf("M")), varOf("Ns"))) `if` (
+                            "<"("M", "N") and
+                                    ("M1" `is` (varOf("M") + 1)) and
+                                    "range"("M1", "N", "Ns")
+                            ))
+                }
+            )
+        }
+
+        val numberOfExecutions = 10 // it will take approximately 3m 40s
+        val timeList = mutableListOf<Long>()
+
+        var solver: Solver
+        var initialTime: TimeInstant
+
+        repeat(numberOfExecutions) {
+            println(it)
+            solver = solverOf(staticKB = queensDB)
+
+            initialTime = currentTimeInstant()
+            solver.solve { atomOf("top") }.toList()
+            timeList.add(currentTimeInstant() - initialTime)
+        }
+
+        println("\n8-Queens: ${timeList.sum() / timeList.size} ms to solve in an average of $numberOfExecutions executions")
+    }
+
+}
