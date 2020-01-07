a3853c7f02d608ccda25671b2cc06d9373d4b720
Giovanni <giovanni.ciatto@unibo.it>
2020-01-07 16:43:38 +0100

    setup gitlab pages, attempt 1

--- documentation/build.gradle.kts
+++ documentation/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     orchidRuntimeOnly("io.github.javaeden.orchid:OrchidDocs:0.18.1")
     orchidRuntimeOnly("io.github.javaeden.orchid:OrchidKotlindoc:0.18.1")
     orchidRuntimeOnly("io.github.javaeden.orchid:OrchidPluginDocs:0.18.1")
-    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidGitlab:0.18.1")
+//    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidGitlab:0.18.1")
 }
 
 repositories {
@@ -14,21 +14,21 @@ repositories {
     maven("https://kotlin.bintray.com/kotlinx/")
 }
 
-fun getPropertyOrWarnForAbsence(key: String): String? {
-    val value = property(key)?.toString()
-    if (value.isNullOrBlank()) {
-        System.err.println("WARNING: $key is not set")
-    }
-    return value
-}
-
-// env ORG_GRADLE_PROJECT_signingKey
-val gitlabApiKey = getPropertyOrWarnForAbsence("signingKey")
+//fun getPropertyOrWarnForAbsence(key: String): String? {
+//    val value = property(key)?.toString()
+//    if (value.isNullOrBlank()) {
+//        System.err.println("WARNING: $key is not set")
+//    }
+//    return value
+//}
+//
+//// env ORG_GRADLE_PROJECT_signingKey
+//val gitlabApiKey = getPropertyOrWarnForAbsence("signingKey")
 
 orchid {
     theme = "Editorial"
 //    baseUrl = "http://username.github.io/project"
     version = rootProject.version.toString()
     args = listOf("--experimentalSourceDoc")
-    gitlabToken = gitlabApiKey
+//    gitlabToken = gitlabApiKey
 }
