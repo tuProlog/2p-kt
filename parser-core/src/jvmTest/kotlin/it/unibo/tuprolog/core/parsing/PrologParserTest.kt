27747207dfbff715eb32cbd8b87dde605481937a
Paolo Verdini <paolo.verdini@studio.unibo.it>
2020-01-21 14:40:11 +0100

    - Implementing jvmTest in parser-core

--- parser-core/src/jvmTest/kotlin/it/unibo/tuprolog/core/parsing/PrologParserTest.kt
+++ parser-core/src/jvmTest/kotlin/it/unibo/tuprolog/core/parsing/PrologParserTest.kt
@@ -1,6 +1,33 @@
 package it.unibo.tuprolog.core.parsing
 
+import it.unibo.tuprolog.core.Scope
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.operators.OperatorSet
+import org.junit.Assert
+
 interface PrologParserTest {
 
+    val scope: Scope
+        get() = Scope.empty()
 
+    fun assertEquals(x: Term, y: Term){
+        Assert.assertTrue("Failing assertion: <$x> == <$y>",x.structurallyEquals(y))
+    }
+
+    fun <T> assertEquals(x: T, y: T){
+        Assert.assertEquals("Failing assertion: <$x> == <$y>",x,y)
+    }
+
+    fun getOperatorSet() = OperatorSet.EMPTY
+
+    fun parseTerm(string: String): Term{
+        val ops = getOperatorSet()
+        return if(ops!=null)
+            with(TermParser.withNoOperator()){
+                Term.parse(string,this.defaultOperatorSet)
+        } else
+            with(TermParserImpl(ops)){
+                Term.parse(string,this.defaultOperatorSet)
+        }
+    }
 }
