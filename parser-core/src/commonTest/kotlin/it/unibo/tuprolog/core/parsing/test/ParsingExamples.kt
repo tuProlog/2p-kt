311658f6a8b22c3e10de07f0f682bb5903800974
Paolo Verdini <paolo.verdini@studio.unibo.it>
2020-01-23 10:49:29 +0100

    - adjust parentheses and format

--- parser-core/src/commonTest/kotlin/it/unibo/tuprolog/core/parsing/test/ParsingExamples.kt
+++ parser-core/src/commonTest/kotlin/it/unibo/tuprolog/core/parsing/test/ParsingExamples.kt
@@ -56,12 +56,12 @@ object ParsingExamples {
             "last_but_one"("X",listOf("X",`_`))
         },
         "last_but_one(X,[_,Y|Ys]) :- last_but_one(X,[Y|Ys])" to prolog {
-            "last_but_one"("X",consOf( `_`, consOf("Y","Ys")) ) impliedBy
+            "last_but_one"("X",consOf( `_`, consOf("Y","Ys"))) impliedBy
                     "last_but_one"("X",consOf("Y","Ys"))
         },
         "element_at(X,[_|L],K) :- K > 1, K1 is K - 1, element_at(X,L,K1)" to prolog {
             "element_at"("X",consOf(`_`,"L"),"K") impliedBy
-                    ( ("K" greaterThan 1) and  (("K1" `is` ("K" - 1)) and  ("element_at"("X","L","K1"))))
+                    (("K" greaterThan 1) and  (("K1" `is` ("K" - 1)) and  ("element_at"("X","L","K1"))))
         },
         "my_length([],0)" to prolog {
             "my_length"(emptyList(),0)
@@ -110,11 +110,11 @@ object ParsingExamples {
         },
         "count(X,[Y|Ys],[Y|Ys],N,[N,X]) :- N > 1, X \\= Y" to prolog {
             "count"("X", consOf("Y","Ys"),consOf("Y","Ys"),"N",consOf("N","X")) impliedBy
-                    ( ("N" greaterThan 1) and ( "\\="("X","Y") ))
+                    (("N" greaterThan 1) and ( "\\="("X","Y")))
         },
         "count(X,[X|Xs],Ys,K,T) :- K1 is K + 1, count(X,Xs,Ys,K1,T)" to prolog {
             "count"("X",consOf("X","Xs"),"Ys","K","T") impliedBy
-                    ( ("K1" `is` ("K".toTerm() + 1)) and ( "count"("X","Xs","Ys","K1","T")))
+                    (("K1" `is` ("K".toTerm() + 1)) and ( "count"("X","Xs","Ys","K1","T")))
         },
         "map_upper_bound(XMax, YMax) :- map_size(XSize, YSize), XMax is XSize - 1, YMax is YSize - 1" to prolog {
             "map_upper_bound"("XMax","YMax") impliedBy
@@ -122,25 +122,25 @@ object ParsingExamples {
         },
         "in_map(X, Y) :- X >= 0, Y >= 0, map_size(XSize, YSize), X < XSize, Y < YSize" to prolog {
             "in_map"("X","Y") impliedBy
-                    ( ("X" greaterThanOrEqualsTo 0) and (("Y" greaterThanOrEqualsTo 0) and ( "map_size"("XSize","YSize") and ( ("X" lowerThan ("XSize")) and ("Y" lowerThan "YSize")))))
+                    (("X" greaterThanOrEqualsTo 0) and (("Y" greaterThanOrEqualsTo 0) and ( "map_size"("XSize","YSize") and (("X" lowerThan ("XSize")) and ("Y" lowerThan "YSize")))))
         },
         "tile(wall, X, Y) :- \\+ in_map(X, Y)" to prolog {
             "tile"("wall","X","Y") impliedBy
-                    ( "\\+"("in_map"("X","Y")) )
+                    ( "\\+"("in_map"("X","Y")))
         },
         "draw_char(X, Y) :- tty_size(_, XSize), X >= XSize, NY is Y + 1, draw_char(0, NY)" to prolog {
             "draw_char"("X","Y") impliedBy
-                    ( "tty_size"(`_`,"XSize") and ( ("X" greaterThanOrEqualsTo "XSize") and ( ("NY" `is` ("Y".toTerm() + 1))  and ("draw_char"(0,"NY"))) ))
+                    ( "tty_size"(`_`,"XSize") and (("X" greaterThanOrEqualsTo "XSize") and (("NY" `is` ("Y".toTerm() + 1))  and ("draw_char"(0,"NY")))))
         },
         "Y < YMsgs -> write(' ') ; display_offset(XOff, YOff), XMap is X + XOff, YMap is Y + YOff, get_character(XMap, YMap, C), format('~s', [C])" to prolog {
             (("Y" lowerThan "YMsgs") then "write"(" ")) or
-                    ( "display_offset"("XOff","YOff") and ( ("XMap" `is` ("X".toTerm() + "XOff")) and ( ("YMap" `is` ("Y".toTerm() + "YOff")) and
-                            ( "get_character"("XMap","YMap","C")  and ( "format"("~s",listOf("C")) )))))
+                    ( "display_offset"("XOff","YOff") and (("XMap" `is` ("X".toTerm() + "XOff")) and (("YMap" `is` ("Y".toTerm() + "YOff")) and
+                            ( "get_character"("XMap","YMap","C")  and ( "format"("~s",listOf("C")))))))
         },
         "display_offset(X, Y) :- player(XPos, YPos), tty_size(YSize, XSize), message_lines(YMsgs), X is XPos - floor(XSize / 2), Y is YPos - floor((YSize - YMsgs) / 2)" to prolog {
             "display_offset"("X","Y") impliedBy
                     ( "player"("XPos","YPos") and ( "tty_size"("YSize","XSize")and ( "message_lines"("YMsgs")  and (
-                            ( "X" `is` ("XPos" - "floor"("XSize" / 2)))  and ( ("Y" `is` ("YPos" - "floor"(("YSize" - "YMsgs") / 2))))))))
+                            ( "X" `is` ("XPos" - "floor"("XSize" / 2)))  and (("Y" `is` ("YPos" - "floor"(("YSize" - "YMsgs") / 2))))))))
         }
     )
 
