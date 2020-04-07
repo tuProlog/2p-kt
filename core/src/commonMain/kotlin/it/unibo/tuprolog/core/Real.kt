c874eefb0456923812b7b6bae0ab97cd524f7ce9
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-08 00:14:01 +0200

    JsNames for Real

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Real.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Real.kt
@@ -6,6 +6,7 @@ import it.unibo.tuprolog.core.RegexUtils.INT
 import it.unibo.tuprolog.core.impl.RealImpl
 import org.gciatto.kt.math.BigDecimal
 import org.gciatto.kt.math.BigInteger
+import kotlin.js.JsName
 import kotlin.jvm.JvmField
 import kotlin.jvm.JvmStatic
 
@@ -32,15 +33,19 @@ interface Real : Numeric {
         val REAL_REGEX_PATTERN = "^[+\\-]?(($INT$DEC$EXP?)|($INT$EXP)|($DEC$EXP?))$".toRegex()
 
         @JvmStatic
+        @JsName("ofBigDecimal")
         fun of(real: BigDecimal): Real = RealImpl(real)
 
         @JvmStatic
+        @JsName("ofDouble")
         fun of(real: Double): Real = of(BigDecimal.of(real))
 
         @JvmStatic
+        @JsName("ofFloat")
         fun of(real: Float): Real = of(BigDecimal.of(real))
 
         @JvmStatic
+        @JsName("parse")
         fun of(real: String): Real = of(BigDecimal.of(real))
     }
 }
