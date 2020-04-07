5d109d107de69b48346b8f775f20e591909af876
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-08 00:13:02 +0200

    JsNames for Integer

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Integer.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Integer.kt
@@ -3,6 +3,7 @@ package it.unibo.tuprolog.core
 import it.unibo.tuprolog.core.impl.IntegerImpl
 import org.gciatto.kt.math.BigDecimal
 import org.gciatto.kt.math.BigInteger
+import kotlin.js.JsName
 import kotlin.jvm.JvmField
 import kotlin.jvm.JvmStatic
 
@@ -29,24 +30,31 @@ interface Integer : Numeric {
         val INTEGER_REGEX_PATTERN = """^[+\-]?(0[xXbBoO])?[0-9A-Fa-f]+$""".toRegex()
 
         @JvmStatic
+        @JsName("ofBigInteger")
         fun of(integer: BigInteger): Integer = IntegerImpl(integer)
 
         @JvmStatic
+        @JsName("ofLong")
         fun of(integer: Long): Integer = of(BigInteger.of(integer))
 
         @JvmStatic
+        @JsName("ofInt")
         fun of(integer: Int): Integer = of(BigInteger.of(integer))
 
         @JvmStatic
+        @JsName("ofShort")
         fun of(integer: Short): Integer = of(BigInteger.of(integer.toLong()))
 
         @JvmStatic
+        @JsName("ofByte")
         fun of(integer: Byte): Integer = of(BigInteger.of(integer.toLong()))
 
         @JvmStatic
+        @JsName("parse")
         fun of(integer: String): Integer = of(BigInteger.of(integer))
 
         @JvmStatic
+        @JsName("parseRadix")
         fun of(integer: String, radix: Int): Integer = of(BigInteger.of(integer, radix))
     }
 }
