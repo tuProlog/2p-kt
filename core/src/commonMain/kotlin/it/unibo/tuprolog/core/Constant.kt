c5f4dd60cf51414a19025f41cae05dc4c6911888
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-07 23:37:38 +0200

    JsNames for Constant

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Constant.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Constant.kt
@@ -1,9 +1,12 @@
 package it.unibo.tuprolog.core
 
+import kotlin.js.JsName
+
 interface Constant : Term {
 
     override val isConstant: Boolean get() = true
 
+    @JsName("value")
     val value: Any
 
     /**
