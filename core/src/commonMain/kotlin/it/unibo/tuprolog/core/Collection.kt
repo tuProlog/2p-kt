e31bd80482646969e833e4e5c26bb07b9c121d66
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-07 23:57:47 +0200

    JsNames for Collection

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Collection.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Collection.kt
@@ -1,19 +1,28 @@
 package it.unibo.tuprolog.core
 
 import kotlin.collections.List
+import kotlin.js.JsName
 
 interface Collection : Struct {
+
+    @JsName("unfoldedSequence")
     val unfoldedSequence: Sequence<Term>
 
+    @JsName("unfoldedList")
     val unfoldedList: List<Term>
 
+    @JsName("unfoldedArray")
     val unfoldedArray: Array<Term>
 
+    @JsName("size")
     val size: Int
 
+    @JsName("toArray")
     fun toArray(): Array<Term>
 
+    @JsName("toList")
     fun toList(): List<Term>
 
+    @JsName("toSequence")
     fun toSequence(): Sequence<Term>
 }
