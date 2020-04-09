e5f3816b10a0fa388d60f868a9adfac2a56b4b29
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-10 00:05:47 +0200

    js names for Indicator

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Indicator.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Indicator.kt
@@ -1,8 +1,7 @@
 package it.unibo.tuprolog.core
 
 import it.unibo.tuprolog.core.impl.IndicatorImpl
-import org.gciatto.kt.math.BigInteger
-import kotlin.jvm.JvmField
+import kotlin.js.JsName
 import kotlin.jvm.JvmStatic
 
 /**
@@ -30,9 +29,11 @@ interface Indicator : Struct {
         get() = 2
 
     /** The indicated functor name Term */
+    @JsName("nameTerm")
     val nameTerm: Term
 
     /** The indicated functor arity Term */
+    @JsName("arityTerm")
     val arityTerm: Term
 
     /**
@@ -42,16 +43,19 @@ interface Indicator : Struct {
      * - its [nameTerm] is an [Atom]
      * - its [arityTerm] is a non-negative [Integer]
      */
+    @JsName("isWellFormed")
     val isWellFormed: Boolean
         get() = nameTerm is Atom &&
                 arityTerm is Integer &&
                 arityTerm.`as`<Integer>().intValue.signum >= 0
 
     /** The indicated functor name, if well-formed */
+    @JsName("indicatedName")
     val indicatedName: String?
         get() = (nameTerm as? Atom)?.value
 
     /** The indicated functor arity, if well-formed */
+    @JsName("indicatedArity")
     val indicatedArity: Int?
         get() = (arityTerm as? Integer)?.intValue?.toInt()?.takeIf { it >= 0 }
 
@@ -70,10 +74,12 @@ interface Indicator : Struct {
 
         /** Creates an indicator denoting functor named [name] with [arity] */
         @JvmStatic
+        @JsName("of")
         fun of(name: Term, arity: Term): Indicator = IndicatorImpl(name, arity)
 
         /** Creates an indicator denoting functor named [name] with [arity] */
         @JvmStatic
+        @JsName("ofString")
         fun of(name: String, arity: Int): Indicator = of(Atom.of(name), Integer.of(arity))
     }
 }
