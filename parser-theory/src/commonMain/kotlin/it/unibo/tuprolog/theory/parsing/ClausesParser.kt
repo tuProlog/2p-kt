5a2b59dbe57e1e71b84aa71398395e3163dc5177
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-10 00:57:28 +0200

    js names for parser-theory

--- parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParser.kt
+++ parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParser.kt
@@ -4,42 +4,55 @@ import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.operators.Operator
 import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.theory.ClauseDatabase
+import kotlin.js.JsName
 import kotlin.jvm.JvmStatic
 
 interface ClausesParser {
+    @JsName("defaultOperatorSet")
     val defaultOperatorSet: OperatorSet
 
+    @JsName("parseClauseDatabaseWithOperators")
     fun parseClauseDatabase(input: String, operators: OperatorSet): ClauseDatabase =
         ClauseDatabase.of(parseClausesLazily(input, operators))
 
+    @JsName("parseClauseDatabase")
     fun parseClauseDatabase(input: String): ClauseDatabase =
         parseClauseDatabase(input, defaultOperatorSet)
 
+    @JsName("parseClausesLazilyWithOperators")
     fun parseClausesLazily(input: String, operators: OperatorSet): Sequence<Clause>
 
+    @JsName("parseClausesLazily")
     fun parseClausesLazily(input: String): Sequence<Clause> =
         parseClausesLazily(input, defaultOperatorSet)
 
+    @JsName("parseClausesWithOperators")
     fun parseClauses(input: String, operators: OperatorSet): List<Clause> =
         parseClausesLazily(input, operators).toList()
 
+    @JsName("parseClauses")
     fun parseClauses(input: String): List<Clause> =
         parseClauses(input, defaultOperatorSet)
 
     companion object {
         @JvmStatic
+        @JsName("withNoOperator")
         val withNoOperator = withOperators()
 
         @JvmStatic
+        @JsName("withStandardOperators")
         val withStandardOperators = withOperators(OperatorSet.STANDARD)
 
         @JvmStatic
+        @JsName("withDefaultOperators")
         val withDefaultOperators = withOperators(OperatorSet.DEFAULT)
 
         @JvmStatic
+        @JsName("withOperatorSet")
         fun withOperators(operators: OperatorSet) = clausesParserWithOperators(operators)
 
         @JvmStatic
+        @JsName("withOperators")
         fun withOperators(vararg operators: Operator) = withOperators(OperatorSet(*operators))
     }
 }
--- parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParserExtensions.kt
+++ parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParserExtensions.kt
@@ -4,14 +4,17 @@ package it.unibo.tuprolog.theory.parsing
 
 import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.theory.ClauseDatabase
+import kotlin.js.JsName
 import kotlin.jvm.JvmName
 
 internal expect fun clausesParserWithOperators(operators: OperatorSet): ClausesParser
 
 private val defaultParser = ClausesParser.withDefaultOperators
 
+@JsName("parseClauseDatabaseWithOperators")
 fun ClauseDatabase.Companion.parse(input: String, operators: OperatorSet): ClauseDatabase =
     defaultParser.parseClauseDatabase(input, operators)
 
+@JsName("parseClauseDatabase")
 fun ClauseDatabase.Companion.parse(input: String): ClauseDatabase =
     defaultParser.parseClauseDatabase(input)
