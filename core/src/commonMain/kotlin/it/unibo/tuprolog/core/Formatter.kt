dd1c2c03efb7c1a50ae533077b36ffa2b4d2d4b3
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-10 00:06:09 +0200

    js names for Formatter

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/Formatter.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/Formatter.kt
@@ -1,5 +1,7 @@
 package it.unibo.tuprolog.core
 
+import kotlin.js.JsName
+
 /**
  * A type for objects encapsulating some logic to convert any object of type [T] into a [String]
  * @param T is the type of the objects this formatter can handle
@@ -10,6 +12,7 @@ interface Formatter<T> {
      * @param value is the object to be converted in [String]
      * @return a [String] representing the object provided as argument
      */
+    @JsName("format")
     fun format(value: T): String
 }
 
