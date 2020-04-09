0a97d0c0fd6e78892bceb10f4624154b6cef1ed1
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-10 01:05:11 +0200

    more package methods in ClausesParserExtensions

--- parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParserExtensions.kt
+++ parser-theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/parsing/ClausesParserExtensions.kt
@@ -2,6 +2,7 @@
 
 package it.unibo.tuprolog.theory.parsing
 
+import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.theory.ClauseDatabase
 import kotlin.js.JsName
@@ -18,3 +19,27 @@ fun ClauseDatabase.Companion.parse(input: String, operators: OperatorSet): Claus
 @JsName("parseClauseDatabase")
 fun ClauseDatabase.Companion.parse(input: String): ClauseDatabase =
     defaultParser.parseClauseDatabase(input)
+
+@JsName("parseAsClauseDatabaseWithOperators")
+fun String.parseAsClauseDatabase(operators: OperatorSet): ClauseDatabase =
+    defaultParser.parseClauseDatabase(this, operators)
+
+@JsName("parseAsClauseDatabase")
+fun String.parseAsClauseDatabase(): ClauseDatabase =
+    defaultParser.parseClauseDatabase(this)
+
+@JsName("parseAsClausesWithOperators")
+fun String.parseAsClauses(operators: OperatorSet): List<Clause> =
+    defaultParser.parseClauses(this, operators)
+
+@JsName("parseAsClauses")
+fun String.parseClauses(): List<Clause> =
+    defaultParser.parseClauses(this)
+
+@JsName("parseAsClausesLazilyWithOperators")
+fun String.parseAsClausesLazily(operators: OperatorSet): Sequence<Clause> =
+    defaultParser.parseClausesLazily(this, operators)
+
+@JsName("parseAsClausesLazily")
+fun String.parseClausesLazily(): Sequence<Clause> =
+    defaultParser.parseClausesLazily(this)
