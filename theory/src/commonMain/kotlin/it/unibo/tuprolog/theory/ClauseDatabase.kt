ebec7bcc78ee73118ff67bd9b7142ef13f3bbb46
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-13 16:56:24 +0200

    js names for :theory

--- theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/ClauseDatabase.kt
+++ theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/ClauseDatabase.kt
@@ -1,18 +1,22 @@
 package it.unibo.tuprolog.theory
 
 import it.unibo.tuprolog.core.*
+import kotlin.js.JsName
 import kotlin.jvm.JvmStatic
 
 interface ClauseDatabase : Iterable<Clause> {
 
     /** All [Clause]s in this database */
+    @JsName("clauses")
     val clauses: Iterable<Clause>
 
     /** Only [clauses] that are [Rule]s */
+    @JsName("rules")
     val rules: Iterable<Rule>
         get() = clauses.filterIsInstance<Rule>()
 
     /** Only [clauses] that are [Directive]s */
+    @JsName("directives")
     val directives: Iterable<Directive>
         get() = clauses.filterIsInstance<Directive>()
 
@@ -41,52 +45,66 @@ interface ClauseDatabase : Iterable<Clause> {
     operator fun get(indicator: Indicator): Sequence<Rule>
 
     /** Adds given clause before all other clauses in this database */
+    @JsName("assertA")
     fun assertA(clause: Clause): ClauseDatabase
 
     /** Adds given clause before all other clauses in this database */
+    @JsName("assertAFact")
     fun assertA(struct: Struct): ClauseDatabase = assertA(Fact.of(struct))
 
     /** Adds given clause after all other clauses in this database */
+    @JsName("assertZ")
     fun assertZ(clause: Clause): ClauseDatabase
 
     /** Adds given clause after all other clauses in this database */
+    @JsName("assertZFact")
     fun assertZ(struct: Struct): ClauseDatabase = assertZ(Fact.of(struct))
 
     /** Tries to delete a matching clause from this database */
+    @JsName("retract")
     fun retract(clause: Clause): RetractResult
 
     /** Tries to delete a matching clause from this database */
+    @JsName("retractByHead")
     fun retract(head: Struct): RetractResult = retract(Rule.of(head, Var.anonymous()))
 
     /** Tries to delete all matching clauses from this database */
+    @JsName("retractAll")
     fun retractAll(clause: Clause): RetractResult
 
     /** Tries to delete all matching clauses from this database */
+    @JsName("retractAllByHead")
     fun retractAll(head: Struct): RetractResult = retractAll(Rule.of(head, Var.anonymous()))
 
     /** An enhanced toString that prints the database in a Prolog program format, if [asPrologText] is `true` */
+    @JsName("toStringAsProlog")
     fun toString(asPrologText: Boolean): String
 
     companion object {
 
         /** Creates an empty ClauseDatabase */
         @JvmStatic
+        @JsName("empty")
         fun empty(): ClauseDatabase = of(emptySequence())
 
         /** Creates a ClauseDatabase with given clauses */
         @JvmStatic
+        @JsName("of")
         fun of(vararg clause: Clause): ClauseDatabase = of(clause.asIterable())
 
         /** Let developers easily create a ClauseDatabase programmatically while avoiding variables names clashing */
         @JvmStatic
+        @JsName("ofScopes")
         fun of(vararg clause: Scope.() -> Clause): ClauseDatabase = of(clause.map { Scope.empty(it) })
 
         /** Creates a ClauseDatabase with given clauses */
         @JvmStatic
+        @JsName("ofSequence")
         fun of(clauses: Sequence<Clause>): ClauseDatabase = of(clauses.asIterable())
 
         /** Creates a ClauseDatabase with given clauses */
         @JvmStatic
+        @JsName("ofIterable")
         fun of(clauses: Iterable<Clause>): ClauseDatabase = ClauseDatabaseImpl(clauses)
     }
 }
--- theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/RetractResult.kt
+++ theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/RetractResult.kt
@@ -1,19 +1,26 @@
 package it.unibo.tuprolog.theory
 
 import it.unibo.tuprolog.core.Clause
+import kotlin.js.JsName
 
 /** A result given after a "retract" operation */
 sealed class RetractResult {
 
     /** The result always present value, is the clause database resulting from the operation execution */
+    @JsName("clauseDatabase")
     abstract val clauseDatabase: ClauseDatabase
 
     /** A successful "retract" operation result, carrying the new [clauseDatabase] and removed [clauses] */
     // TODO use lists in place of Iterables
-    data class Success(override val clauseDatabase: ClauseDatabase, val clauses: Iterable<Clause>) : RetractResult() {
+    data class Success(
+        override val clauseDatabase: ClauseDatabase,
+        @JsName("clauses") val clauses: Iterable<Clause>
+    ) : RetractResult() {
 
         /** Gets the first successfully retracted clause */
-        val firstClause: Clause get() = clauses.first()
+        @JsName("firstClause")
+        val firstClause: Clause
+            get() = clauses.first()
     }
 
     /** A failed "retract" operation result, carrying the unchanged [clauseDatabase] */
