c6e960234bf36b9708c27a2a322ca0bcf463f607
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-25 01:09:01 +0200

    missing override in DummyInstances.kt

--- test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/DummyInstances.kt
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/DummyInstances.kt
@@ -1,5 +1,11 @@
 package it.unibo.tuprolog.solve
 
+import it.unibo.tuprolog.solve.channel.InputChannel
+import it.unibo.tuprolog.solve.channel.OutputChannel
+import it.unibo.tuprolog.solve.exception.PrologWarning
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.theory.ClauseDatabase
+
 /**
  * Utils singleton that contains dummy instances, to be used when in a test something is not important
  *
@@ -18,5 +24,18 @@ object DummyInstances {
         override val outputChannels: Nothing by lazy { throw NotImplementedError() }
         override val substitution: Nothing by lazy { throw NotImplementedError() }
         override val prologStackTrace: Nothing by lazy { throw NotImplementedError() }
+
+        override fun createSolver(
+            libraries: Libraries,
+            flags: PrologFlags,
+            staticKb: ClauseDatabase,
+            dynamicKb: ClauseDatabase,
+            stdIn: InputChannel<String>,
+            stdOut: OutputChannel<String>,
+            stdErr: OutputChannel<String>,
+            warnings: OutputChannel<PrologWarning>
+        ): Solver {
+            throw NotImplementedError()
+        }
     }
 }
