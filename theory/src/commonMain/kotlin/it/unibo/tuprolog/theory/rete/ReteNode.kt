6fac4bf1d1a3b719cb8ffa8c0352519ac3f3dbd1
Manuel Bonarrigo <manuel.bonarrigo@studio.unibo.it>
2020-04-27 16:26:22 +0000

    Change generic type naming

--- theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/rete/ReteNode.kt
+++ theory/src/commonMain/kotlin/it/unibo/tuprolog/theory/rete/ReteNode.kt
@@ -1,41 +1,41 @@
 package it.unibo.tuprolog.theory.rete
 
 /** A class modeling a Rete Tree Node */
-internal interface ReteNode<Key, Element> {
+internal interface ReteNode<K, E> {
 
     /** All direct children of this node */
-    val children: MutableMap<Key, ReteNode<*, Element>>
+    val children: MutableMap<K, ReteNode<*, E>>
 
     /** All indexed elements by this node and its children */
-    val indexedElements: Sequence<Element>
+    val indexedElements: Sequence<E>
 
     /** Indexes a new element under this node */
-    fun put(element: Element, beforeOthers: Boolean)
+    fun put(element: E, beforeOthers: Boolean)
 
     /** Indexes a new element under this node, after the others */
-    fun put(element: Element) =
+    fun put(element: E) =
         put(element, false)
 
     /** Gets a sequence of matching elements starting from give one */
-    fun get(element: @UnsafeVariance Element): Sequence<Element>
+    fun get(element: @UnsafeVariance E): Sequence<E>
 
     /**
      * Removes elements matching given [element], respecting provided [limit], and returns them.
      * A negative limit will be considered as if there's no limit, i.e. all matching elements will be removed
      */
-    fun remove(element: @UnsafeVariance Element, limit: Int): Sequence<Element>
+    fun remove(element: @UnsafeVariance E, limit: Int): Sequence<E>
 
     /**
      * Removes one element matching given [element], and returns it;
      */
-    fun remove(element: @UnsafeVariance Element): Sequence<Element> =
+    fun remove(element: @UnsafeVariance E): Sequence<E> =
         remove(element, 1)
 
     /** Removes all elements matching given one */
-    fun removeAll(element: @UnsafeVariance Element): Sequence<Element>
+    fun removeAll(element: @UnsafeVariance E): Sequence<E>
 
     /** Creates a deep copy of the tree index; it doesn't copy indexed elements */
-    fun deepCopy(): ReteNode<Key, Element>
+    fun deepCopy(): ReteNode<K, E>
 
     /** An enhanced toString that can print nodes in Tree fashion */
     fun toString(treefy: Boolean): String
---
+++ theory/src/commonTest/kotlin/it/unibo/tuprolog/theory/ListedClauseIndexingTest.kt
@@ -0,0 +1,4 @@
+package it.unibo.tuprolog.theory
+
+class ListedClauseIndexingTest {
+}
