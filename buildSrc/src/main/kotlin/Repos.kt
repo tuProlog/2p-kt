03093b29e738b5be1707c34156719e3b7d6215ce
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-04-29 14:28:40 +0200

    use Google mirrors for maven central

---
+++ buildSrc/src/main/kotlin/Repos.kt
@@ -0,0 +1,8 @@
+object Repos {
+    val dokka = "https://dl.bintray.com/kotlin/dokka"
+    val kotlinx = "https://kotlin.bintray.com/kotlinx"
+
+    object Mirrors {
+        val mavenCentralEurope = "https://maven-central-eu.storage-download.googleapis.com/maven2"
+    }
+}
