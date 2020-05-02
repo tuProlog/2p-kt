35d09995e62ad0abb52957549656403ed2639d5c
Manuel Bonarrigo <manuel.bonarrigo@studio.unibo.it>
2020-05-02 16:09:39 +0000

    restored multiset capability for a retetree

--- theory/src/commonMain/kotlin/it.unibo.tuprolog.collections/rete/nodes/set/ArityNode.kt
+++ theory/src/commonMain/kotlin/it.unibo.tuprolog.collections/rete/nodes/set/ArityNode.kt
@@ -1,14 +1,16 @@
-package it.unibo.tuprolog.collections.rete.nodes.set
+package it.unibo.tuprolog.collections.rete.nodes.old
 
 import it.unibo.tuprolog.core.Rule
-import it.unibo.tuprolog.collections.rete.AbstractLeafReteNode
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.collections.rete.AbstractIntermediateReteNode
+import it.unibo.tuprolog.collections.rete.ReteNode
 import it.unibo.tuprolog.unify.Unificator.Companion.matches
 
 /** An intermediate node indexing by Rules head's arity */
 internal data class ArityNode(
     private val arity: Int,
-    override val leafElements: MutableList<Rule> = mutableListOf()
-) : AbstractLeafReteNode<Rule>() {
+    override val children: MutableMap<Term?, ReteNode<*, Rule>> = mutableMapOf()
+) : AbstractIntermediateReteNode<Term?, Rule>(children) {
 
     init {
         require(arity >= 0) { "ArityNode arity should be greater than or equal to 0" }
@@ -16,9 +18,51 @@ internal data class ArityNode(
 
     override val header = "Arity($arity)"
 
-    override fun get(element: Rule): Sequence<Rule> =
-        indexedElements.filter { it matches element }
+    override fun put(element: Rule, beforeOthers: Boolean) = when {
+        element.head.arity > 0 -> {
+            val headFirstArg = element.head[0]
+            val child = children.getOrElse(headFirstArg) {
+
+                children.retrieve<ArgNode> { head -> head != null && head structurallyEquals headFirstArg }
+                    .singleOrNull()
+            }
+
+            child ?: ArgNode(0, headFirstArg)
+                .also { children[headFirstArg] = it }
+        }
+
+        else -> children.getOrPut(null) { NoArgsNode() }
+
+    }.put(element, beforeOthers)
+
+    override fun selectChildren(element: Rule) = when {
+        element.head.arity > 0 ->
+            children.retrieve<ArgNode> { head -> head != null && head matches element.head[0] }
+
+        else -> sequenceOf(children[null])
+    }
+
+    override fun removeWithLimit(element: Rule, limit: Int): Sequence<Rule> =
+        selectChildren(element)
+            .foldWithLimit(mutableListOf<Rule>(), limit) { yetRemoved, currentChild ->
+                yetRemoved.also {
+                    it += currentChild?.remove(element, limit - it.count())
+                        ?: emptySequence()
+                }
+            }.asSequence()
+
+    // removeAll optimized implementation w.r.t. super class
+    override fun removeAll(element: Rule): Sequence<Rule> =
+        selectChildren(element)
+            .fold(mutableListOf<Rule>()) { yetRemoved, currentChild ->
+                yetRemoved.also {
+                    it += currentChild?.removeAll(element) ?: emptySequence()
+                }
+            }.asSequence()
 
     override fun deepCopy(): ArityNode =
-        ArityNode(arity, leafElements.toMutableList())
+        ArityNode(
+            arity,
+            children.deepCopy({ it }, { it.deepCopy() })
+        )
 }
