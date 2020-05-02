49e10c1d3eac5df8808348910cfc934382940961
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2020-05-02 18:01:49 +0200

    + unification infix methods example

---
+++ examples/src/main/kotlin/it/unibo/tuprolog/examples/UnificationInfixExample.kt
@@ -0,0 +1,17 @@
+package it.unibo.tuprolog.examples
+
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.unify.Unificator.Companion.matches
+import it.unibo.tuprolog.unify.Unificator.Companion.mguWith
+import it.unibo.tuprolog.unify.Unificator.Companion.unifyWith
+
+fun main() {
+    val term = Struct.of("father", Atom.of("abraham"), Atom.of("isaac"))
+    val template = Struct.of("father", Var.of("X"), Atom.of("isaac"))
+
+    println(term mguWith  template)     // {X_0=abraham}
+    println(term matches template)      // true
+    println(term unifyWith template)    // father(abraham, isaac)
+}
