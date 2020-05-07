81705569f0c941a5d9531d2f6e76f57f678cb273
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2020-05-07 17:38:22 +0200

    + unification fail example

--- examples/src/main/kotlin/it/unibo/tuprolog/examples/unify/UnificationFailExample.kt
+++ examples/src/main/kotlin/it/unibo/tuprolog/examples/unify/UnificationFailExample.kt
@@ -2,22 +2,21 @@ package it.unibo.tuprolog.examples.unify
 
 import it.unibo.tuprolog.core.Atom
 import it.unibo.tuprolog.core.Struct
-import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.core.Substitution
 import it.unibo.tuprolog.unify.Unificator
-
-// TODO: with/without occurs-check
 
 fun main() {
     val unificator = Unificator.default
 
     val term = Struct.of("father", Atom.of("abraham"), Atom.of("isaac"))
-    val template = Struct.of("father", Var.of("X"), Atom.of("isaac"))
+    val template = Struct.of("father", Atom.of("isaac"), Atom.of("abraham"))
 
     val substitution = unificator.mgu(term, template)
     val match = unificator.match(term, template)
     val unified = unificator.unify(term, template)
 
-    println(substitution) // {X_0=abraham}
-    println(match)        // true
-    println(unified)      // father(abraham, isaac)
+    println(substitution is Substitution.Fail)  // true
+    println(substitution.isFailed)              // true
+    println(match)                              // false
+    println(unified)                            // null
 }
