892e895f593ac33ca8ed7d6a578160a0ab667fe6
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-05-07 16:22:01 +0200

    more examples

--- examples/src/main/kotlin/it/unibo/tuprolog/examples/solve/SolverExample.kt
+++ examples/src/main/kotlin/it/unibo/tuprolog/examples/solve/SolverExample.kt
@@ -1,20 +1,59 @@
 package it.unibo.tuprolog.examples.solve
 
+import it.unibo.tuprolog.core.Numeric
+import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.dsl.theory.prolog
-import it.unibo.tuprolog.solve.Solution
-import it.unibo.tuprolog.solve.Solver
-import it.unibo.tuprolog.solve.classicWithDefaultBuiltins
+import it.unibo.tuprolog.solve.*
+import it.unibo.tuprolog.solve.exception.error.TypeError
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.solve.library.Library
+
+val gtSignature = Signature("gt", 2)
+
+fun gt(request: Solve.Request<ExecutionContext>): Sequence<Solve.Response> {
+    val arg1: Term = request.arguments[0]
+    val arg2: Term = request.arguments[1]
+
+    if (arg1 !is Numeric) {
+        throw TypeError.forGoal(
+            request.context,
+            request.signature,
+            TypeError.Expected.NUMBER,
+            arg1
+        )
+    }
+    if (arg2 !is Numeric) {
+        throw TypeError.forGoal(
+            request.context,
+            request.signature,
+            TypeError.Expected.NUMBER,
+            arg2
+        )
+    }
+
+    return if (arg1.castTo<Numeric>().decimalValue > arg2.castTo<Numeric>().decimalValue) {
+        sequenceOf(request.replySuccess())
+    } else {
+        sequenceOf(request.replyFail())
+    }
+}
 
 fun main() {
     prolog {
-        val solver =  Solver.classicWithDefaultBuiltins(
+        val solver = Solver.classicWithDefaultBuiltins(
+            libraries = Libraries(
+                Library.of(
+                    primitives = mapOf(gtSignature to ::gt),
+                    alias = "it.unibo.lrizzato.myprimives"
+                )
+            ),
             staticKb = theoryOf(
                 fact { "user"("giovanni") },
                 fact { "user"("lorenzo") },
                 rule { "user"(`_`) impliedBy fail() }
             )
         )
-        val query = "user"("X") and "write"("hello: ") and "write"("X") and "nl"
+        val query = "user"("X") and "write"("hello: ") and "write"("X") and "nl" and "gt"(2, 1)
         solver.solve(query).forEach {
             when (it) {
                 is Solution.No -> println("no.\n")
@@ -24,6 +63,12 @@ fun main() {
                         println("\t${assignment.key} / ${assignment.value}")
                     }
                     println()
+                }
+                is Solution.Halt -> {
+                    println("halt: ${it.exception.message}")
+                    for (err in it.exception.prologStackTrace) {
+                        println("\t $err")
+                    }
                 }
             }
         }
