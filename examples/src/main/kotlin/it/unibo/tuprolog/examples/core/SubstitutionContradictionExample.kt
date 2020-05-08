a5963b5b4728a05c0fb9449eccb394abd09b0895
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2020-05-08 17:53:37 +0200

    + substitution contradiction example

---
+++ examples/src/main/kotlin/it/unibo/tuprolog/examples/core/SubstitutionContradictionExample.kt
@@ -0,0 +1,24 @@
+package it.unibo.tuprolog.examples.core
+
+import it.unibo.tuprolog.core.Scope
+import it.unibo.tuprolog.core.Substitution
+
+/**
+ *
+ *
+ * @author Lorenzo
+ */
+fun main() {
+    Scope.empty {
+        val term = structOf("father", varOf("X"), atomOf("isaac"))
+
+        val sub1 = Substitution.of(varOf("X"), atomOf("abraham"))
+        val sub2 = Substitution.of(varOf("X"), atomOf("nahor"))
+
+        val substitution = sub1 + sub2 // contradiction!
+        val result = substitution.applyTo(term) // father(X_0, isaac) (substitution could not be performed)
+
+        println(substitution)
+        println(result)
+    }
+}
