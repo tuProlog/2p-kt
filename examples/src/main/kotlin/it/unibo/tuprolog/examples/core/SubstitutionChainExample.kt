59a2cdaef505b70807d6ab0745d0ac8aea4c528f
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2020-05-08 18:07:33 +0200

    + substitution chain example

---
+++ examples/src/main/kotlin/it/unibo/tuprolog/examples/core/SubstitutionChainExample.kt
@@ -0,0 +1,23 @@
+package it.unibo.tuprolog.examples.core
+
+import it.unibo.tuprolog.core.Scope
+import it.unibo.tuprolog.core.Substitution
+
+/**
+ *
+ *
+ * @author Lorenzo
+ */
+fun main() {
+    Scope.empty {
+        val substitution = Substitution.of(
+                varOf("X") to varOf("Y"),
+                varOf("Y") to varOf("Z")
+        )
+
+        val originalZ = substitution.getOriginal(varOf("Z"))
+
+        println(substitution)
+        println(originalZ)
+    }
+}
