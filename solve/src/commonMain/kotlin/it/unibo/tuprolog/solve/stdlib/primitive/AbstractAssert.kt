e5f7031ef9fa478df3d39623a68ebd6b3bda540a
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-06-08 10:19:50 +0200

    let asser*/1 accept rules

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/AbstractAssert.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/AbstractAssert.kt
@@ -1,5 +1,7 @@
 package it.unibo.tuprolog.solve.stdlib.primitive
 
+import it.unibo.tuprolog.core.Clause
+import it.unibo.tuprolog.core.Fact
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.solve.ExecutionContext
@@ -8,12 +10,13 @@ import it.unibo.tuprolog.solve.Solve
 abstract class AbstractAssert(suffix: String, private val before: Boolean) : SideEffect1<ExecutionContext>("assert$suffix") {
     override fun accept(request: Solve.Request<ExecutionContext>, term: Term): Solve.Response =
         with(request.ensuringArgumentIsStruct(0)) {
+            val clause = if (term is Clause) term else Fact.of(term as Struct)
             replySuccess(
                 dynamicKb = context.dynamicKb.let {
                     if (before) {
-                        it.assertA(term as Struct)
+                        it.assertA(clause)
                     } else {
-                        it.assertZ(term as Struct)
+                        it.assertZ(clause)
                     }
                 }
             )
