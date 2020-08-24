d20df3dcc8ad60617b443c6e5445b86d79144412
Davide Greco <davide.greco4@studio.unibo.it>
2020-08-24 17:22:20 +0200

    Arg Test implementation

---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestArg.kt
@@ -0,0 +1,157 @@
+package it.unibo.tuprolog.solve
+
+interface TestArg {
+    companion object{
+        fun prototype(solverFactory: SolverFactory): TestArg =
+                TestArgImpl(solverFactory)
+    }
+
+    /** A short test max duration */
+    val shortDuration: TimeDuration
+        get() = 250L
+
+    /** A medium test max duration */
+    val mediumDuration: TimeDuration
+        get() = 2 * shortDuration
+
+    /** A long test max duration */
+    val longDuration: TimeDuration
+        get() = 4 * mediumDuration
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,foo(a,b),a).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testArgFromFoo()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,foo(a,b),X).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `X` to `a`.
+     */
+    fun testArgFromFooX()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,foo(X,b),a).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `X` to `a`.
+     */
+    fun testArgFromFoo2()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(2,foo(a, f(X,b), c), f(a, Y)).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 2 solutions which binds variable `X` to `a` and variable `Y` to `b`.
+     */
+    fun testArgFromFooInF()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,foo(X,b),Y).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `X` to variable `Y`.
+     */
+    fun testArgFromFooY()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,foo(a,b),b).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testArgFromFooInSecondTerm()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(0,foo(a,b),foo).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testArgFromFooInFoo()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(3,foo(3,4),N).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testArgNumberFromFoo()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(X,foo(a,b),a).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception instantiation_error.
+     */
+    fun testArgXFromFoo()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(1,X,a).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception instantiation_error.
+     */
+    fun testArgNumberFromX()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(0,atom,A).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception type_error(compound, atom).
+     */
+    fun testArgFromAtom()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(0,3,A).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception type_error(compound, 3).
+     */
+    fun testArgFromNumber()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(-3,foo(a,b),A).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception domain_error(not_less_than_zero,-3).
+     */
+    fun testNegativeArgFromFoo()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- arg(a,foo(a,b),X).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     * producing exception type_error(integer,a).
+     */
+    fun testArgAFromFoo()
+
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestArgImpl.kt
@@ -0,0 +1,257 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.exception.error.DomainError
+import it.unibo.tuprolog.solve.exception.error.InstantiationError
+import it.unibo.tuprolog.solve.exception.error.TypeError
+import kotlin.collections.listOf as ktListOf
+
+internal class TestArgImpl(private val solverFactory: SolverFactory) : TestArg {
+
+    override fun testArgFromFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("a", "b"), "a")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.yes()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFooX() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("a", "b"), "X")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.yes("X" to "a")),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFoo2() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("X", "b"), "a")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.yes("X" to "a")),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFooInF() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(2, "foo"("a", "f"("X", "b"), "c"), "f"("a", "Y"))
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    with(query) {
+                        ktListOf(
+                                yes("X" to "a"),
+                                yes("Y" to "b")
+                        )
+                    },
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFooY() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("X", "b"), "Y")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.yes("Y" to "X")),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFooInSecondTerm() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("a", "b"), "b")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.no()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromFooInFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "foo"("a", "b"), "foo")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.no()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgNumberFromFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(3, "foo"(3, 4), "N")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.no()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgXFromFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"("X", "foo"("a", "b"), "a")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            InstantiationError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    varOf("X")
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgNumberFromX() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(1, "X", "a")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            InstantiationError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    varOf("X")
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromAtom() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(0, atom("X"), "A")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            TypeError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    TypeError.Expected.ATOM,
+                                    numOf(4)
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgFromNumber() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(0, 3, "A")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            TypeError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    TypeError.Expected.COMPOUND,
+                                    numOf(3)
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testNegativeArgFromFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"(-3, "foo"("a", "b"), "A")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            DomainError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    DomainError.Expected.NOT_LESS_THAN_ZERO,
+                                    numOf(-3)
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testArgAFromFoo() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = "arg"("a", "foo"("a", "b"), "X")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    ktListOf(query.halt(
+                            TypeError.forGoal(
+                                    DummyInstances.executionContext,
+                                    Signature("arg", 3),
+                                    TypeError.Expected.INTEGER,
+                                    numOf("a")
+                            )
+                    )
+                    ),
+                    solutions
+            )
+        }
+    }
+}
