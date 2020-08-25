d8a6c779ca45ae756553acbe8570019ed568f644
Davide Greco <davide.greco4@studio.unibo.it>
2020-08-25 12:06:12 +0200

    Unify test implementation

---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestUnify.kt
@@ -0,0 +1,125 @@
+package it.unibo.tuprolog.solve
+
+interface TestUnify {
+    companion object{
+        fun prototype(solverFactory: SolverFactory): TestUnify =
+                TestUnifyImpl(solverFactory)
+    }
+
+    /** A short test max duration */
+    val shortDuration: TimeDuration
+        get() = 250L
+
+    /** A medium test max duration */
+    val mediumDuration: TimeDuration
+        get() = 2 * shortDuration
+
+    /** A long test max duration */
+    val longDuration: TimeDuration
+        get() = 4 * mediumDuration
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- `=`(1,1).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testNumberUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- `=`(X,1).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `X` to the value `1`.
+     */
+    fun testNumberXUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- `=`(X,Y).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `Y` to the variable `X`.
+     */
+    fun testXYUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- ('='(X,Y),'='(X,abc)).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 2 solution which binds variable `X` to `abc` and the variable `Y` to `abc`.
+     */
+    fun testDoubleUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(f(X,def),f(def,Y)).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 2 solution which binds variable `X` to `abc` and the variable `Y` to `abc`.
+     */
+    fun testFDefUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(1,2).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testDiffNumberUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(1,1.0).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testDecNumberUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(g(X),f(f(X))).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testGUnifyFX()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(f(X,1),f(a(X))).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testFUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(f(X,Y,X),f(a(X),a(Y),Y,2)).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testFMultipleTermUnify()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- '='(f(A,B,C),f(g(B,B),g(C,C),g(D,D))).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 3 solutions which binds variable `A` to `g(g(g(D,D),g(D,D)),g(g(D,D),g(D,D)))`,
+     * the variable `B` to `g(g(D,D),g(D,D))` and the variable `C` to `g(D,D)`.
+     */
+    fun testMultipleTermUnify()
+}
