1d235256d122a8ff06dcffc21a45adc8a12a3ef8
Davide Greco <davide.greco4@studio.unibo.it>
2020-08-28 16:25:33 +0200

    Test Number implementation

---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestNumber.kt
@@ -0,0 +1,56 @@
+package it.unibo.tuprolog.solve
+
+/**
+ * Tests of number
+ */
+interface TestNumber : SolverTest{
+    companion object {
+        fun prototype(solverFactory: SolverFactory): TestNumber =
+            TestNumberImpl(solverFactory)
+    }
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- number(3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testBasicNum()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- number(3.3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testDecNum()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- number(-3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testNegNum()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- number(a).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testLetterNum()
+
+    /**
+     * Tests the query
+     * ```prolog
+     * ?- number(X).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testXNum()
+}
