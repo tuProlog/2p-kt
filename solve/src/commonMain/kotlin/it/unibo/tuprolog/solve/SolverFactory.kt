b6d68470861c41ac742f8ceab5b881333ca3ed8e
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-08-28 16:14:30 +0200

    make ad-hoc OutputChannel for warnings

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/SolverFactory.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/SolverFactory.kt
@@ -43,7 +43,7 @@ interface SolverFactory {
 
     @JsName("defaultWarningsChannel")
     val defaultWarningsChannel: OutputChannel<PrologWarning>
-        get() = OutputChannel.stdErr()
+        get() = OutputChannel.warn()
 
     @JsName("solver")
     fun solverOf(
--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/channel/Channels.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/channel/Channels.kt
@@ -1,7 +1,11 @@
 package it.unibo.tuprolog.solve.channel
 
+import it.unibo.tuprolog.solve.exception.PrologWarning
+
 internal expect fun stdin(): InputChannel<String>
 
 internal expect fun <T> stdout(): OutputChannel<T>
 
 internal expect fun <T> stderr(): OutputChannel<T>
+
+internal expect fun warning(): OutputChannel<PrologWarning>
