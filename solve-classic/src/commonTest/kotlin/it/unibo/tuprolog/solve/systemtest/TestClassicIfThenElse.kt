fda1f57def62f05374cb7544b61d0c7e845a0fc7
Davide Greco <davide.greco4@studio.unibo.it>
2020-08-29 16:16:22 +0200

    Test If-Then-Else implementation

---
+++ solve-classic/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestClassicIfThenElse.kt
@@ -0,0 +1,50 @@
+package it.unibo.tuprolog.solve.systemtest
+
+import it.unibo.tuprolog.solve.ClassicSolverFactory
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.TestIfThenElse
+import kotlin.test.Test
+
+class TestClassicIfThenElse : TestIfThenElse, SolverFactory by ClassicSolverFactory  {
+    private val prototype = TestIfThenElse.prototype(this)
+
+    @Test
+    override fun testIfTrueElseFail() {
+        prototype.testIfTrueElseFail()
+    }
+
+    @Test
+    override fun testIfFailElseTrue() {
+        prototype.testIfFailElseTrue()
+    }
+
+    @Test
+    override fun testIfTrueThenElseFail() {
+        prototype.testIfTrueThenElseFail()
+    }
+
+    @Test
+    override fun testIfFailElseFail() {
+        prototype.testIfFailElseFail()
+    }
+
+    @Test
+    override fun testIfXTrueElseX() {
+        prototype.testIfXTrueElseX()
+    }
+
+    @Test
+    override fun testIfFailElseX() {
+        prototype.testIfFailElseX()
+    }
+
+    @Test
+    override fun testIfThenElseOrWithDoubleSub() {
+        prototype.testIfThenElseOrWithDoubleSub()
+    }
+
+    @Test
+    override fun testIfOrElseTrue() {
+        prototype.testIfOrElseTrue()
+    }
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestIfThenElseImpl.kt
@@ -0,0 +1,122 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.dsl.theory.prolog
+
+internal class TestIfThenElseImpl(private val solverFactory: SolverFactory) : TestIfThenElse{
+    override fun testIfTrueElseFail() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(true, true), fail)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes()),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfFailElseTrue() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(fail, true), true)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes()),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfTrueThenElseFail() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(true, fail), fail)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.no()),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfFailElseFail() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(fail, true), fail)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.no()),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfXTrueElseX() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(true, ("X" `=` 1)), "X" `=` 2)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes("X" to 1)),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfFailElseX() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(fail, ("X" `=` 1)), "X" `=` 2)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes("X" to 2)),
+                solutions
+            )
+        }
+    }
+
+    override fun testIfThenElseOrWithDoubleSub() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(true,("X" `=` 1) or ("X" `=` 2)), true)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                with(query) {
+                    kotlin.collections.listOf(
+                        yes("X" to 1),
+                        yes("X" to 2)
+                    )
+                },
+                solutions
+            )
+        }
+    }
+
+    override fun testIfOrElseTrue() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ";"("->"(("X" `=` 1) or ("X" `=` 2), true), true)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes("X" to 1)),
+                solutions
+            )
+        }
+    }
+}
