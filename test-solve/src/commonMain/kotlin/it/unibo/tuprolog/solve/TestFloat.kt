88a521d6a6db1a5a56c3de4becbb1022407843ec
Davide Greco <davide.greco4@studio.unibo.it>
2020-08-31 10:07:37 +0200

    Test Float implementation

---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestFloat.kt
@@ -0,0 +1,53 @@
+package it.unibo.tuprolog.solve
+
+interface TestFloat : SolverTest {
+    companion object {
+        fun prototype(solverFactory: SolverFactory): TestFloat =
+            TestFloatImpl(solverFactory)
+    }
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- float(3.3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testFloatDec()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- float(-3.3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testFloatDecNeg()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- float(3).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testFloatNat()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- float(atom).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testFloatAtom()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- float(X).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testFloatX()
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestFloatImpl.kt
@@ -0,0 +1,76 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.dsl.theory.prolog
+
+internal class TestFloatImpl(private val solverFactory: SolverFactory) : TestFloat {
+    override fun testFloatDec() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = float(3.3)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes()),
+                solutions
+            )
+        }
+    }
+
+    override fun testFloatDecNeg() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = float(- 3.3)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.yes()),
+                solutions
+            )
+        }
+    }
+
+    override fun testFloatNat() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = float(3)
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.no()),
+                solutions
+            )
+        }
+    }
+
+    override fun testFloatAtom() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = float("atom")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.no()),
+                solutions
+            )
+        }
+    }
+
+    override fun testFloatX() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = float("X")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                kotlin.collections.listOf(query.no()),
+                solutions
+            )
+        }
+    }
+
+}
