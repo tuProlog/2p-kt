e7f1a3f2d7ca44756d563658e632df62dae8cdc8
Davide Greco <davide.greco4@studio.unibo.it>
2020-09-01 17:31:46 +0200

    Test Catch_And_Throw implementation

---
+++ solve-classic/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestClassicCatchAndThrow.kt
@@ -0,0 +1,20 @@
+package it.unibo.tuprolog.solve.systemtest
+
+import it.unibo.tuprolog.solve.ClassicSolverFactory
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.TestCatchAndThrow
+import kotlin.test.Test
+
+class TestClassicCatchAndThrow : TestCatchAndThrow, SolverFactory by ClassicSolverFactory  {
+    private val prototype = TestCatchAndThrow.prototype(this)
+
+    @Test
+    override fun testCatchThrow() {
+        prototype.testCatchThrow()
+    }
+
+    @Test
+    override fun testCatchFail() {
+        prototype.testCatchFail()
+    }
+}
