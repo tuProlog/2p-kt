cd9491f2fa16d312b01829528984beee833663be
Davide Greco <davide.greco4@studio.unibo.it>
2020-09-01 16:45:08 +0200

    Test Fail implementation

---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestFailImpl.kt
@@ -0,0 +1,73 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.exception.error.ExistenceError
+import it.unibo.tuprolog.solve.flags.Unknown
+
+internal class TestFailImpl(private val solverFactory: SolverFactory) : TestFail {
+    override fun testFail() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = fail
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    kotlin.collections.listOf(query.no()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testUndefPred() { //classic solver: undef_pred/1 instead of undef_pred/0
+                                   //streams solver: `No(query=undef_pred)` instead of undef_pred/0
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins(
+                    flags = FlagStore.of(Unknown to Unknown.ERROR)
+            )
+
+            val query = atomOf("undef_pred")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    kotlin.collections.listOf(
+                            query.halt(
+                                    ExistenceError.forProcedure(
+                                            DummyInstances.executionContext,
+                                            Signature("undef_pred", 1)
+                                    )
+                            )
+                    ),
+                    solutions
+            )
+        }
+    }
+
+    override fun testSetFlagFail() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ("set_prolog_flag"("unknown" and fail) and "undef_pred")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    kotlin.collections.listOf(query.no()),
+                    solutions
+            )
+        }
+    }
+
+    override fun testSetFlagWarning() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val query = ("set_prolog_flag"("unknown" and "warning") and "undef_pred")
+            val solutions = solver.solve(query, mediumDuration).toList()
+
+            assertSolutionEquals(
+                    kotlin.collections.listOf(query.no()),
+                    solutions
+            )
+        }
+    }
+}
