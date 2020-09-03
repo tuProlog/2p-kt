6ede53ea0c1491d67305674175988d29aaabada7
Davide Greco <davide.greco4@studio.unibo.it>
2020-09-03 18:33:35 +0200

    Test (unimplemented predicate)Copy_Term implementation with @Ignore

---
+++ solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestStreamsCopyTerm.kt
@@ -0,0 +1,65 @@
+package it.unibo.tuprolog.solve.systemtest
+
+import it.unibo.tuprolog.solve.StreamsSolverFactory
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.TestCopyTerm
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+class TestStreamsCopyTerm  : TestCopyTerm, SolverFactory by StreamsSolverFactory  {
+    private val prototype = TestCopyTerm.prototype(this)
+
+    @Test
+    @Ignore
+    override fun testCopyXNum() {
+        prototype.testCopyXNum()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyAnyA() {
+        prototype.testCopyAnyA()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopySum() {
+        prototype.testCopySum()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyAnyAny() {
+        prototype.testCopyAnyAny()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyTripleSum() {
+        prototype.testCopyTripleSum()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyAA() {
+        prototype.testCopyAA()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyAB() {
+        prototype.testCopyAB()
+    }
+
+    @Test
+    @Ignore
+    override fun testCopyF() {
+        prototype.testCopyF()
+    }
+
+    @Test
+    @Ignore
+    override fun testDoubleCopy() {
+        prototype.testDoubleCopy()
+    }
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestCopyTerm.kt
@@ -0,0 +1,93 @@
+package it.unibo.tuprolog.solve
+
+interface TestCopyTerm : SolverTest {
+    companion object {
+        fun prototype(solverFactory: SolverFactory): TestCopyTerm =
+                TestCopyTermImpl(solverFactory)
+    }
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(X,3).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testCopyXNum()
+
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(_,a).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testCopyAnyA()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(a+X,X+b).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     *  producing 1 solution which binds variable `X` to `a`.
+     */
+    fun testCopySum()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(_,_).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testCopyAnyAny()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(X+X+Y,A+B+B).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     *  producing 1 solution which binds variable `B` to `A`.
+     */
+    fun testCopyTripleSum()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(a,a).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testCopyAA()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(a,b).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testCopyAB()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- copy_term(f(a),f(X)).
+     * ```
+     * succeeds on a solver initialized with default built-ins and with and empty theory,
+     * producing 1 solution which binds variable `X` to `A`.
+     */
+    fun testCopyF()
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- (copy_term(a+X,X+b),copy_term(a+X,X+b)).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory.
+     */
+    fun testDoubleCopy()
+}
