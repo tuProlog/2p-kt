0799df501835a5f33aeb49849e1fe1e9bc8b9b7b
Davide Greco <davide.greco4@studio.unibo.it>
2020-09-04 17:05:00 +0200

    add @Ignore to Once test

--- solve-classic/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestClassicOnce.kt
+++ solve-classic/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestClassicOnce.kt
@@ -3,37 +3,44 @@ package it.unibo.tuprolog.solve.systemtest
 import it.unibo.tuprolog.solve.ClassicSolverFactory
 import it.unibo.tuprolog.solve.SolverFactory
 import it.unibo.tuprolog.solve.TestOnce
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class TestClassicOnce : TestOnce, SolverFactory by ClassicSolverFactory  {
     private val prototype = TestOnce.prototype(this)
 
     @Test
+    @Ignore
     override fun testOnceCut() {
         prototype.testOnceCut()
     }
 
     @Test
+    @Ignore
     override fun testOnceCutOr() {
         prototype.testOnceCutOr()
     }
 
     @Test
+    @Ignore
     override fun testOnceRepeat() {
         prototype.testOnceRepeat()
     }
 
     @Test
+    @Ignore
     override fun testOnceFail() {
         prototype.testOnceFail()
     }
 
     @Test
+    @Ignore
     override fun testOnceNum() {
         prototype.testOnceNum()
     }
 
     @Test
+    @Ignore
     override fun testOnceX() {
         prototype.testOnceX()
     }
--- solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestStreamsOnce.kt
+++ solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestStreamsOnce.kt
@@ -3,37 +3,44 @@ package it.unibo.tuprolog.solve.systemtest
 import it.unibo.tuprolog.solve.StreamsSolverFactory
 import it.unibo.tuprolog.solve.SolverFactory
 import it.unibo.tuprolog.solve.TestOnce
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class TestStreamsOnce : TestOnce, SolverFactory by StreamsSolverFactory  {
     private val prototype = TestOnce.prototype(this)
 
     @Test
+    @Ignore
     override fun testOnceCut() {
         prototype.testOnceCut()
     }
 
     @Test
+    @Ignore
     override fun testOnceCutOr() {
         prototype.testOnceCutOr()
     }
 
     @Test
+    @Ignore
     override fun testOnceRepeat() {
         prototype.testOnceRepeat()
     }
 
     @Test
+    @Ignore
     override fun testOnceFail() {
         prototype.testOnceFail()
     }
 
     @Test
+    @Ignore
     override fun testOnceNum() {
         prototype.testOnceNum()
     }
 
     @Test
+    @Ignore
     override fun testOnceX() {
         prototype.testOnceX()
     }
