7e0d5758691f007aff5126dc703df77ea4cb7ed0
Davide Greco <davide.greco4@studio.unibo.it>
2020-09-04 17:05:25 +0200

    test Repeat implementation

---
+++ solve-classic/src/commonTest/kotlin/it/unibo/tuprolog/solve/systemtest/TestClassicRepeat.kt
@@ -0,0 +1,15 @@
+package it.unibo.tuprolog.solve.systemtest
+
+import it.unibo.tuprolog.solve.ClassicSolverFactory
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.TestRepeat
+import kotlin.test.Test
+
+class TestClassicRepeat  : TestRepeat, SolverFactory by ClassicSolverFactory  {
+    private val prototype = TestRepeat.prototype(this)
+
+    @Test
+    override fun testRepeat() {
+        prototype.testRepeat()
+    }
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestRepeat.kt
@@ -0,0 +1,17 @@
+package it.unibo.tuprolog.solve
+
+interface TestRepeat : SolverTest {
+    companion object {
+        fun prototype(solverFactory: SolverFactory): TestRepeat =
+                TestRepeatImpl(solverFactory)
+    }
+
+    /**
+     * Tests the queries
+     * ```prolog
+     * ?- (repeat,!,fail).
+     * ```
+     * fails on a solver initialized with default built-ins and with and empty theory,
+     */
+    fun testRepeat()
+}
