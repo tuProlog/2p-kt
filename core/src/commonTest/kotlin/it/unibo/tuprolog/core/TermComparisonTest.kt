deb4f4ad29e38fab326514b79fb8c5ed4a2b74e2
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-10-24 15:32:15 +0200

    make term comparison tests more robust

--- core/src/commonTest/kotlin/it/unibo/tuprolog/core/TermComparisonTest.kt
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/TermComparisonTest.kt
@@ -4,13 +4,35 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TermComparisonTest {
-    private val ordered = orderedTerms
+
+    private val base = listOf(
+        Var.of("A"),
+        Var.of("A"),
+        Var.of("B"),
+        Var.of("C"),
+        Var.anonymous(),
+        Var.anonymous(),
+    ).sortedWith { x, y -> compareStringsLocaleIndependently(x.completeName, y.completeName) } + listOf(
+        Real.of(1.1),
+        Real.of(2.2),
+    ).sortedWith { x, y -> x.decimalValue.compareTo(y.decimalValue) } + listOf(
+        Integer.of(1),
+        Integer.of(2),
+    ).sortedWith { x, y -> x.intValue.compareTo(y.intValue) } + listOf(
+        Atom.of(""),
+        Atom.of("a"),
+        Atom.of("a b"),
+        Atom.of("b"),
+    ).sortedWith { x, y -> compareStringsLocaleIndependently(x.value, y.value) }
+
+    private val ordered =
+        base + base.map { Struct.of("f", it) } + base.map { Struct.of("g", it) }
 
     private val unordered = ordered.shuffled()
 
     @Test
     fun testCompareTo() {
-        val reordered = unordered.sortedWith(Comparator { a, b -> a.compareTo(b) })
+        val reordered = unordered.sortedWith { a, b -> a.compareTo(b) }
         assertEquals(ordered, reordered)
     }
 
--- core/src/commonTest/kotlin/it/unibo/tuprolog/core/TestComparisonUtils.kt
+++
@@ -1,5 +0,0 @@
-package it.unibo.tuprolog.core
-
-import kotlin.collections.List
-
-expect val orderedTerms: List<Term>
