cb39dde14002bcb82a46b490e191e798357bc3a4
Giovanni <giovanni.ciatto@unibo.it>
2020-11-26 16:23:41 +0100

    fix npm dependencies

--- solve-classic/build.gradle.kts
+++ solve-classic/build.gradle.kts
@@ -1,5 +1,4 @@
 kotlin {
-
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -9,36 +8,8 @@ kotlin {
         }
 
         val commonTest by getting {
-            dependsOn(commonMain)
             dependencies {
                 api(project(":test-solve"))
-            }
-        }
-
-        jvm {
-            val main = compilations["main"]
-            val test = compilations["test"]
-
-            main.defaultSourceSet {
-                dependsOn(commonMain)
-            }
-            test.defaultSourceSet {
-                dependsOn(commonTest)
-                dependsOn(main.defaultSourceSet)
-            }
-        }
-
-        js {
-
-            val main = compilations["main"]
-            val test = compilations["test"]
-
-            main.defaultSourceSet {
-                dependsOn(commonMain)
-            }
-            test.defaultSourceSet {
-                dependsOn(commonTest)
-                dependsOn(main.defaultSourceSet)
             }
         }
     }
--- test-solve/build.gradle.kts
+++ test-solve/build.gradle.kts
@@ -10,22 +10,14 @@ kotlin {
             }
         }
 
-        val commonTest by getting {
-            dependsOn(commonMain)
-        }
-
         jvm {
             val main = compilations["main"]
             val test = compilations["test"]
 
             main.defaultSourceSet {
-                dependsOn(commonMain)
                 dependencies {
                     implementation(kotlin("test-junit"))
                 }
-            }
-            test.defaultSourceSet {
-                dependsOn(main.defaultSourceSet)
             }
         }
 
@@ -34,13 +26,9 @@ kotlin {
             val test = compilations["test"]
 
             main.defaultSourceSet {
-                dependsOn(commonMain)
                 dependencies {
                     implementation(kotlin("test-js"))
                 }
-            }
-            test.defaultSourceSet {
-                dependsOn(main.defaultSourceSet)
             }
         }
     }
