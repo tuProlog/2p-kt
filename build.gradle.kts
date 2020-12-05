4bce22f7d952f28fa33dcbb5899e3fca1bb670cd
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-12-05 17:21:22 +0100

    add contributors to kotlinMultiplatform block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.github.gciatto.kt.mpp.ProjectExtensions.jvmProjects
+import io.github.gciatto.kt.mpp.ProjectExtensions.ktProjects
+
 plugins {
     id("io.github.gciatto.kt-mpp-pp")
     id("org.danilopianini.git-sensitive-semantic-versioning")
@@ -21,4 +24,30 @@ kotlinMultiplatform {
     otherProjects("documentation")
     ktProjects(allOtherSubprojects)
     developer("Giovanni Ciatto", "giovanni.ciatto@unibo.it", "http://about.me/gciatto")
+    developer("Enrico Siboni", "enrico.siboni3@studio.unibo.it")
+    developer("Paolo Verdini", "paolo.verdini@studio.unibo.it")
+    developer("Manuel Bonarrigo", "manuel.bonarrigo@studio.unibo.it")
+    developer("Sofia Montebugnoli", "sofia.montebugnoli2@studio.unibo.it")
+    developer("Davide Greco", "davide.greco4@studio.unibo.it")
+    developer("Silvia Lanzoni", "silvia.lanzoni5@studio.unibo.it")
+    developer("Lorenzo Rizzato", "lorenzo.rizzato@studio.unibo.it")
+}
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            ktProjects.filter { it.name !in setOf("solve-streams", "test-solve") }.forEach {
+                dependencies {
+                    api(project(":${it.name}"))
+                }
+            }
+        }
+        val jvmMain by getting {
+            jvmProjects.filter { it.name !in setOf("examples") }.forEach {
+                dependencies {
+                    api(project(":${it.name}"))
+                }
+            }
+        }
+    }
 }
