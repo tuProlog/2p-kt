9851f2f30f220b14ca2410443ccb65cf4550d749
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-12-30 16:18:24 +0100

    [wip] implementing initialization in :solve-classic

--- solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/solve/classic/ClassicSolver.kt
+++ solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/solve/classic/ClassicSolver.kt
@@ -13,6 +13,8 @@ import it.unibo.tuprolog.solve.classic.fsm.State
 import it.unibo.tuprolog.solve.classic.fsm.StateInit
 import it.unibo.tuprolog.solve.classic.fsm.clone
 import it.unibo.tuprolog.solve.currentTimeInstant
+import it.unibo.tuprolog.solve.directives.partition
+import it.unibo.tuprolog.solve.exception.warning.InitializationIssue
 import it.unibo.tuprolog.solve.getAllOperators
 import it.unibo.tuprolog.solve.library.Libraries
 import it.unibo.tuprolog.solve.toOperatorSet
@@ -22,8 +24,8 @@ import it.unibo.tuprolog.theory.Theory
 internal open class ClassicSolver(
     libraries: Libraries = Libraries.empty(),
     flags: FlagStore = FlagStore.empty(),
-    staticKb: Theory = Theory.empty(),
-    dynamicKb: Theory = MutableTheory.empty(),
+    initialStaticKb: Theory = Theory.empty(),
+    initialDynamicKb: Theory = MutableTheory.empty(),
     inputChannels: InputStore<*> = ExecutionContextAware.defaultInputChannels(),
     outputChannels: OutputStore<*> = ExecutionContextAware.defaultOutputChannels()
 ) : Solver {
@@ -32,13 +34,17 @@ internal open class ClassicSolver(
         ClassicExecutionContext(
             libraries = libraries,
             flags = flags,
-            staticKb = staticKb,
-            dynamicKb = dynamicKb.toMutableTheory(),
-            operators = getAllOperators(libraries, staticKb, dynamicKb).toOperatorSet(),
+            staticKb = Theory.emptyIndexed(),
+            dynamicKb = MutableTheory.emptyIndexed(),
+            operators = getAllOperators(libraries).toOperatorSet(),
             inputChannels = inputChannels,
             outputChannels = outputChannels
         )
     )
+
+    init {
+        initializeKb(initialStaticKb, initialDynamicKb)
+    }
 
     protected fun updateContext(contextMapper: ClassicExecutionContext.() -> ClassicExecutionContext) {
         val ctx = state.context
@@ -68,6 +74,59 @@ internal open class ClassicSolver(
             require(newState.context.step == newStep)
             state = newState
         }.asSequence()
+    }
+
+    protected fun initializeKb(staticKb: Theory?, dynamicKb: Theory?) {
+        val staticKbPartitioning = staticKb?.partition()
+        val dynamicKbPartitioning = dynamicKb?.partition(staticByDefault = false)
+        val newStaticKb = Theory.indexedOf(
+            sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+                .filterNotNull()
+                .flatMap { it.staticClauses.asSequence() }
+        )
+        val newDynamicKb = MutableTheory.indexedOf(
+            sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+                .filterNotNull()
+                .flatMap { it.dynamicClauses.asSequence() }
+        )
+        val newOperators = sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+            .filterNotNull()
+            .map { it.operators }
+            .reduce(OperatorSet::plus)
+        val newFlags = sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+            .filterNotNull()
+            .map { it.flagStore }
+            .reduce(FlagStore::plus)
+        val includes = sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+            .filterNotNull()
+            .flatMap { it.includes }
+            .map { Struct.of("consult", it) }
+        includes.forEach(this::solveInitialGoal)
+        updateContext {
+            copy(
+                staticKb = this.staticKb + newStaticKb,
+                dynamicKb = this.dynamicKb + newDynamicKb,
+                operators = operators + newOperators,
+                flags = flags + newFlags
+            )
+        }
+        val initialGoals = sequenceOf(staticKbPartitioning, dynamicKbPartitioning)
+            .filterNotNull()
+            .flatMap { it.initialGoals }
+        initialGoals.forEach(this::solveInitialGoal)
+    }
+
+    private fun solveInitialGoal(goal: Struct) {
+        for (solution in solve(goal)) {
+            when (solution) {
+                is Solution.No -> {
+                    warnings?.write(InitializationIssue(goal, null, state.context))
+                }
+                is Solution.Halt -> {
+                    warnings?.write(InitializationIssue(goal, solution.exception, solution.exception.contexts))
+                }
+            }
+        }
     }
 
     override val libraries: Libraries
