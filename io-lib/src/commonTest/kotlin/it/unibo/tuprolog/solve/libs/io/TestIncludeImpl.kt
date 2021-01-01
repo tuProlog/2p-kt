4a0371d527ad5384ef4a94c2435f1d0d839ce18c
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-01-01 13:25:37 +0100

    add tests for include/1 and load/1 directive

---
+++ io-lib/src/commonTest/kotlin/it/unibo/tuprolog/solve/libs/io/TestIncludeImpl.kt
@@ -0,0 +1,107 @@
+package it.unibo.tuprolog.solve.libs.io
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.DirectiveTestsUtils.solverInitializersWithEventsList
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.exception.warning.InitializationIssue
+import it.unibo.tuprolog.solve.libs.io.primitives.Consult
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TestIncludeImpl(private val solverFactory: SolverFactory) : TestInclude {
+
+    private fun theoryWithLocalInclude(loadGoal: String) =
+        prolog {
+            val parentsPath = findResource("Parents.pl")
+            theoryOf(
+                directive { loadGoal(parentsPath.toString()) },
+                fact { "someFact" }
+            )
+        }
+
+    private fun testLocalInclude(loadGoal: String) =
+        prolog {
+            val theory = theoryWithLocalInclude(loadGoal)
+            for ((solverOf, events) in solverInitializersWithEventsList(solverFactory)) {
+                val solver = solverOf(theory)
+
+                val expectedTheory = ExampleTheories.PARENTS + theory
+
+                assertEquals(ktListOf<Any>(), events)
+                assertTrue {
+                    expectedTheory.equals(solver.staticKb + solver.dynamicKb, useVarCompleteName = false)
+                }
+            }
+        }
+
+    override fun testLocalInclude() {
+        testLocalInclude("include")
+    }
+
+    override fun testLocalLoad() {
+        testLocalInclude("load")
+    }
+
+    private fun theoryWithRemoteInclude(loadGoal: String) =
+        prolog {
+            theoryOf(
+                directive { include(ExampleUrls.PARENTS) },
+                fact { "someFact" }
+            )
+        }
+
+    private fun testRemoteInclude(loadGoal: String) =
+        prolog {
+            val theory = theoryWithRemoteInclude(loadGoal)
+            for ((solverOf, events) in solverInitializersWithEventsList(solverFactory)) {
+                val solver = solverOf(theory)
+
+                val expectedTheory = ExampleTheories.PARENTS + theory
+
+                assertEquals(ktListOf<Any>(), events)
+                assertTrue {
+                    expectedTheory.equals(solver.staticKb + solver.dynamicKb, useVarCompleteName = false)
+                }
+            }
+        }
+
+    override fun testRemoteInclude() {
+        testRemoteInclude("include")
+    }
+
+    override fun testRemoteLoad() {
+        testRemoteInclude("load")
+    }
+
+    private fun theoryWithMissingInclude(loadGoal: String) =
+        prolog {
+            theoryOf(
+                directive { include("/path/to/missing.pl") },
+                fact { "someFact" }
+            )
+        }
+
+    private fun testMissingInclude(loadGoal: String) =
+        prolog {
+            val theory = theoryWithMissingInclude(loadGoal)
+            for ((solverOf, events) in solverInitializersWithEventsList(solverFactory)) {
+                val solver = solverOf(theory)
+
+                assertTrue { events.size == 1 }
+                assertTrue { events[0] is InitializationIssue }
+                assertTrue {
+                    (events[0] as? InitializationIssue)?.message?.contains(Consult.functor) ?: false
+                }
+
+                assertEquals(theory, solver.staticKb + solver.dynamicKb)
+            }
+        }
+
+    override fun testMissingInclude() {
+        testMissingInclude("include")
+    }
+
+    override fun testMissingLoad() {
+        testMissingInclude("load")
+    }
+}
---
+++ io-lib/src/commonTest/kotlin/it/unibo/tuprolog/solve/libs/io/TestStreamsInclude.kt
@@ -0,0 +1,46 @@
+package it.unibo.tuprolog.solve.libs.io
+
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.solve.streams.StreamsSolverFactory
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+@Ignore
+class TestStreamsInclude : TestInclude, SolverFactory by StreamsSolverFactory {
+
+    private val prototype = TestInclude.prototype(this)
+
+    override val defaultLibraries: Libraries
+        get() = super.defaultLibraries + Libraries.of(IOLib)
+
+    @Test
+    override fun testLocalInclude() {
+        prototype.testLocalInclude()
+    }
+
+    @Test
+    override fun testLocalLoad() {
+        prototype.testLocalLoad()
+    }
+
+    @Test
+    override fun testRemoteInclude() {
+        prototype.testRemoteInclude()
+    }
+
+    @Test
+    override fun testRemoteLoad() {
+        prototype.testRemoteLoad()
+    }
+
+    @Test
+    override fun testMissingInclude() {
+        prototype.testMissingInclude()
+    }
+
+    @Test
+    override fun testMissingLoad() {
+        prototype.testMissingLoad()
+    }
+}
