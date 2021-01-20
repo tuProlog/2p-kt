a8077b9744082a32623c3640da9f0846d9851b5d
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-01-20 16:07:37 +0100

    + example of probability via tags

--- prob-solve/src/commonMain/kotlin/it.unibo.tuprolog.solve/ProbExtensions.kt
+++ prob-solve/src/commonMain/kotlin/it.unibo.tuprolog.solve/ProbExtensions.kt
@@ -3,13 +3,19 @@
 package it.unibo.tuprolog.solve
 
 import it.unibo.tuprolog.utils.Taggable
-import it.unibo.tuprolog.utils.addTag
+import it.unibo.tuprolog.utils.setTag
 import kotlin.jvm.JvmName
+import kotlin.math.max
+import kotlin.math.min
 
-const val PROBABILITY_TAG = "it.unibo.tuprolog.probability"
+private const val PROBABILITY_TAG = "it.unibo.tuprolog.probability"
 
-var <T : Taggable<T>, U : T> U.probability: Double?
-    get() = getTag(PROBABILITY_TAG)
-    set(value) {
-        addTag(PROBABILITY_TAG to value)
-    }
+const val DEFAULT_PROBABILITY = 0.0
+
+private fun normalize(probability: Double): Double =
+    max(min(1.0, probability), 0.0)
+
+val <T : Taggable<T>, U : T> U.probability: Double
+    get() = getTag(PROBABILITY_TAG) ?: DEFAULT_PROBABILITY
+
+fun <T : Taggable<T>, U : T> U.setProbability(value: Double): U = setTag(PROBABILITY_TAG, normalize(value))
