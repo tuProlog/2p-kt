018f1b17a89f3d2fedd3c40d56553354cca85dbb
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-01-23 15:04:21 +0100

    make Substitution.{plus, minus, filter} preserve tags

--- core/src/commonTest/kotlin/it/unibo/tuprolog/core/TestSubstitutionsMetadata.kt
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/TestSubstitutionsMetadata.kt
@@ -15,51 +15,56 @@ class TestSubstitutionsMetadata {
 
     private val X = Var.of("X")
 
+    private val Y = Var.of("Y")
+
+    private val Z = Var.of("Z")
+
     private val substitutions = listOf(
         Substitution.failed(),
         Substitution.empty(),
-        Substitution.unifier("Y", X),
-        Substitution.unifier("Y", Var.anonymous()),
-        Substitution.unifier("Y", Atom.of("atom")),
-        Substitution.unifier("Y", Integer.ONE),
-        Substitution.unifier("Y", Real.ONE_TENTH),
-        Substitution.unifier("Y", EmptyList()),
-        Substitution.unifier("Y", EmptySet()),
-        Substitution.unifier("Y", Cons.of(Real.ONE_HALF, X)),
-        Substitution.unifier("Y", Struct.of("g", X)),
-        Substitution.unifier("Y", List.of(X)),
-        Substitution.unifier("Y", Set.of(X)),
-        Substitution.unifier("Y", Tuple.of(X, X)),
-        Substitution.unifier("Y", Fact.of(Struct.of("f", X))),
-        Substitution.unifier("Y", Fact.of(List.of(X))),
-        Substitution.unifier("Y", Fact.of(Set.of(X))),
-        Substitution.unifier("Y", Fact.of(Tuple.of(X, X))),
-        Substitution.unifier("Y", Directive.of(Struct.of("h", X), Tuple.of(X, X))),
-        Substitution.unifier("Y", Rule.of(Struct.of("h", X), Tuple.of(X, X))),
-        Substitution.of("Z", X),
-        Substitution.of("Z", Var.anonymous()),
-        Substitution.of("Z", Atom.of("atom")),
-        Substitution.of("Z", Integer.ONE),
-        Substitution.of("Z", Real.ONE_TENTH),
-        Substitution.of("Z", EmptyList()),
-        Substitution.of("Z", EmptySet()),
-        Substitution.of("Z", Cons.of(Real.ONE_HALF, X)),
-        Substitution.of("Z", Struct.of("g", X)),
-        Substitution.of("Z", List.of(X)),
-        Substitution.of("Z", Set.of(X)),
-        Substitution.of("Z", Tuple.of(X, X)),
-        Substitution.of("Z", Fact.of(Struct.of("f", X))),
-        Substitution.of("Z", Fact.of(List.of(X))),
-        Substitution.of("Z", Fact.of(Set.of(X))),
-        Substitution.of("Z", Fact.of(Tuple.of(X, X))),
-        Substitution.of("Z", Directive.of(Struct.of("h", X), Tuple.of(X, X))),
-        Substitution.of("Z", Rule.of(Struct.of("h", X), Tuple.of(X, X))),
+        Substitution.unifier(Y, X),
+        Substitution.unifier(Y, Var.anonymous()),
+        Substitution.unifier(Y, Atom.of("atom")),
+        Substitution.unifier(Y, Integer.ONE),
+        Substitution.unifier(Y, Real.ONE_TENTH),
+        Substitution.unifier(Y, EmptyList()),
+        Substitution.unifier(Y, EmptySet()),
+        Substitution.unifier(Y, Cons.of(Real.ONE_HALF, X)),
+        Substitution.unifier(Y, Struct.of("g", X)),
+        Substitution.unifier(Y, List.of(X)),
+        Substitution.unifier(Y, Set.of(X)),
+        Substitution.unifier(Y, Tuple.of(X, X)),
+        Substitution.unifier(Y, Fact.of(Struct.of("f", X))),
+        Substitution.unifier(Y, Fact.of(List.of(X))),
+        Substitution.unifier(Y, Fact.of(Set.of(X))),
+        Substitution.unifier(Y, Fact.of(Tuple.of(X, X))),
+        Substitution.unifier(Y, Directive.of(Struct.of("h", X), Tuple.of(X, X))),
+        Substitution.unifier(Y, Rule.of(Struct.of("h", X), Tuple.of(X, X))),
+        Substitution.of(Z, X),
+        Substitution.of(Z, Var.anonymous()),
+        Substitution.of(Z, Atom.of("atom")),
+        Substitution.of(Z, Integer.ONE),
+        Substitution.of(Z, Real.ONE_TENTH),
+        Substitution.of(Z, EmptyList()),
+        Substitution.of(Z, EmptySet()),
+        Substitution.of(Z, Cons.of(Real.ONE_HALF, X)),
+        Substitution.of(Z, Struct.of("g", X)),
+        Substitution.of(Z, List.of(X)),
+        Substitution.of(Z, Set.of(X)),
+        Substitution.of(Z, Tuple.of(X, X)),
+        Substitution.of(Z, Fact.of(Struct.of("f", X))),
+        Substitution.of(Z, Fact.of(List.of(X))),
+        Substitution.of(Z, Fact.of(Set.of(X))),
+        Substitution.of(Z, Fact.of(Tuple.of(X, X))),
+        Substitution.of(Z, Directive.of(Struct.of("h", X), Tuple.of(X, X))),
+        Substitution.of(Z, Rule.of(Struct.of("h", X), Tuple.of(X, X))),
     )
 
     private data class Metadata<T>(val value: T)
 
     private val someKey = "some_key1"
     private val someOtherKey = "some_key2"
+    private val yetAnotherKey = "some_key3"
     private val someValue1 = Metadata(0L)
     private val someValue2 = Metadata(1L)
     private val someOtherValue = Metadata(2L)
@@ -151,9 +156,58 @@ class TestSubstitutionsMetadata {
 
     @Test
     fun settingAnExistingTagReplacesThatTag() {
-        val taggedSubstitutions = substitutions.map { it.setTags(someKey to someValue1, someOtherKey to someOtherValue) }
+        val taggedSubstitutions =
+            substitutions.map { it.setTags(someKey to someValue1, someOtherKey to someOtherValue) }
         for (replaced in taggedSubstitutions.map { it.setTag(someKey, someValue2) }) {
             assertEquals(someValue2, replaced.getTag<Metadata<Long>>(someKey))
+        }
+    }
+
+    private fun alteringPreservesTags(alter: Substitution.() -> Substitution) {
+        val tags = mapOf(someKey to someValue1, someOtherKey to someValue2)
+        for (substitution in substitutions.map { it.setTags(tags) }) {
+            assertEquals(tags, substitution.tags)
+            assertEquals(substitution.tags, substitution.alter().tags)
+        }
+    }
+
+    private fun summingMergesTags(other: Substitution, sum: Substitution.(Substitution) -> Substitution) {
+        val tags = mapOf(someKey to someValue1, someOtherKey to someValue2)
+        for (substitution in substitutions.map { it.setTags(tags) }) {
+            assertEquals(tags, substitution.tags)
+            assertEquals(tags + other.tags, substitution.sum(other).tags)
+        }
+    }
+
+    @Test
+    fun minusIterablePreservesTags() {
+        alteringPreservesTags { this - listOf(X, Y, Z) }
+    }
+
+    @Test
+    fun minusPreservesTags() {
+        alteringPreservesTags { this - X - Y - Z }
+    }
+
+    @Test
+    fun filterWithFunctionPreservesTags() {
+        alteringPreservesTags { filter { (k, _) -> k in setOf(X, Y, Z) } }
+    }
+
+    @Test
+    fun filterPreservesTags() {
+        alteringPreservesTags { filter(setOf(X, Y, Z)) }
+    }
+
+    @Test
+    fun plusPreservesTags() {
+        summingMergesTags(Substitution.empty().setTag(yetAnotherKey, someOtherValue)) { plus(it) }
+    }
+
+    @Test
+    fun plusWithFunctionPreservesTags() {
+        summingMergesTags(Substitution.of(X to EmptySet()).setTag(yetAnotherKey, someOtherValue)) {
+            plus(it) { x, y -> x + y }
         }
     }
 }
