7b6b67ab96fbd3e5067a28ae1f879a8499351270
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-01-29 18:37:35 +0100

    test custom data primitives

---
+++ solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/streams/systemtest/TestStreamsCustomData.kt
@@ -0,0 +1,33 @@
+package it.unibo.tuprolog.solve.streams.systemtest
+
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.TestCustomData
+import it.unibo.tuprolog.solve.streams.StreamsSolverFactory
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+@Ignore
+class TestStreamsCustomData : TestCustomData, SolverFactory by StreamsSolverFactory {
+
+    val prototype = TestCustomData.prototype(this)
+
+    @Test
+    override fun testApi() {
+        prototype.testApi()
+    }
+
+    @Test
+    override fun testEphemeralData() {
+        prototype.testEphemeralData()
+    }
+
+    @Test
+    override fun testDurableData() {
+        prototype.testDurableData()
+    }
+
+    @Test
+    override fun testPersistentData() {
+        prototype.testPersistentData()
+    }
+}
---
+++ test-solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/TestCustomDataImpl.kt
@@ -0,0 +1,107 @@
+package it.unibo.tuprolog.solve
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.stdlib.primitive.GetDurable
+import it.unibo.tuprolog.solve.stdlib.primitive.GetEphemeral
+import it.unibo.tuprolog.solve.stdlib.primitive.GetPersistent
+import it.unibo.tuprolog.solve.stdlib.primitive.SetDurable
+import it.unibo.tuprolog.solve.stdlib.primitive.SetEphemeral
+import it.unibo.tuprolog.solve.stdlib.primitive.SetPersistent
+
+class TestCustomDataImpl(private val solverFactory: SolverFactory) : TestCustomData {
+
+    override fun testApi() {
+        val solver = solverFactory.solverWithDefaultBuiltins()
+
+        solver.assertHasPredicateInAPI(GetPersistent)
+        solver.assertHasPredicateInAPI(GetEphemeral)
+        solver.assertHasPredicateInAPI(GetDurable)
+        solver.assertHasPredicateInAPI(SetPersistent)
+        solver.assertHasPredicateInAPI(SetEphemeral)
+        solver.assertHasPredicateInAPI(SetDurable)
+    }
+
+    override fun testEphemeralData() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val setQuery = SetEphemeral.functor("key", 1)
+            val getQuery = GetEphemeral.functor("key", X)
+            val query = setQuery and (getQuery or getQuery)
+
+            assertSolutionEquals(
+                ktListOf(setQuery.yes()),
+                solver.solve(setQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(getQuery.no()),
+                solver.solve(getQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(
+                    query.yes(X to 1),
+                    query.no()
+                ),
+                solver.solve(query, shortDuration).toList()
+            )
+        }
+    }
+
+    override fun testDurableData() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val setQuery = SetDurable.functor("key", 1)
+            val getQuery = GetDurable.functor("key", X)
+            val query = setQuery and (getQuery or getQuery)
+
+            assertSolutionEquals(
+                ktListOf(setQuery.yes()),
+                solver.solve(setQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(getQuery.no()),
+                solver.solve(getQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(
+                    query.yes(X to 1),
+                    query.yes(X to 1)
+                ),
+                solver.solve(query, shortDuration).toList()
+            )
+        }
+    }
+
+    override fun testPersistentData() {
+        prolog {
+            val solver = solverFactory.solverWithDefaultBuiltins()
+
+            val setQuery = SetPersistent.functor("key", 1)
+            val getQuery = GetPersistent.functor("key", X)
+            val query = setQuery and (getQuery or getQuery)
+
+            assertSolutionEquals(
+                ktListOf(setQuery.yes()),
+                solver.solve(setQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(getQuery.yes(X to 1)),
+                solver.solve(getQuery, shortDuration).toList()
+            )
+
+            assertSolutionEquals(
+                ktListOf(
+                    query.yes(X to 1),
+                    query.yes(X to 1)
+                ),
+                solver.solve(query, shortDuration).toList()
+            )
+        }
+    }
+}
