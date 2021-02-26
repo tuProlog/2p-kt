0d2087e5b45dc2af53ddb3649e0d486d6ea8619f
Giovanni <giovanni.ciatto@unibo.it>
2021-02-26 10:44:07 +0100

    let classic and streams solvers rely on Solution.cleanUp

--- solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/solve/classic/impl/AbstractSolutionIterator.kt
+++ solve-classic/src/commonMain/kotlin/it/unibo/tuprolog/solve/classic/impl/AbstractSolutionIterator.kt
@@ -1,7 +1,5 @@
 package it.unibo.tuprolog.solve.classic.impl
 
-import it.unibo.tuprolog.core.Substitution
-import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.solve.Solution
 import it.unibo.tuprolog.solve.classic.SolutionIterator
 import it.unibo.tuprolog.solve.classic.fsm.EndState
@@ -27,18 +25,4 @@ internal abstract class AbstractSolutionIterator(state: State) : SolutionIterato
     }
 
     protected abstract fun computeNextState(state: State, step: Long): State
-
-    private fun Solution.cleanUp(): Solution =
-        when (this) {
-            is Solution.Yes -> cleanUp()
-            else -> this
-        }
-
-    private fun Solution.Yes.cleanUp(): Solution.Yes {
-        return copy(substitution = substitution.cleanUp(query.variables.toSet()))
-    }
-
-    private fun Substitution.Unifier.cleanUp(toRetain: Set<Var>): Substitution.Unifier {
-        return filter { _, term -> (term !is Var) || (term in toRetain) }
-    }
 }
