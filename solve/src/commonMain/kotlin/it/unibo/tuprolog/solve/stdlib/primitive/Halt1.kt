b8e5855063e3563bc659ac2627be9a41a9db89a8
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-07-16 16:59:45 +0200

    format Halt1

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/Halt1.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/Halt1.kt
@@ -1,6 +1,5 @@
 package it.unibo.tuprolog.solve.stdlib.primitive
 
-import it.unibo.tuprolog.core.Integer
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.exception.HaltException
