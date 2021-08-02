f3587ed6260e16d1533c82f600b3a588b22b23b9
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-08-02 21:52:53 +0200

    + ReadEditSolve examples on both Kt and Java

---
+++ examples/src/test/java/it/unibo/tuprolog/examples/ReadEditSolveJvm.java
@@ -0,0 +1,52 @@
+package it.unibo.tuprolog.examples;
+
+import it.unibo.tuprolog.core.Integer;
+import it.unibo.tuprolog.core.Struct;
+import it.unibo.tuprolog.core.Var;
+import it.unibo.tuprolog.solve.MutableSolver;
+import it.unibo.tuprolog.solve.Solution;
+import it.unibo.tuprolog.solve.Solver;
+import it.unibo.tuprolog.theory.Theory;
+import it.unibo.tuprolog.theory.parsing.ClausesReader;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReadEditSolveJvm {
+
+    @Test
+    public void main() {
+        InputStream inputStream = ReadEditSolveJvm.class.getResourceAsStream("increment.pl");
+        ClausesReader clausesReader = ClausesReader.getWithDefaultOperators();
+        Theory theory = clausesReader.readTheory(inputStream);
+        MutableSolver solver = Solver.getClassic().mutableSolverWithDefaultBuiltins(theory);
+        Struct fact = Struct.of("diff", Integer.of(15));
+        solver.assertZ(fact);
+        Struct query = Struct.of("increment", Integer.of(15), Var.of("X"));
+        Iterator<Solution> solutionIterator = solver.solve(query).iterator();
+        while (solutionIterator.hasNext()) {
+            Solution solution = solutionIterator.next();
+            if (solution.isYes()) {
+                assertEquals(Integer.of(15), solution.getSubstitution().getByName("X"));
+            }
+        }
+    }
+
+    @Test
+    public void simple() {
+        InputStream inputStream = ReadEditSolveJvm.class.getResourceAsStream("increment.pl");
+        ClausesReader clausesReader = ClausesReader.getWithDefaultOperators();
+        Theory theory = clausesReader.readTheory(inputStream);
+        MutableSolver solver = Solver.getClassic().mutableSolverWithDefaultBuiltins(theory);
+        Struct fact = Struct.of("diff", Integer.of(15));
+        solver.assertZ(fact);
+        Struct query = Struct.of("increment", Integer.of(15), Var.of("X"));
+        Solution solution = solver.solveOnce(query);
+        if (solution.isYes()) {
+            assertEquals(Integer.of(15), solution.getSubstitution().getByName("X"));
+        }
+    }
+}
---
+++ examples/src/test/kotlin/it/unibo/tuprolog/examples/ReadEditSolveKt.kt
@@ -0,0 +1,42 @@
+package it.unibo.tuprolog.examples
+
+import it.unibo.tuprolog.core.Integer
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.theory.parsing.ClausesReader
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ReadEditSolveKt {
+    @Test
+    fun main() {
+        val inputStream = ReadEditSolveKt::class.java.getResourceAsStream("increment.pl")!!
+        val clauseReader = ClausesReader.withDefaultOperators
+        val theory = clauseReader.readTheory(inputStream)
+        val solver = Solver.classic.mutableSolverWithDefaultBuiltins(theory)
+        val fact = Struct.of("diff", Integer.of(15))
+        solver.assertZ(fact)
+        val query = Struct.of("increment", Integer.of(15), Var.of("X"))
+        for (solution in solver.solve(query)) {
+            if (solution.isYes) {
+                assertEquals(Integer.of(15), solution.substitution.getByName("X"))
+            }
+        }
+    }
+
+    @Test
+    fun simple() {
+        val inputStream = ReadEditSolveKt::class.java.getResourceAsStream("increment.pl")!!
+        val clauseReader = ClausesReader.withDefaultOperators
+        val theory = clauseReader.readTheory(inputStream)
+        val solver = Solver.classic.mutableSolverWithDefaultBuiltins(theory)
+        val fact = Struct.of("diff", Integer.of(15))
+        solver.assertZ(fact)
+        val query = Struct.of("increment", Integer.of(15), Var.of("X"))
+        val solution = solver.solveOnce(query)
+        if (solution.isYes) {
+            assertEquals(Integer.of(15), solution.substitution.getByName("X"))
+        }
+    }
+}
