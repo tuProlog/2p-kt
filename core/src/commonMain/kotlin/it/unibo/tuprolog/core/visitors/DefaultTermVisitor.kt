e77b629daa992f05ae99fdb56c65283a7f422082
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-09-06 17:06:45 +0200

    + PyUtils on jvm

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/visitors/DefaultTermVisitor.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/visitors/DefaultTermVisitor.kt
@@ -1,8 +1,19 @@
 package it.unibo.tuprolog.core.visitors
 
 import it.unibo.tuprolog.core.Term
+import kotlin.js.JsName
+import kotlin.jvm.JvmStatic
 
 abstract class DefaultTermVisitor<T> : AbstractTermVisitor<T>() {
     override fun <X : Term> join(term: X, f1: (X) -> T, vararg fs: (X) -> T): T =
         sequenceOf(f1, *fs).map { it(term) }.first()
+
+    companion object {
+        @JvmStatic
+        @JsName("of")
+        fun <X> of(defaultValue: (Term) -> X): DefaultTermVisitor<X> =
+            object : DefaultTermVisitor<X>() {
+                override fun defaultValue(term: Term): X = defaultValue(term)
+            }
+    }
 }
