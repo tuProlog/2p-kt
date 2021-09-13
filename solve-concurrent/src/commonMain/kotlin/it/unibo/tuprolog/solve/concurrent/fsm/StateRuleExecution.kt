e96f5ed3d5770d4a63b6af11ae11bc10c9a07e7f
Andrea <andrea.giordano5@studio.unibo.it>
2021-09-13 11:02:27 +0200

    Added empty StateRuleExecution

---
+++ solve-concurrent/src/commonMain/kotlin/it/unibo/tuprolog/solve/concurrent/fsm/StateRuleExecution.kt
@@ -0,0 +1,11 @@
+package it.unibo.tuprolog.solve.concurrent.fsm
+
+import it.unibo.tuprolog.solve.concurrent.ConcurrentExecutionContext
+
+data class StateRuleExecution(override val context: ConcurrentExecutionContext) : State{
+    override fun next(): Iterable<State> {
+        TODO("Not yet implemented")
+    }
+
+    override fun clone(context: ConcurrentExecutionContext): State = copy(context = context)
+}
