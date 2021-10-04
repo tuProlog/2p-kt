64fb985a178c3855f4e3a107aec362634d1176d6
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-04 17:45:53 +0200

    Refactored test to generalize

--- solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentTrue.kt
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentTrue.kt
@@ -1,24 +1,34 @@
 package it.unibo.tuprolog.solve.concurrent
 
 import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.Solution
 import it.unibo.tuprolog.solve.SolverFactory
 import it.unibo.tuprolog.solve.SolverTest
 import it.unibo.tuprolog.solve.yes
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class TestConcurrentTrue : SolverTest, SolverFactory by ConcurrentSolverFactory {
-    @Test
+interface TestConcurrentTrue<T> : FromSequence<T>, SolverFactory {
+
     fun testTrue() {
         prolog {
             val solver = solverWithDefaultBuiltins()
             val query = atomOf("true")
 
-            val solutions = MultiSet(solver.solve(query,mediumDuration))
-
-            assertUnorderedSolutionEquals(
-                MultiSet(query.yes()),
-                solutions
+            assertEquals(
+                fromSequence(solver.solve(query,mediumDuration)),
+                fromSequence(sequenceOf(query.yes()))
             )
+
         }
     }
+}
+
+class TestConcurrentTrueImpl : TestConcurrentTrue<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @Test
+    override fun testTrue() = super.testTrue()
+
 }
