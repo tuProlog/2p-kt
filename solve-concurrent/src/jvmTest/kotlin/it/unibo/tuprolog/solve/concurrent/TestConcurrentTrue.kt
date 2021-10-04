e7a689a77e7327dce6fee93c7029ee64f3f59016
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-04 12:04:26 +0200

    Added first working concurrent test

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentTrue.kt
@@ -0,0 +1,24 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.SolverTest
+import it.unibo.tuprolog.solve.yes
+import kotlin.test.Test
+
+class TestConcurrentTrue : SolverTest, SolverFactory by ConcurrentSolverFactory {
+    @Test
+    fun testTrue() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+            val query = atomOf("true")
+
+            val solutions = MultiSet(solver.solve(query))
+
+            assertUnorderedSolutionEquals(
+                MultiSet(query.yes()),
+                solutions
+            )
+        }
+    }
+}
