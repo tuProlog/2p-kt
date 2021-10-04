d65bd679c5772757172681dae0c1e0dec7d04b31
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-04 11:59:47 +0200

    Refactored channelToSequence conversion

--- solve-concurrent/src/jvmMain/kotlin/it/unibo/tuprolog/solve/concurrent/CoroutinesUtilsJvm.kt
+++ solve-concurrent/src/jvmMain/kotlin/it/unibo/tuprolog/solve/concurrent/CoroutinesUtilsJvm.kt
@@ -2,17 +2,39 @@ package it.unibo.tuprolog.solve.concurrent
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
 import java.util.concurrent.LinkedBlockingQueue
 
-actual fun <T> ReceiveChannel<T>.toSequence(coroutineScope: CoroutineScope): Sequence<T> =
-    consumeAsFlow().toSequence(coroutineScope)
+// todo clean, old (hopefully useless) implementation
+//actual fun <T> ReceiveChannel<T>.toSequence(coroutineScope: CoroutineScope): Sequence<T> =
+//    consumeAsFlow().toSequence(coroutineScope)
 
+@OptIn(ExperimentalCoroutinesApi::class)
+actual fun <T> ReceiveChannel<T>.toSequence(coroutineScope: CoroutineScope): Sequence<T> {
+    val queue = LinkedBlockingQueue<Any?>()
+    coroutineScope.launch {
+        while (! this@toSequence.isClosedForReceive)
+            queue.add(receive())
+        queue.add(PoisonPill)
+    }
+    return sequence {
+        while (true) {
+            val current = queue.take()
+            if (current == PoisonPill) {
+                break
+            } else {
+                yield(current as T)
+            }
+        }
+    }
+}
+
+// todo clean, old (hopefully useless) implementation
 actual fun <T> Flow<T>.toSequence(coroutineScope: CoroutineScope): Sequence<T> {
     val queue = LinkedBlockingQueue<Any?>()
     coroutineScope.launch {
