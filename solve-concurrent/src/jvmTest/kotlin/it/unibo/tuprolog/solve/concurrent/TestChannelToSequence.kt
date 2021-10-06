aabccf835c144016d68751e6b9be05dc1fc47d14
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-06 17:29:08 +0200

    Added test for channel to sequence conversion

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestChannelToSequence.kt
@@ -0,0 +1,52 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TestChannelToSequence {
+
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+
+    @Test
+    fun testEmptyChannel(){
+        val channel: Channel<String> = Channel()
+        val job = scope.launch { channel.close() }
+        val emptySeq = channel.toSequence(scope)
+        assertTrue(emptySeq.count() == 0)
+        scope.launch {
+            job.join()
+            assertTrue(channel.isClosedForReceive)
+            assertTrue(channel.isClosedForSend)
+        }
+    }
+
+    @Test
+    fun testEarlyClose(){
+        val channel: Channel<String> = Channel()
+        scope.launch {
+            channel.close()
+            val seq = channel.toSequence(scope)
+            assertTrue(seq.count() == 0)
+            assertTrue(channel.isClosedForReceive)
+            assertTrue(channel.isClosedForSend)
+        }
+    }
+
+    @Test
+    fun testChannelToSequence(){
+        val times = 100
+        val channel: Channel<String> = Channel(Channel.UNLIMITED)
+        scope.launch {
+            for(c in 0 until  times)
+                channel.send("$c")
+            channel.close()
+        }
+        val seq = channel.toSequence(scope)
+        seq.forEachIndexed { index, s -> assertEquals("$index", s) }
+    }
+}
