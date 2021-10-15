50fd025bac0e0b5f441eaab0e930a45c06306692
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-15 12:02:54 +0200

    Added tests CatchAndThrow

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentCatchAndThrow.kt
@@ -0,0 +1,42 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.DummyInstances
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.exception.error.SystemError
+import it.unibo.tuprolog.solve.halt
+import it.unibo.tuprolog.solve.no
+
+interface TestConcurrentCatchAndThrow<T : WithAssertingEquals> : FromSequence<T>, SolverFactory {
+
+    fun testCatchThrow() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = (catch(true, C, write("something")) and `throw`("blabla"))
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(
+                query.halt(
+                    SystemError.forUncaughtException(
+                        DummyInstances.executionContext,
+                        atomOf("blabla")
+                    )
+                )
+            )
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testCatchFail() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = catch(number_chars("A", "L"), "error"("instantiation_error", `_`), fail)
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+}
---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentCatchAndThrowImpl.kt
@@ -0,0 +1,19 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentCatchAndThrowImpl :
+    TestConcurrentCatchAndThrow<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCatchThrow() = multiRunConcurrentTest { super.testCatchThrow() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCatchFail() = multiRunConcurrentTest { super.testCatchFail() }
+}
