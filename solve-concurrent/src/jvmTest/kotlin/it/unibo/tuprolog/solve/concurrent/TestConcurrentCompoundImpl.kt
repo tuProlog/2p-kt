91aa74611c915a26476a2f8cf055082f30406e4b
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-19 10:54:06 +0200

    Added tests Compound

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentCompoundImpl.kt
@@ -0,0 +1,39 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentCompoundImpl :
+    TestConcurrentCompound<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundDec() = multiRunConcurrentTest { super.testCompoundDec() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundNegDec() = multiRunConcurrentTest { super.testCompoundNegDec() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundNegA() = multiRunConcurrentTest { super.testCompoundNegA() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundAny() = multiRunConcurrentTest { super.testCompoundAny() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundA() = multiRunConcurrentTest { super.testCompoundA() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundAOfB() = multiRunConcurrentTest { super.testCompoundAOfB() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testCompoundListA() = multiRunConcurrentTest { super.testCompoundListA() }
+}
