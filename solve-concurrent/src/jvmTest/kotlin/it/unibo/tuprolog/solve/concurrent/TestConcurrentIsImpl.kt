eeb2ece869696110315f7b87010cdfd8be077d76
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-19 10:51:38 +0200

    Added tests Is

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentIsImpl.kt
@@ -0,0 +1,35 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentIsImpl :
+    TestConcurrentIs<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsResult() = multiRunConcurrentTest { super.testIsResult() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsXY() = multiRunConcurrentTest { super.testIsXY() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsFoo() = multiRunConcurrentTest { super.testIsFoo() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsNNumber() = multiRunConcurrentTest { super.testIsNNumber() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsNumberFoo() = multiRunConcurrentTest { super.testIsNumberFoo() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIsXFloat() = multiRunConcurrentTest { super.testIsXFloat() }
+}
