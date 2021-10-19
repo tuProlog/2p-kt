39cc8b9657fc1173f490b958e0249efa6e675e71
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-19 10:52:58 +0200

    Added tests Float

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentFloatImpl.kt
@@ -0,0 +1,31 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentFloatImpl :
+    TestConcurrentFloat<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testFloatDec() = multiRunConcurrentTest { super.testFloatDec() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testFloatDecNeg() = multiRunConcurrentTest { super.testFloatDecNeg() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testFloatNat() = multiRunConcurrentTest { super.testFloatNat() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testFloatAtom() = multiRunConcurrentTest { super.testFloatAtom() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testFloatX() = multiRunConcurrentTest { super.testFloatX() }
+}
