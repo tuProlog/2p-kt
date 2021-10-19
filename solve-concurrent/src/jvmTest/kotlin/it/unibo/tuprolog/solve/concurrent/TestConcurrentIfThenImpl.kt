38d1408067011ef233c31234976ff9fb4433321c
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-19 10:52:22 +0200

    Added partial tests IfThen

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentIfThenImpl.kt
@@ -0,0 +1,35 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentIfThenImpl :
+    TestConcurrentIfThen<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testIfThenTrue() = multiRunConcurrentTest { super.testIfThenTrue() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIfThenFail() = multiRunConcurrentTest { super.testIfThenFail() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testIfThenFailTrue() = multiRunConcurrentTest { super.testIfThenFailTrue() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testIfThenXtoOne() = multiRunConcurrentTest { super.testIfThenXtoOne() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testIfThenXOr() = multiRunConcurrentTest { super.testIfThenXOr() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testIfThenOrWithDoubleSub() = multiRunConcurrentTest { super.testIfThenOrWithDoubleSub() }
+}
