d489b3e48e89a4e2bf1b86fa9101f1ca671051e0
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 12:52:44 +0200

    Added tests NumberCodes

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentNumberCodesImpl.kt
@@ -0,0 +1,39 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentNumberCodesImpl :
+    TestConcurrentNumberCodes<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesListIsVar() = multiRunConcurrentTest { super.testNumberCodesListIsVar() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesNumIsDecimal() = multiRunConcurrentTest { super.testNumberCodesNumIsDecimal() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesListIsVar2() = multiRunConcurrentTest { super.testNumberCodesListIsVar2() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesOk() = multiRunConcurrentTest { super.testNumberCodesOk() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesCompleteTest() = multiRunConcurrentTest { super.testNumberCodesCompleteTest() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesNegativeNumber() = multiRunConcurrentTest { super.testNumberCodesNegativeNumber() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNumberCodesChar() = multiRunConcurrentTest { super.testNumberCodesChar() }
+}
