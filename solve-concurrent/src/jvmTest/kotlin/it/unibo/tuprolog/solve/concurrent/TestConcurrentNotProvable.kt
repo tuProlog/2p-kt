d514995e9263061a7d00bf73ab17fa0bd3aa7f28
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 12:21:29 +0200

    Added partial tests NotProvable

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentNotProvable.kt
@@ -0,0 +1,122 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.DummyInstances
+import it.unibo.tuprolog.solve.Signature
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.exception.error.InstantiationError
+import it.unibo.tuprolog.solve.exception.error.TypeError
+import it.unibo.tuprolog.solve.halt
+import it.unibo.tuprolog.solve.no
+import it.unibo.tuprolog.solve.yes
+
+interface TestConcurrentNotProvable<T : WithAssertingEquals> : FromSequence<T>, SolverFactory {
+
+    val errorSignature: Signature
+
+    fun testNPTrue() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"(true)
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testNPCut() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"("!")
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testNPCutFail() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"(("!" and fail))
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testOrNotCutFail() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = ((("X" `=` 1) or ("X" `=` 2)) and "not"(("!" and fail)))
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(
+                sequenceOf(
+                    query.yes("X" to 1),
+                    query.yes("X" to 2)
+                )
+            )
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testNPEquals() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"(4 `=` 5)
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testNPNum() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"(3)
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(
+                query.halt(
+                    TypeError.forGoal(
+                        DummyInstances.executionContext,
+                        errorSignature,
+                        TypeError.Expected.CALLABLE,
+                        numOf(3)
+                    )
+                )
+            )
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testNPX() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            val query = "not"("X")
+            val solutions = fromSequence(solver.solve(query, mediumDuration))
+            val expected = fromSequence(
+                query.halt(
+                    InstantiationError.forGoal(
+                        DummyInstances.executionContext,
+                        errorSignature,
+                        varOf("X")
+                    )
+                )
+            )
+
+            expected.assertingEquals(solutions)
+        }
+    }
+}
---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentNotProvableImpl.kt
@@ -0,0 +1,42 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.Signature
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentNotProvableImpl :
+    TestConcurrentNotProvable<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    override val errorSignature: Signature = Signature("ensure_executable", 1)
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testNPTrue() = multiRunConcurrentTest { super.testNPTrue() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testNPCut() = multiRunConcurrentTest { super.testNPCut() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNPCutFail() = multiRunConcurrentTest { super.testNPCutFail() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testOrNotCutFail() = multiRunConcurrentTest { super.testOrNotCutFail() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNPEquals() = multiRunConcurrentTest { super.testNPEquals() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNPNum() = multiRunConcurrentTest { super.testNPNum() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testNPX() = multiRunConcurrentTest { super.testNPX() }
+}
