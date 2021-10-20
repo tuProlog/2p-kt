ca3539ecb08ca8a712a64a3cd94fac85d59adf0e
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 15:25:19 +0200

    Added partial tests SubAtom

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentSubAtomImpl.kt
@@ -0,0 +1,56 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentSubAtomImpl :
+    TestConcurrentSubAtom<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomSubIsVar() = multiRunConcurrentTest { super.testSubAtomSubIsVar() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomSubIsVar2() = multiRunConcurrentTest { super.testSubAtomSubIsVar2() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomSubIsVar3() = multiRunConcurrentTest { super.testSubAtomSubIsVar3() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomDoubleVar4() = multiRunConcurrentTest { super.testSubAtomDoubleVar4() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomInstantiationError() = multiRunConcurrentTest { super.testSubAtomInstantiationError() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomTypeErrorAtomIsInteger() =
+        multiRunConcurrentTest { super.testSubAtomTypeErrorAtomIsInteger() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomTypeErrorSubIsInteger() =
+        multiRunConcurrentTest { super.testSubAtomTypeErrorSubIsInteger() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomTypeErrorBeforeIsNotInteger() =
+        multiRunConcurrentTest { super.testSubAtomTypeErrorBeforeIsNotInteger() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testSubAtomTypeErrorLengthIsNotInteger() =
+    //     multiRunConcurrentTest { super.testSubAtomTypeErrorLengthIsNotInteger() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testSubAtomTypeErrorAfterIsNotInteger() =
+        multiRunConcurrentTest { super.testSubAtomTypeErrorAfterIsNotInteger() }
+}
