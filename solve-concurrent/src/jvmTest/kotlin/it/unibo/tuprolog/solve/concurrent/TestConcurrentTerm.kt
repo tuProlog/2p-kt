7215cb4f2582b68ecf7cff385c41618a0480bdf4
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 15:24:44 +0200

    Added tests Term

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentTerm.kt
@@ -0,0 +1,333 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.SolverFactory
+import it.unibo.tuprolog.solve.no
+import it.unibo.tuprolog.solve.yes
+
+interface TestConcurrentTerm<T : WithAssertingEquals> : FromSequence<T>, SolverFactory {
+
+    fun testTermDiff() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "\\=="(intOf(1), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="(intOf(1), intOf(2))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="("X", intOf(1))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="("X", "Y")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="(`_`, `_`)
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="("X", "a"("X"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "\\=="("f"("a"), "f"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testTermEq() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "=="(intOf(1), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="(intOf(1), intOf(2))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="("X", intOf(1))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="("X", "Y")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="(`_`, `_`)
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="("X", "a"("X"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "=="("f"("a"), "f"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testTermGreaterThan() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "@>"(realOf(1.0), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("aardvark", "zebra")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("short", "short")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("short", "shorter")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("foo"("b"), "foo"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>"("foo"("a", "X"), "foo"("b", "Y"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testTermGreaterThanEq() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "@>="(realOf(1.0), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("aardvark", "zebra")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("short", "short")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("short", "shorter")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("foo"("b"), "foo"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@>="("foo"("a", "X"), "foo"("b", "Y"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testTermLessThan() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "@<"(realOf(1.0), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("aardvark", "zebra")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("short", "short")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("short", "shorter")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("foo"("b"), "foo"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@<"("foo"("a", "X"), "foo"("b", "Y"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+
+    fun testTermLessThanEq() {
+        prolog {
+            val solver = solverWithDefaultBuiltins()
+
+            var query = "@=<"(realOf(1.0), intOf(1))
+            var solutions = fromSequence(solver.solve(query, mediumDuration))
+            var expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"(realOf(1.1), realOf(1.1))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"(realOf(1.0), realOf(1.2))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"(intOf(1), intOf(1))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"(intOf(1), intOf(2))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("aardvark", "zebra")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("short", "short")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("short", "shorter")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("foo"("b"), "foo"("a"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.no())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("X", "X")
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+
+            query = "@=<"("foo"("a", "X"), "foo"("b", "Y"))
+            solutions = fromSequence(solver.solve(query, mediumDuration))
+            expected = fromSequence(query.yes())
+
+            expected.assertingEquals(solutions)
+        }
+    }
+}
---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentTermImpl.kt
@@ -0,0 +1,35 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentTermImpl :
+    TestConcurrentTerm<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermDiff() = multiRunConcurrentTest { super.testTermDiff() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermEq() = multiRunConcurrentTest { super.testTermEq() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermGreaterThan() = multiRunConcurrentTest { super.testTermGreaterThan() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermGreaterThanEq() = multiRunConcurrentTest { super.testTermGreaterThanEq() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermLessThan() = multiRunConcurrentTest { super.testTermLessThan() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTermLessThanEq() = multiRunConcurrentTest { super.testTermLessThanEq() }
+}
