8960bc95b6fc56bf0ebab2da3c6af4d376621691
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 13:04:21 +0200

    Added partial tests Repeat

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentRepeatImpl.kt
@@ -0,0 +1,15 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+// import kotlinx.coroutines.ExperimentalCoroutinesApi
+// import kotlin.test.Test
+
+class TestConcurrentRepeatImpl :
+    TestConcurrentRepeat<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testRepeat() = multiRunConcurrentTest { super.testRepeat() }
+}
