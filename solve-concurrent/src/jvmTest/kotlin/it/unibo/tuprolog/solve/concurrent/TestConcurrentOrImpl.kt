73558383dffe4d429a963b049bc8a548949f7eb5
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-20 13:03:51 +0200

    Added tests Or

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentOrImpl.kt
@@ -0,0 +1,31 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentOrImpl :
+    TestConcurrentOr<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testTrueOrFalse() = multiRunConcurrentTest { super.testTrueOrFalse() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testCutFalseOrTrue() = multiRunConcurrentTest { super.testCutFalseOrTrue() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testCutCall() = multiRunConcurrentTest { super.testCutCall() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testCutAssignedValue() = multiRunConcurrentTest { super.testCutAssignedValue() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testOrDoubleAssignment() = multiRunConcurrentTest { super.testOrDoubleAssignment() }
+}
