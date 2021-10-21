8812dbda5ba46ebcbfaadc4d386f3ca2fc262fde
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-21 15:52:18 +0200

    Added partial tests CustomData

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentCustomDataImpl.kt
@@ -0,0 +1,27 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+
+class TestConcurrentCustomDataImpl :
+    TestConcurrentCustomData<MultiSet>,
+    SolverFactory by ConcurrentSolverFactory,
+    FromSequence<MultiSet> by ConcurrentFromSequence {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testApi() = multiRunConcurrentTest { super.testApi() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testEphemeralData() = multiRunConcurrentTest { super.testEphemeralData() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    override fun testDurableData() = multiRunConcurrentTest { super.testDurableData() }
+
+    // @OptIn(ExperimentalCoroutinesApi::class)
+    // @Test
+    // override fun testPersistentData() = multiRunConcurrentTest { super.testPersistentData() }
+}
