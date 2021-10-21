6dc6a520ea439d379e478be7906368775a524a2e
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-21 16:21:31 +0200

    Added tests SolutionPresentation

---
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentSolutionPresentation.kt
@@ -0,0 +1,26 @@
+package it.unibo.tuprolog.solve.concurrent
+
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.dsl.theory.prolog
+import it.unibo.tuprolog.solve.Solution
+import it.unibo.tuprolog.solve.SolverFactory
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+interface TestConcurrentSolutionPresentation<T : WithAssertingEquals> : FromSequence<T>, SolverFactory {
+
+    fun testSolutionWithDandlingVars() {
+        prolog {
+            val theory = theoryOf(
+                fact { "append"("seq"(X), X) }
+            )
+            val solver = solverOf(staticKb = theory)
+            val query = "append"(A, B)
+            val sol = solver.solveOnce(query)
+            assertTrue { sol is Solution.Yes }
+            assertTrue { ktSetOf(A, B).all { it in sol.substitution.keys } }
+            assertTrue { sol.substitution[A]!! matches "seq"(`_`) }
+            assertEquals("X", (sol.substitution[B] as? Var)?.name)
+        }
+    }
+}
