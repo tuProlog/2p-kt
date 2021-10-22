2a177c64b79aaf422f4b10e812bfa5f981868823
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-10-22 11:20:17 +0200

    support limits in concurrent resolution via coroutine cancellation

---
+++ solve-concurrent/src/jsMain/kotlin/it/unibo/tuprolog/solve/concurrent/AtomicIntJs.kt
@@ -0,0 +1,39 @@
+package it.unibo.tuprolog.solve.concurrent
+
+class AtomicIntJs(override var value: Int) : AtomicInt {
+
+    override fun plusAssign(delta: Int) {
+        value += delta
+    }
+
+    override fun minusAssign(delta: Int) {
+        value -= delta
+    }
+
+    override fun incAndGet(delta: Int): Int {
+        value += delta
+        return value
+    }
+
+    override fun decAndGet(delta: Int): Int {
+        value -= delta
+        return value
+    }
+
+    override fun toString(): String = "AtomicIntJs(value=$value)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class.js != other::class.js) return false
+
+        other as AtomicIntJs
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = value
+}
+
+internal actual fun atomicInt(value: Int): AtomicInt = AtomicIntJs(value)
