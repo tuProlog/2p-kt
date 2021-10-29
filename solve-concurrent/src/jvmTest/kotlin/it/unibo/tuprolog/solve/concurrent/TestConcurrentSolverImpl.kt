57eb1c615c9b1cdcef34c661f93429bde6e79199
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-29 12:45:14 +0200

    Added single ignore with filing cause and some utility

--- solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentSolverImpl.kt
+++ solve-concurrent/src/jvmTest/kotlin/it/unibo/tuprolog/solve/concurrent/TestConcurrentSolverImpl.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.test.Ignore
 import kotlin.test.Test
 
-@Ignore
 class TestConcurrentSolverImpl :
     TestConcurrentSolver<MultiSet>,
     SolverFactory by ConcurrentSolverFactory,
@@ -28,6 +27,7 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Side effects need to be implemented
     override fun testSideEffectsPersistentAfterBacktracking1() = multiRunConcurrentTest {
         super.testSideEffectsPersistentAfterBacktracking1()
     }
@@ -40,18 +40,21 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Assert need to be implemented
     override fun testAssert() = multiRunConcurrentTest {
         super.testAssert()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Assert need to be implemented
     override fun testAssertZ() = multiRunConcurrentTest {
         super.testAssertZ()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Assert need to be implemented
     override fun testAssertA() = multiRunConcurrentTest {
         super.testAssertA()
     }
@@ -76,48 +79,56 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testIfThen1() = multiRunConcurrentTest {
         super.testIfThen1()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testIfThen2() = multiRunConcurrentTest {
         super.testIfThen2()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testIfThenElse1() = multiRunConcurrentTest {
         super.testIfThenElse1()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testIfThenElse2() = multiRunConcurrentTest {
         super.testIfThenElse2()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // unhandled sleep
     override fun testTimeout1() = multiRunConcurrentTest {
         super.testTimeout1()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // unhandled sleep
     override fun testTimeout2() = multiRunConcurrentTest {
         super.testTimeout2()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // unhandled sleep
     override fun testTimeout3() = multiRunConcurrentTest {
         super.testTimeout3()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // unhandled sleep
     override fun testTimeout4() = multiRunConcurrentTest {
         super.testTimeout4()
     }
@@ -130,43 +141,49 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testSimpleCutAlternatives() = multiRunConcurrentTest {
         super.testSimpleCutAlternatives()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testCutAndConjunction() = multiRunConcurrentTest {
         super.testCutAndConjunction()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testCutConjunctionAndBacktracking() = multiRunConcurrentTest {
         super.testCutConjunctionAndBacktracking()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    @Ignore // does not terminate
+    @Ignore // timeout need to be implemented, does not terminate
     override fun testMaxDurationParameterAndTimeOutException() = multiRunConcurrentTest {
         super.testMaxDurationParameterAndTimeOutException()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // if is nondeterministic
     override fun testPrologStandardSearchTreeExample() = multiRunConcurrentTest {
         super.testPrologStandardSearchTreeExample()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testPrologStandardSearchTreeWithCutExample() = multiRunConcurrentTest {
         super.testPrologStandardSearchTreeWithCutExample()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testBacktrackingWithCustomReverseListImplementation() = multiRunConcurrentTest {
         super.testBacktrackingWithCustomReverseListImplementation()
     }
@@ -179,60 +196,70 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testConjunctionProperties() = multiRunConcurrentTest {
         super.testConjunctionProperties()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testCallPrimitive() = multiRunConcurrentTest {
         super.testCallPrimitive()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Cut need to be implemented
     override fun testCallPrimitiveTransparency() = multiRunConcurrentTest {
         super.testCallPrimitiveTransparency()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // same solution count does not match (probably correct)
     override fun testCatchPrimitive() = multiRunConcurrentTest {
         super.testCatchPrimitive()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // solutions do not match (probably correct)
     override fun testCatchPrimitiveTransparency() = multiRunConcurrentTest {
         super.testCatchPrimitiveTransparency()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // todo check needed, solutions appear equals but tests fail
     override fun testHaltPrimitive() = multiRunConcurrentTest {
         super.testHaltPrimitive()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // todo check needed, solution is not halt but unknown cause
     override fun testNotPrimitive() = multiRunConcurrentTest {
         super.testNotPrimitive()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // todo check needed, solutions do not match
     override fun testNotModularity() = multiRunConcurrentTest {
         super.testNotModularity()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Arrow need to be implemented
     override fun testIfThenRule() = multiRunConcurrentTest {
         super.testIfThenRule()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Arrow need to be implemented
     override fun testIfThenElseRule() = multiRunConcurrentTest {
         super.testIfThenElseRule()
     }
@@ -257,6 +284,7 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // some API need to be implemented (abolish, assert, etc.)
     override fun testBuiltinApi() = multiRunConcurrentTest {
         super.testBuiltinApi()
     }
@@ -293,42 +321,49 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // backtracking is not present in concurrent solver, probably useless test
     override fun testBasicBacktracking1() = multiRunConcurrentTest {
         super.testBasicBacktracking1()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // backtracking is not present in concurrent solver, probably useless test
     override fun testBasicBacktracking2() = multiRunConcurrentTest {
         super.testBasicBacktracking2()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // backtracking is not present in concurrent solver, probably useless test
     override fun testBasicBacktracking3() = multiRunConcurrentTest {
         super.testBasicBacktracking3()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // backtracking is not present in concurrent solver, probably useless test
     override fun testBasicBacktracking4() = multiRunConcurrentTest {
         super.testBasicBacktracking4()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Assert need to be implemented
     override fun testAssertRules() = multiRunConcurrentTest {
         super.testAssertRules()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Retract need to be implemented
     override fun testRetract() = multiRunConcurrentTest {
         super.testRetract()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // todo check needed, more solutions than expected
     override fun testNatural() = multiRunConcurrentTest {
         super.testNatural()
     }
@@ -347,12 +382,14 @@ class TestConcurrentSolverImpl :
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Retract need to be implemented
     override fun testRetractAll() = multiRunConcurrentTest {
         super.testRetractAll()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    @Ignore // Append need to be implemented
     override fun testAppend() = multiRunConcurrentTest {
         super.testAppend()
     }
