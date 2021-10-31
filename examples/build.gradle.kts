7e9a26b24f5f4968f5c8b08f92e2c9a2f13dc19c
Andrea <andrea.giordano5@studio.unibo.it>
2021-10-31 12:26:24 +0100

    Added NQueens example for performance test

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     api(project(":solve-classic"))
+    api(project(":solve-concurrent"))
     api(project(":dsl-theory"))
     api(project(":parser-theory"))
     api(project(":io-lib"))
