384c6081499a9168c3902942df694fcd98ca4665
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-06-19 19:09:31 +0200

    fix: adapt tests involving kt-math to new API

--- dsl-core/src/commonTest/kotlin/it/unibo/tuprolog/dsl/PlatformSpecificValues.kt
+++ dsl-core/src/commonTest/kotlin/it/unibo/tuprolog/dsl/PlatformSpecificValues.kt
@@ -3,6 +3,7 @@ package it.unibo.tuprolog.dsl
 import it.unibo.tuprolog.core.Numeric
 
 expect object PlatformSpecificValues {
+    val THREE_POINT_ONE_DOUBLE: Numeric
     val THREE_POINT_ONE_FLOAT: Numeric
     val ONE_POINT_ZERO: Numeric
     val MINUS_THREE: Numeric
