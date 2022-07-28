4c8b7c5a15e74462937bf5d590a06d25d34242c8
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-07-28 16:00:11 +0200

    feat: add libraryOf and runtimeOf facility methods

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/Utils.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/Utils.kt
@@ -7,10 +7,18 @@ import it.unibo.tuprolog.core.Directive
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.core.operators.Operator
 import it.unibo.tuprolog.core.operators.OperatorSet
+import it.unibo.tuprolog.solve.function.FunctionWrapper
+import it.unibo.tuprolog.solve.function.LogicFunction
 import it.unibo.tuprolog.solve.library.Library
 import it.unibo.tuprolog.solve.library.Runtime
+import it.unibo.tuprolog.solve.primitive.Primitive
+import it.unibo.tuprolog.solve.primitive.PrimitiveWrapper
+import it.unibo.tuprolog.solve.rule.RuleWrapper
+import it.unibo.tuprolog.theory.MutableTheory
 import it.unibo.tuprolog.theory.Theory
+import kotlin.js.JsName
 import kotlin.jvm.JvmName
+import kotlin.jvm.JvmOverloads
 
 /** Performs the given [action] on each element, giving a lookahead hint (i.e. if there's another element to process after). */
 inline fun <T> Iterator<T>.forEachWithLookahead(action: (T, Boolean) -> Unit) {
@@ -51,4 +59,28 @@ fun getAllOperators(libraries: Runtime, vararg theories: Theory): Sequence<Opera
 
 fun Sequence<Operator>.toOperatorSet(): OperatorSet {
     return OperatorSet(this)
+}
+
+@JvmOverloads
+@JsName("libraryOf")
+fun libraryOf(alias: String? = null, item1: AbstractWrapper<*>, vararg items: AbstractWrapper<*>): Library {
+    val theory = MutableTheory.empty()
+    val primitives = mutableMapOf<Signature, Primitive>()
+    val functions = mutableMapOf<Signature, LogicFunction>()
+    for (item in arrayOf(item1, *items)) {
+        when (item) {
+            is PrimitiveWrapper<*> -> primitives += item.descriptionPair
+            is FunctionWrapper<*> -> functions += item.descriptionPair
+            is RuleWrapper<*> -> theory.assertZ(item.implementation)
+            else -> throw NotImplementedError("Cannot handle wrappers of type ${item::class}")
+        }
+    }
+    val library = Library.of(primitives, theory, OperatorSet.EMPTY, functions)
+    return alias?.let { Library.of(it, library) } ?: library
+}
+
+@JvmOverloads
+@JsName("runtimeOf")
+fun runtimeOf(alias: String? = null, item1: AbstractWrapper<*>, vararg items: AbstractWrapper<*>): Runtime {
+    return libraryOf(alias, item1, *items).toRuntime()
 }
