82a7835fe275bc24801ba64235891b163ec9b674
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-08-10 10:58:47 +0200

    feat(test): add creation tests for all solve-* module, expect solve-concurrent

---
+++ solve-streams/src/jvmTest/java/it/unibo/tuprolog/solve/TestJavaConstructionForSolveStreams.java
@@ -0,0 +1,33 @@
+package it.unibo.tuprolog.solve;
+
+import org.junit.Test;
+
+public class TestJavaConstructionForSolveStreams extends TestJavaConstruction {
+    public TestJavaConstructionForSolveStreams() {
+        super(SolveStreamsTest.expectations);
+    }
+
+    @Override
+    @Test
+    public void testClassicFactory() {
+        super.testClassicFactory();
+    }
+
+    @Override
+    @Test
+    public void testStreamsFactory() {
+        super.testStreamsFactory();
+    }
+
+    @Override
+    @Test
+    public void testProblogFactory() {
+        super.testProblogFactory();
+    }
+
+    @Override
+    @Test
+    public void testPrologFactory() {
+        super.testPrologFactory();
+    }
+}
