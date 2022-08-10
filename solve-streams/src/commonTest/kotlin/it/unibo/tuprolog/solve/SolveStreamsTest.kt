9756c6ae292e733aa5b197135d5f366c9db5ca5e
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-08-10 10:58:47 +0200

    feat(test): add creation tests for all solve-* module, expect solve-concurrent

---
+++ solve-streams/src/commonTest/kotlin/it/unibo/tuprolog/solve/SolveStreamsTest.kt
@@ -0,0 +1,10 @@
+package it.unibo.tuprolog.solve
+
+import kotlin.jvm.JvmField
+
+object SolveStreamsTest {
+    @JvmField
+    val expectations = Expectations(
+        streamsShouldWork = true
+    )
+}
