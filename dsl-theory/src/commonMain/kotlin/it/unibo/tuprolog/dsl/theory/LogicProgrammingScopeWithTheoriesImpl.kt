50a9504cf468ffc8c2542468934625f96c9db0a5
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-08-11 16:56:15 +0200

    fix(style): format code

--- dsl-theory/src/commonMain/kotlin/it/unibo/tuprolog/dsl/theory/LogicProgrammingScopeWithTheoriesImpl.kt
+++ dsl-theory/src/commonMain/kotlin/it/unibo/tuprolog/dsl/theory/LogicProgrammingScopeWithTheoriesImpl.kt
@@ -3,5 +3,6 @@ package it.unibo.tuprolog.dsl.theory
 import it.unibo.tuprolog.dsl.unify.LogicProgrammingScopeWithUnification
 import it.unibo.tuprolog.unify.Unificator
 
-class LogicProgrammingScopeWithTheoriesImpl(unificator: Unificator) : LogicProgrammingScopeWithTheories,
+class LogicProgrammingScopeWithTheoriesImpl(unificator: Unificator) :
+    LogicProgrammingScopeWithTheories,
     LogicProgrammingScopeWithUnification by LogicProgrammingScopeWithUnification.of(unificator)
