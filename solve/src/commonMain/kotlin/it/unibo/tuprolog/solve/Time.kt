39ef87acc784688a7322b750905d8c9262e60679
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-09-12 18:41:38 +0200

    feat: TimeUnit in :solve

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/Time.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/Time.kt
@@ -14,3 +14,24 @@ typealias TimeDuration = Long
 /** A function returning current Time instant */
 @JsName("currentTimeInstant")
 expect fun currentTimeInstant(): TimeInstant
+
+enum class TimeUnit(private val millis: Long) {
+    MILLIS(1L),
+    SECONDS(1000L),
+    MINUTES(60L),
+    HOURS(60L),
+    DAYS(24),
+    WEEKS(7),
+    YEARS(365);
+
+    @JvmName("toDuration")
+    fun toDuration(): TimeDuration =
+        (sequenceOf(MILLIS, SECONDS, MINUTES, HOURS, DAYS) + if (this == YEARS) YEARS else WEEKS)
+            .takeWhile { it.ordinal <= this.ordinal }
+            .map { it.millis }
+            .reduce(TimeDuration::times)
+}
+
+operator fun TimeDuration.times(unit: TimeUnit): TimeDuration = this * unit.toDuration()
+
+operator fun Int.times(unit: TimeUnit): TimeDuration = this.toLong() * unit
