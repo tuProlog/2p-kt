f724f505fc378b22e483bc012731fa7723b10295
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-10 17:29:14 +0200

    fix: make findall/3, bagof/3, and setof/3 respect the timeout

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/FindAll.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/FindAll.kt
@@ -1,26 +1,23 @@
 package it.unibo.tuprolog.solve.stdlib.primitive
 
-import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.Substitution
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.solve.ExecutionContext
-import it.unibo.tuprolog.solve.Solution
 import it.unibo.tuprolog.solve.primitive.Solve
-import it.unibo.tuprolog.solve.primitive.TernaryRelation
 import it.unibo.tuprolog.unify.Unificator.Companion.mguWith
 import it.unibo.tuprolog.core.List as LogicList
 
-object FindAll : TernaryRelation.NonBacktrackable<ExecutionContext>("findall") {
-    override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term, third: Term): Solve.Response {
+object FindAll : AbstractCollectingPrimitive("findall") {
+
+    override fun Solve.Request<ExecutionContext>.computeAllSubstitutions(
+        first: Term,
+        second: Term,
+        third: Term
+    ): Sequence<Substitution> {
         ensuringArgumentIsInstantiated(1)
         ensuringArgumentIsCallable(1)
-        val solutions = solve(second as Struct).toList()
-        val error = solutions.asSequence().filterIsInstance<Solution.Halt>().firstOrNull()
-        if (error != null) {
-            return replyException(error.exception.pushContext(context))
-        }
-        val mapped = solutions.asSequence()
-            .filterIsInstance<Solution.Yes>()
-            .map { first[it.substitution].freshCopy() }
-        return replyWith(third mguWith LogicList.of(mapped))
+        val solutions = computeIntermediateSolutions(second.castToStruct())
+        val mapped = solutions.map { first[it.substitution].freshCopy() }
+        return sequenceOf(third mguWith LogicList.of(mapped))
     }
 }
