01db59311636e6d06eb062d49534b4c528d75ab5
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-12 14:40:28 +0200

    feat(test): adapt DummyExecutionContext in :gui tests

--- gui/src/commonTest/kotlin/it/unibo/tuprolog/ui/gui/DummyExecutionContext.kt
+++ gui/src/commonTest/kotlin/it/unibo/tuprolog/ui/gui/DummyExecutionContext.kt
@@ -3,7 +3,11 @@ package it.unibo.tuprolog.ui.gui
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.core.Substitution
 import it.unibo.tuprolog.core.operators.OperatorSet
-import it.unibo.tuprolog.solve.*
+import it.unibo.tuprolog.solve.ExecutionContext
+import it.unibo.tuprolog.solve.MutableSolver
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.solve.TimeDuration
+import it.unibo.tuprolog.solve.TimeInstant
 import it.unibo.tuprolog.solve.channel.InputStore
 import it.unibo.tuprolog.solve.channel.OutputStore
 import it.unibo.tuprolog.solve.data.CustomDataStore
