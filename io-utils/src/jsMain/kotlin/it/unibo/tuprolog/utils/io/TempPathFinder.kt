e687f18b0e0598d7247de63a67f391c4f4295157
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-23 20:03:06 +0200

    feat: temp files with random tag on js

--- io-utils/src/jsMain/kotlin/it/unibo/tuprolog/utils/io/TempPathFinder.kt
+++ io-utils/src/jsMain/kotlin/it/unibo/tuprolog/utils/io/TempPathFinder.kt
@@ -2,11 +2,22 @@ package it.unibo.tuprolog.utils.io
 
 import it.unibo.tuprolog.Platform
 import it.unibo.tuprolog.currentPlatform
+import kotlinx.browser.window
 
 internal actual object TempPathFinder {
     private val os by lazy {
         js("require('os')")
     }
+
+    private val math by lazy {
+        if (currentPlatform() == Platform.BROWSER) {
+            window.asDynamic().Math
+        } else {
+            js("Math")
+        }
+    }
+
+    private fun randomTag() = math.random().toString().replace(".", "")
 
     actual val tempDirectory: String
         get() = if (currentPlatform() == Platform.NODE) {
@@ -16,8 +27,8 @@ internal actual object TempPathFinder {
         }
 
     actual fun file(name: String, extension: String): String =
-        "$tempDirectory/$name.$extension"
+        "$tempDirectory/$name${randomTag()}.$extension"
 
     actual fun directory(name: String): String =
-        "$tempDirectory/$name"
+        "$tempDirectory/$name${randomTag()}"
 }
