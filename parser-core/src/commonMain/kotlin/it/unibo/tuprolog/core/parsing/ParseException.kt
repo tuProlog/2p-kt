8830910b558cbe601b872a00623944dc35458d02
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-23 18:57:41 +0200

    feat: equals and hashCode for it.unibo.tuprolog.core.parsing.ParseException

--- parser-core/src/commonMain/kotlin/it/unibo/tuprolog/core/parsing/ParseException.kt
+++ parser-core/src/commonMain/kotlin/it/unibo/tuprolog/core/parsing/ParseException.kt
@@ -23,4 +23,28 @@ open class ParseException(
             ", offendingSymbol='" + offendingSymbol + '\'' +
             '}'
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as ParseException
+
+        if (input != other.input) return false
+        if (offendingSymbol != other.offendingSymbol) return false
+        if (line != other.line) return false
+        if (column != other.column) return false
+        if (clauseIndex != other.clauseIndex) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = input?.hashCode() ?: 0
+        result = 31 * result + offendingSymbol.hashCode()
+        result = 31 * result + line
+        result = 31 * result + column
+        result = 31 * result + clauseIndex
+        return result
+    }
 }
