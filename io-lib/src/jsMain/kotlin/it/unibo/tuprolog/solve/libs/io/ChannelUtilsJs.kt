e6556e9b5b0abc5d4ab26e438cf2d64e26b34f6e
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-25 11:20:10 +0200

    feat: support parsing terms from input channels on js as well

--- io-lib/src/jsMain/kotlin/it/unibo/tuprolog/solve/libs/io/ChannelUtilsJs.kt
+++ io-lib/src/jsMain/kotlin/it/unibo/tuprolog/solve/libs/io/ChannelUtilsJs.kt
@@ -2,9 +2,22 @@ package it.unibo.tuprolog.solve.libs.io
 
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.core.operators.OperatorSet
+import it.unibo.tuprolog.core.parsing.TermParser
 import it.unibo.tuprolog.solve.channel.InputChannel
-import it.unibo.tuprolog.utils.io.exceptions.IOException
+import it.unibo.tuprolog.solve.channel.impl.InputChannelFromString
+import kotlin.jvm.Synchronized
 
+@Synchronized
 actual fun InputChannel<String>.asTermChannel(operators: OperatorSet): InputChannel<Term> {
-    throw IOException("Reading terms is still not supported for JS")
+    if (this !is InputChannelFromString) {
+        throw IllegalStateException("Channel $streamTerm does not supporting reading terms, as it is of type ${this::class.simpleName}")
+    }
+    return if (containsExtension(KEY)) {
+        getExtension(KEY)!!
+    } else {
+        val termIterator = TermParser.withOperators(operators).parseTerms(string).iterator()
+        val termChannel = InputChannel.of(termIterator::next, termIterator::hasNext)
+        setExtension(KEY, termChannel)
+        termChannel
+    }
 }
--- io-lib/src/jvmMain/kotlin/it/unibo/tuprolog/solve/libs/io/ChannelUtilsJvm.kt
+++ io-lib/src/jvmMain/kotlin/it/unibo/tuprolog/solve/libs/io/ChannelUtilsJvm.kt
@@ -5,21 +5,18 @@ import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.core.parsing.TermReader
 import it.unibo.tuprolog.solve.channel.InputChannel
 import it.unibo.tuprolog.solve.channel.ReaderChannel
-import java.util.WeakHashMap
-
-private val cache = WeakHashMap<InputChannel<String>, InputChannel<Term>>()
 
 @Synchronized
 actual fun InputChannel<String>.asTermChannel(operators: OperatorSet): InputChannel<Term> {
     if (this !is ReaderChannel) {
         throw IllegalStateException("Channel $streamTerm does not supporting reading terms, as it is of type ${this::class.simpleName}")
     }
-    return if (cache.containsKey(this)) {
-        cache[this]!!
+    return if (containsExtension(KEY)) {
+        getExtension(KEY)!!
     } else {
         val termIterator = TermReader.withOperators(operators).readTerms(reader).iterator()
         val termChannel = InputChannel.of(termIterator::next, termIterator::hasNext)
-        cache[this] = termChannel
+        setExtension(KEY, termChannel)
         termChannel
     }
 }
