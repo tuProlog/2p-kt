f989f0930bc07e45ce5adba68b380f6987831172
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-11-29 10:10:31 +0100

    fix: avoid using default unificator in '\='/2

--- solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/NotUnifiableWith.kt
+++ solve/src/commonMain/kotlin/it/unibo/tuprolog/solve/stdlib/primitive/NotUnifiableWith.kt
@@ -4,10 +4,9 @@ import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.primitive.BinaryRelation
 import it.unibo.tuprolog.solve.primitive.Solve
-import it.unibo.tuprolog.unify.Unificator.Companion.matches
 
 /** Implementation of '\='/2 predicate */
 object NotUnifiableWith : BinaryRelation.Predicative<ExecutionContext>("\\=") {
     override fun Solve.Request<ExecutionContext>.compute(first: Term, second: Term): Boolean =
-        (first matches second).not()
+        !match(first, second)
 }
