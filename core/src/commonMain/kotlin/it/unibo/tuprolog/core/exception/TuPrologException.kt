9c082d836a3749214aa7bebdd56874a9af0d9e6a
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-01-02 20:07:51 +0100

    fix: TuPrologException (and subclasses) instantiation succeeds

--- core/src/commonMain/kotlin/it/unibo/tuprolog/core/exception/TuPrologException.kt
+++ core/src/commonMain/kotlin/it/unibo/tuprolog/core/exception/TuPrologException.kt
@@ -1,12 +1,7 @@
 package it.unibo.tuprolog.core.exception
 
-import kotlin.jvm.JvmOverloads
-
 /**
  * Base class for all `tuProlog` related exceptions
- *
- * @param message the detail message string.
- * @param cause the cause of this exception.
  */
 open class TuPrologException : RuntimeException {
     constructor(message: String?, cause: Throwable?) : super(message, cause)
