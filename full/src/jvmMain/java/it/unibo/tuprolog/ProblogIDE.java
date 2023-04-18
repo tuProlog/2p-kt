0c07d677cb3926b6bc3e632199a1b78392880941
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-04-18 18:50:13 +0200

    chore: disable projects :ide and :ide-plp

--- full/src/jvmMain/java/it/unibo/tuprolog/ProblogIDE.java
+++ full/src/jvmMain/java/it/unibo/tuprolog/ProblogIDE.java
@@ -1,9 +1,9 @@
 package it.unibo.tuprolog;
 
-import it.unibo.tuprolog.ui.gui.PLPMain;
+//import it.unibo.tuprolog.ui.gui.PLPMain;
 
 public class ProblogIDE {
     public static void main(String[] args) {
-        PLPMain.main(args);
+//        PLPMain.main(args);
     }
 }
--- full/src/jvmMain/java/it/unibo/tuprolog/PrologIDE.java
+++ full/src/jvmMain/java/it/unibo/tuprolog/PrologIDE.java
@@ -1,9 +1,9 @@
 package it.unibo.tuprolog;
 
-import it.unibo.tuprolog.ui.gui.Main;
+//import it.unibo.tuprolog.ui.gui.Main;
 
 public class PrologIDE {
     public static void main(String[] args) {
-        Main.main(args);
+//        Main.main(args);
     }
 }
