87bb90f55f658f6045592031ee97f63d9b78da84
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2023-04-27 22:37:20 +0200

    Added Redux bindings to web application state variables, disabled most of Main.kt functionalities

--- ide-web/src/main/kotlin/it/unibo/tuprolog/ide/web/Main.kt
+++ ide-web/src/main/kotlin/it/unibo/tuprolog/ide/web/Main.kt
@@ -1,6 +1,10 @@
 package it.unibo.tuprolog.ide.web
 
-import js.uri.encodeURIComponent
+import State
+import Counter
+import EditorTab
+import OnFileLoad
+import TuProlog
 import mui.lab.TabContext
 import mui.lab.TabPanel
 import mui.material.*
@@ -9,11 +13,10 @@ import react.*
 import react.dom.client.createRoot
 import react.dom.html.ReactHTML
 import react.redux.Provider
-import testTuprolog
+import react.redux.useDispatch
+import react.redux.useSelector
 import web.dom.document
 import web.html.HTML
-import kotlin.js.Date
-
 
 fun main() {
     val root = document.createElement(HTML.div)
@@ -21,8 +24,6 @@ fun main() {
     createRoot(root)
         .render(Root.create())
 }
-
-class EditorTab(var fileName: String, var editorValue: String)
 
 val Root = FC<Props> {
     Provider {
@@ -33,36 +34,26 @@ val Root = FC<Props> {
 
 
 val App = FC<Props> {
-    var editorSelectedTab by useState("")
-    val editorTabs by useState(mutableListOf<EditorTab>())
 
     var isErrorAlertOpen by useState(false)
     var errorAlertMessage by useState("")
-
-    fun addNewEditor(content: String = "") {
-        val fileName: String = "undefined_" + Date().getTime() + ".pl"
-        editorTabs.add(
-            EditorTab(
-                fileName, content.trimIndent()
-            )
-        )
-
-        editorSelectedTab = fileName
-    }
+    val editorSelectedTab = useSelector<State, String> { s -> s.tuProlog.editorSelectedTab }
+    val editorTabs = useSelector<State, List<EditorTab>> { s -> s.tuProlog.editorTabs }
+    val dispatcher = useDispatch<OnFileLoad, TuProlog>()
 
     useEffectOnce {
-        addNewEditor("""
-            % member2(List, Elem, ListWithoutElem)
-            member2([X|Xs],X,Xs).
-            member2([X|Xs],E,[X|Ys]):-member2(Xs,E,Ys).
-            % permutation(Ilist, Olist)
-            permutation([],[]).
-            permutation(Xs, [X | Ys]) :-
-            member2(Xs,X,Zs),
-            permutation(Zs, Ys).
-
-            % permutation([10,20,30],L).
-        """)
+//        addNewEditor("""
+//            % member2(List, Elem, ListWithoutElem)
+//            member2([X|Xs],X,Xs).
+//            member2([X|Xs],E,[X|Ys]):-member2(Xs,E,Ys).
+//            % permutation(Ilist, Olist)
+//            permutation([],[]).
+//            permutation(Xs, [X | Ys]) :-
+//            member2(Xs,X,Zs),
+//            permutation(Zs, Ys).
+//
+//            % permutation([10,20,30],L).
+//        """)
 
 //        addNewEditor("""
 //            nat(z).
@@ -82,59 +73,61 @@ val App = FC<Props> {
         Stack {
             NavBar {
                 onFileLoad = { fileName: String, editorValue: String ->
-                    if (editorTabs.find { it.fileName == fileName } == null) {
-                        editorTabs.add(EditorTab(fileName, editorValue))
-                    } else {
-                        errorAlertMessage = "File already exists"
-                        isErrorAlertOpen = true
-                    }
-                    editorSelectedTab = fileName
+                    console.log(fileName, editorValue)
+                    dispatcher(OnFileLoad(fileName, editorValue))
+//                    if (editorTabs.find { it.fileName == fileName } == null) {
+//                        editorTabs.add(EditorTab(fileName, editorValue))
+//                    } else {
+//                        errorAlertMessage = "File already exists"
+//                        isErrorAlertOpen = true
+//                    }
+//                    editorSelectedTab = fileName
                 }
                 onAddEditor = {
-                    addNewEditor()
+//                    addNewEditor()
                 }
                 onCloseEditor = {
-                    if (editorTabs.size > 1) {
-                        // find the deletable tab panel index
-                        val index = editorTabs.indexOfFirst { it.fileName == editorSelectedTab }
-                        editorTabs.removeAt(index)
-                        // select new ide
-                        if (index == 0)
-                            editorSelectedTab = editorTabs[index].fileName
-                        else
-                            editorSelectedTab = editorTabs[index - 1].fileName
-                    }
+//                    if (editorTabs.size > 1) {
+//                        // find the deletable tab panel index
+//                        val index = editorTabs.indexOfFirst { it.fileName == editorSelectedTab }
+//                        editorTabs.removeAt(index)
+//                        // select new ide
+//                        if (index == 0)
+//                            editorSelectedTab = editorTabs[index].fileName
+//                        else
+//                            editorSelectedTab = editorTabs[index - 1].fileName
+//                    }
                 }
                 onDownloadTheory = {
-                    val editorText = editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue ?: ""
-                    if (editorText != "") {
-                        val elem = document.createElement(HTML.a)
-                        elem.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(editorText))
-                        elem.setAttribute("download", editorSelectedTab)
-                        elem.click()
-                        isErrorAlertOpen = false
-                    } else {
-                        errorAlertMessage = "No theory specified"
-                        isErrorAlertOpen = true
-                    }
+//                    val editorText = editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue ?: ""
+//                    if (editorText != "") {
+//                        val elem = document.createElement(HTML.a)
+//                        elem.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(editorText))
+//                        elem.setAttribute("download", editorSelectedTab)
+//                        elem.click()
+//                        isErrorAlertOpen = false
+//                    } else {
+//                        errorAlertMessage = "No theory specified"
+//                        isErrorAlertOpen = true
+//                    }
                 }
 
                 currentFileName = editorSelectedTab
 
                 onRenameEditor = { it ->
-                    val isOk: EditorTab? = editorTabs.find { it3 -> it3.fileName == it }
-                    if (isOk == null) {
-                        val indexForRename = editorTabs.indexOfFirst { it3 -> it3.fileName == editorSelectedTab }
-                        editorTabs[indexForRename].fileName = it
-                        editorSelectedTab = editorTabs[indexForRename].fileName
-                        isErrorAlertOpen = false
-                    } else {
-                        errorAlertMessage = if (it != editorSelectedTab)
-                            "Cannot rename file. A file with this name already exists"
-                        else
-                            "Cannot rename file with the same value"
-                        isErrorAlertOpen = true
-                    }
+//                    val isOk: EditorTab? = editorTabs.find { it3 -> it3.fileName == it }
+//                    if (isOk == null) {
+//                        val indexForRename = editorTabs.indexOfFirst { it3 -> it3.fileName == editorSelectedTab }
+//                        editorTabs[indexForRename].fileName = it
+//                        editorSelectedTab = editorTabs[indexForRename].fileName
+//                        isErrorAlertOpen = false
+//                    } else {
+//                        errorAlertMessage = if (it != editorSelectedTab)
+//                            "Cannot rename file. A file with this name already exists"
+//                        else
+//                            "Cannot rename file with the same value"
+//                        isErrorAlertOpen = true
+//                    }
                 }
 
             }
@@ -146,7 +139,7 @@ val App = FC<Props> {
                     variant = TabsVariant.scrollable
                     scrollButtons = TabsScrollButtons.auto
                     onChange = { _, newValue ->
-                        editorSelectedTab = newValue as String
+//                        editorSelectedTab = newValue as String
                     }
 
                     editorTabs.forEach {
@@ -165,7 +158,7 @@ val App = FC<Props> {
                             value = it.editorValue
                             height = "63vh"
                             onChange = {
-                                editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue = it
+//                                editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue = it
                             }
                             beforeMount = {
                             }
@@ -188,9 +181,9 @@ val App = FC<Props> {
 
             QueryEditor {
                 onSolve = {
-                    val editorTheory = editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue ?: ""
-                    val editorQuery = it
-                    testTuprolog(editorTheory, editorQuery)
+//                    val editorTheory = editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue ?: ""
+//                    val editorQuery = it
+//                    testTuprolog(editorTheory, editorQuery)
                 }
             }
 
--- ide-web/src/main/kotlin/it/unibo/tuprolog/ide/web/utils/Redux.kt
+++ ide-web/src/main/kotlin/it/unibo/tuprolog/ide/web/utils/Redux.kt
@@ -1,59 +1,86 @@
 import react.Reducer
-import react.redux.Provider
-import react.redux.useDispatch
-import react.redux.useSelector
 import redux.RAction
-import redux.compose
 import redux.createStore
 import redux.rEnhancer
+import kotlin.js.Date
 import kotlin.reflect.KProperty1
 
 // Utils
 fun <S, A, R> combineReducers(reducers: Map<KProperty1<S, R>, Reducer<*, A>>): Reducer<S, A> {
     return redux.combineReducers(reducers.mapKeys { it.key.name })
 }
+class EditorTab(var fileName: String, var editorValue: String)
+
 
 // Stato
 data class Counter(var count: Int)
+data class TuProlog(var editorSelectedTab: String, var editorQuery: String, var editorTabs: MutableList<EditorTab>)
 
 data class State(
-    val counter: Counter = Counter(10)
-
-
-
+    val counter: Counter,
+    var tuProlog: TuProlog,
 )
-
 // Azioni disponibili
 
 class Increase : RAction
-
 class Decrease : RAction
+class AddEditorTab(val content: String = "") : RAction
+class OnFileLoad(val fileName: String = "", val editorValue: String = "") : RAction
+
 
 // Gestione di azioni
-fun counter(counter: Counter = Counter(10), action: RAction): Counter = when (action) {
+fun counterActions(state: Counter = Counter(10), action: RAction): Counter = when (action) {
     is Increase -> {
-        counter.count++
-        counter
+        state.count++
+        state
     }
-
     is Decrease -> {
-        counter.count--
-        counter
+        state.count--
+        state
     }
-
-    else -> counter
+    else -> state
 }
+
+fun tuPrologActions(state: TuProlog = TuProlog("", "", mutableListOf()), action: RAction): TuProlog = when (action) {
+    is AddEditorTab -> {
+        val fileName: String = "undefined_" + Date().getTime() + ".pl"
+        state.editorTabs.add(
+            EditorTab(
+                fileName, action.content.trimIndent()
+            )
+        )
+        state.editorSelectedTab = fileName
+        state
+    }
+    is OnFileLoad -> {
+        console.log(action.fileName, action.editorValue)
+        if (state.editorTabs.find { it.fileName == action.fileName } == null) {
+            state.editorTabs.add(EditorTab(action.fileName, action.editorValue))
+        } else {
+//            errorAlertMessage = "File already exists"
+//            isErrorAlertOpen = true
+        }
+        state.editorSelectedTab = action.fileName
+        state
+    }
+    else -> state
+}
+
 // Redux Store
 
 fun rootReducer(
     state: State,
     action: Any
 ) = State(
-    counter(state.counter, action.unsafeCast<RAction>()),
+    counterActions(state.counter, action.unsafeCast<RAction>()),
+    tuPrologActions(state.tuProlog, action.unsafeCast<RAction>()),
 )
 
 val myStore = createStore(
     ::rootReducer,
-    State(),
+    State(
+        Counter(10),
+        TuProlog("", "", mutableListOf())
+    ),
     rEnhancer()
 )
