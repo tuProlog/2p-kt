bb5aa3a5072098b9f12c5ce828cd5f2f06fcaf13
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-04-28 18:12:38 +0200

    wip: breaking down :ide release into multiple jars

--- ide/build.gradle.kts
+++ ide/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.configurationcache.extensions.capitalized
 
 plugins {
     `kotlin-jvm-only`
@@ -11,6 +12,8 @@ plugins {
 
 val arguments: String? by project
 
+val supportedPlatforms = listOf("win", "linux", "mac", "mac-aarch64")
+
 dependencies {
     api(project(":io-lib"))
     api(project(":oop-lib"))
@@ -20,7 +23,7 @@ dependencies {
 
     libs.javafx.graphics.get().let {
         val dependencyNotation = "${it.module.group}:${it.module.name}:${it.versionConstraint.preferredVersion}"
-        listOf("win", "linux", "mac", "mac-aarch64").forEach { platform ->
+        supportedPlatforms.forEach { platform ->
             runtimeOnly("$dependencyNotation:$platform")
         }
     }
@@ -39,16 +42,38 @@ application {
     mainClass.set(entryPoint)
 }
 
-val shadowJar = tasks.getByName<ShadowJar>("shadowJar") {
-    manifest { attributes("Main-Class" to entryPoint) }
-    archiveBaseName.set("${rootProject.name}-${project.name}")
-    archiveVersion.set(project.version.toString())
-    archiveClassifier.set("redist")
-    sourceSets.main {
-        runtimeClasspath.filter { it.exists() }
-            .map { if (it.isDirectory) it else zipTree(it) }
-            .forEach { from(it) }
+fun shadowJar(
+    platform: String? = null,
+    name: String = "shadowJar" + (platform?.let { "For${it.capitalized()}" } ?: ""),
+    excludedPlatforms: List<String> = platform?.let { supportedPlatforms - it } ?: emptyList()
+): ShadowJar = tasks.maybeCreate(name, ShadowJar::class.java).also { jarTask ->
+    jarTask.manifest { attributes("Main-Class" to entryPoint) }
+    jarTask.archiveBaseName.set("${rootProject.name}-${project.name}")
+    jarTask.archiveVersion.set(project.version.toString())
+    if (platform !== null) {
+        jarTask.archiveClassifier.set("redist-$platform")
+    } else {
+        jarTask.archiveClassifier.set("redist")
     }
-    from(files("${rootProject.projectDir}/LICENSE"))
-    dependsOn("classes")
+    sourceSets.main {
+        println("Dependencies for platform ${platform ?: "all"}")
+        runtimeClasspath.filter { it.exists() }
+            .filter { file -> excludedPlatforms.none { file.name.endsWith("$it.jar") }.also {
+                if (!it) println("Exclude $file")
+            } }
+            .map { if (it.isDirectory) it else zipTree(it) }
+            .forEach { println(it) ; jarTask.from(it) }
+    }
+    jarTask.from(files("${rootProject.projectDir}/LICENSE"))
+    jarTask.dependsOn("classes")
+    tasks.maybeCreate("allShadowJars").also {
+        it.dependsOn(jarTask)
+        it.group = "shadow"
+    }
+}
+
+val shadowJar = shadowJar()
+
+for (platform in supportedPlatforms) {
+    shadowJar(platform)
 }
