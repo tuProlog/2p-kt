3b7949093437b33519d6976948d389c932831f84
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-07-02 10:07:06 +0200

    chore(build): fix buildSrc JVM target and mavenCentralRepo

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,3 +12,17 @@ dependencies {
     implementation(libs.shadowJar)
     implementation(libs.kotlin.gradlePlugin)
 }
+
+val targetJvm = libs.versions.jvm.get()
+val targetJava = JavaVersion.valueOf("VERSION_$targetJvm")
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = targetJvm
+    }
+}
+
+java {
+    sourceCompatibility = targetJava
+    targetCompatibility = targetJava
+}
