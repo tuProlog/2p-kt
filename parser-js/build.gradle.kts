7dde17d9771aa190e54a172d3dbdee9d0d8f54fa
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-07-06 10:17:04 +0200

    chore(build): fix packaging of parser-js

--- parser-js/build.gradle.kts
+++ parser-js/build.gradle.kts
@@ -18,23 +18,10 @@ kotlin {
     }
 }
 
-tasks.create<Copy>("jar") {
-    group = "build"
-    tasks.assemble.orNull?.dependsOn(this)
-    dependsOn("jsJar")
-    destinationDir = buildDir.resolve("libs")
-    from(destinationDir) {
-        rename("(.*)\\.klib", "$1.jar")
-    }
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-publishing {
-    publications.withType<MavenPublication> {
-        val jarFile = buildDir.resolve("libs").resolve("${project.name}-${project.version}.jar")
-        artifact(jarFile)
-        tasks.withType<AbstractPublishToMaven>()
-            .matching { it.name.contains(name, ignoreCase = true) }
-            .configureEach { dependsOn("jar") }
+afterEvaluate {
+    publishing {
+        publications.withType<MavenPublication> {
+            pom.packaging = "klib"
+        }
     }
 }
